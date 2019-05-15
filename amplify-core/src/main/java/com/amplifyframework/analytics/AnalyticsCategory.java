b9811dc2dac9edef3a74f5aaba43a674939db7dc
Karthikeyan Vasuki Balasubramaniam <karthba@amazon.com>
2019-05-15 15:44:11 -0700

    Add Stub for Analytics Category

---
+++ amplify-core/src/main/java/com/amplifyframework/analytics/AnalyticsCategory.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amplifyframework.analytics;
+
+import android.support.annotation.NonNull;
+
+import com.amplifyframework.core.exception.NoSuchProviderException;
+
+public class AnalyticsCategory implements AnalyticsCategoryClientBehavior {
+    public static AnalyticsCategoryProvider getProvider(@NonNull String key)
+            throws NoSuchProviderException {
+        return null;
+    }
+
+    public static void removeProvider(@NonNull String key)
+            throws NoSuchProviderException {
+
+    }
+
+    public static void disable() {
+
+    }
+
+    public static void enable() {
+
+    }
+
+    public static void recordEvent(String name) {
+
+    }
+
+    public static void recordEvent(AnalyticsEvent event) {
+
+    }
+
+    public static void updateProfile(AnalyticsProfile profile) {
+
+    }
+}
