ff3252f90191901c62c2ab0c2357b80fae82833d
Karthikeyan <kvasukib@ucsd.edu>
2019-11-01 00:01:01 -0700

    [DataStore] Add Model, ModelSchema and StorageAdapter interface (#44)
    * [DataStore] Add Model, ModelSchema and StorageAdapter interface
    
    * [DataStore] Add Model, ModelSchema and StorageAdapter interface
    
    * [DataStore] Add Model, ModelSchema and StorageAdapter interface
    
    * [DataStore] Add Model, ModelSchema and StorageAdapter interface

---
+++ aws-amplify-datastore/src/main/java/com/amplifyframework/datastore/annotations/ModelField.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amplifyframework.datastore.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation is used to indicate that a member of a class which extends
+ * {@link com.amplifyframework.datastore.model.Model} should be serialized or
+ * deserialized into the Datastore.
+ *
+ * The {@link RetentionPolicy#RUNTIME} annotation is added to
+ * retain {@link ModelField} at runtime for the reflection capabilities to work
+ * in order to check if this annotation is present for a field of a Model.
+ *
+ * {@link ElementType#FIELD} annotation is added to indicate
+ * {@link ModelField} annotation can be used only on Fields (Data members of a class).
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface ModelField {
+    /**
+     * Returns flag indicating if the field is required or optional.
+     * @return if the field is required or optional.
+     *          True if required, False if optional.
+     *          Default is optional (False).
+     */
+    boolean isRequired() default false;
+
+    /**
+     * Returns the target name of the field.
+     * @return the name of the field in the target.
+     *          For example: the name of the field
+     *          in the GraphQL schema.
+     */
+    String targetName() default "";
+}
