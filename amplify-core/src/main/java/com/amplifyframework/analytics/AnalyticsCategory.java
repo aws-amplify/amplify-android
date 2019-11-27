6015c5e77e478a645e92bc60e2eba91d57443d62
Roshan Kumar <roskumr@amazon.com>
2019-11-27 10:59:05 -0800

    Analytics configuration and record event API

--- amplify-core/src/main/java/com/amplifyframework/analytics/AnalyticsCategory.java
+++ amplify-core/src/main/java/com/amplifyframework/analytics/AnalyticsCategory.java
@@ -21,6 +21,9 @@ import com.amplifyframework.ConfigurationException;
 import com.amplifyframework.core.category.Category;
 import com.amplifyframework.core.category.CategoryType;
 
+import java.util.Collection;
+import java.util.Map;
+
 /**
  * Defines the Client API consumed by the application.
  * Internally routes the calls to the Analytics CategoryType
@@ -59,6 +62,11 @@ public final class AnalyticsCategory extends Category<AnalyticsPlugin<?>>
     }
 
     @Override
+    public void identifyUser(@NonNull String id, @NonNull AnalyticsProfile profile) {
+
+    }
+
+    @Override
     public void disable() {
         synchronized (LOCK) {
             enabled = false;
@@ -81,7 +89,7 @@ public final class AnalyticsCategory extends Category<AnalyticsPlugin<?>>
     }
 
     @Override
-    public void recordEvent(@NonNull final AnalyticsEvent analyticsEvent)
+    public void recordEvent(@NonNull final GeneralAnalyticsEvent analyticsEvent)
             throws AnalyticsException, ConfigurationException {
         if (enabled) {
             getSelectedPlugin().recordEvent(analyticsEvent);
@@ -89,10 +97,17 @@ public final class AnalyticsCategory extends Category<AnalyticsPlugin<?>>
     }
 
     @Override
-    public void updateProfile(@NonNull AnalyticsProfile analyticsProfile)
-            throws AnalyticsException, ConfigurationException {
-        if (enabled) {
-            getSelectedPlugin().updateProfile(analyticsProfile);
-        }
+    public void registerGlobalProperties(Map<String, Object> properties) {
+
+    }
+
+    @Override
+    public void unregisterGlobalProperties(Collection<String> keys) {
+
+    }
+
+    @Override
+    public void flushEvents() {
+
     }
 }
