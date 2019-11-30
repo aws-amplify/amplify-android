65c9a8902a08bfce5d16682b7cb5ff576b06d3f1
David Daudelin <sinceallelsefails@gmail.com>
2019-11-29 23:05:11 -0500

    [DataStore] ModelSync Contract and Data Objects

---
+++ aws-datastore/src/main/java/com/amplifyframework/datastore/network/ModelSyncMetadata.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amplifyframework.datastore.network;
+
+import com.amplifyframework.core.model.Model;
+import com.amplifyframework.core.model.annotations.ModelConfig;
+import com.amplifyframework.core.model.annotations.ModelField;
+
+/**
+ * System model to represent the versioning/sync metadata for an object.
+ */
+@ModelConfig
+@SuppressWarnings({"MemberName", "ParameterName"})
+public final class ModelSyncMetadata implements Model {
+    private final @ModelField(targetType = "ID", isRequired = true) String id;
+    private final @ModelField(targetType = "Boolean", isRequired = true) Boolean _deleted;
+    private final @ModelField(targetType = "Int", isRequired = true) Integer _version;
+    private final @ModelField(targetType = "AWSTimestamp", isRequired = true) Long _lastChangedAt;
+
+    /**
+     * Constructor for this metadata model.
+     * @param id The ID of the object this is holding the metadata for (also this object's own ID)
+     * @param deleted Whether this object was deleted since the last sync time specified
+     * @param version What version this object was last seen at
+     * @param lastChangedAt When was this object last changed
+     */
+    public ModelSyncMetadata(String id, Boolean deleted, Integer version, Long lastChangedAt) {
+        this.id = id;
+        this._deleted = deleted;
+        this._version = version;
+        this._lastChangedAt = lastChangedAt;
+    }
+
+    /**
+     * Gets ID.
+     * @return ID
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Gets the deleted status.
+     * @return True if deleted, False otherwise
+     */
+    public Boolean isDeleted() {
+        return _deleted;
+    }
+
+    /**
+     * Gets the version.
+     * @return version
+     */
+    public Integer getVersion() {
+        return _version;
+    }
+
+    /**
+     * Gets last changed at time.
+     * @return last changed at time
+     */
+    public Long getLastChangedAt() {
+        return _lastChangedAt;
+    }
+}
