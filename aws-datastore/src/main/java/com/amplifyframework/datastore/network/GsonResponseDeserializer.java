508d6e15a55ade613b02b2b8b18fb0b5cbe760cb
Jameson Williams <jhwill@amazon.com>
2019-12-03 14:23:39 -0800

    [DataStore] List deserialization as detail of response deserialization (#157)

--- aws-datastore/src/main/java/com/amplifyframework/datastore/network/GsonResponseDeserializer.java
+++ aws-datastore/src/main/java/com/amplifyframework/datastore/network/GsonResponseDeserializer.java
@@ -15,12 +15,23 @@
 
 package com.amplifyframework.datastore.network;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.amplifyframework.api.graphql.GraphQLResponse;
 import com.amplifyframework.core.model.Model;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -55,5 +66,51 @@ final class GsonResponseDeserializer implements ResponseDeserializer {
         T model = gson.fromJson(json, clazz);
         ModelMetadata metadata = gson.fromJson(json, ModelMetadata.class);
         return new ModelWithMetadata<>(model, metadata);
+    }
+
+    /**
+     * Custom list deserializer since some lists come back not as an array of the items but as an object which contains
+     * an items property with the list of items and a nextToken property for pagination purposes.
+     */
+    public static final class GsonListDeserializer implements JsonDeserializer<List<Object>> {
+        @Override
+        @Nullable
+        @SuppressWarnings("unchecked")
+        public List<Object> deserialize(@NonNull JsonElement json, @NonNull Type typeOfT, @Nullable JsonDeserializationContext context)
+                throws JsonParseException {
+
+            // If the json we got is not really a List and this List has a generics type...
+            if (json.isJsonObject() && typeOfT instanceof ParameterizedType) {
+                // Because this is a list and typeOfT is ParameterizedType we can be sure this is a safe cast.
+                Class<Object> clazz = (Class<Object>) ((ParameterizedType) typeOfT).getActualTypeArguments()[0];
+                JsonObject jsonObject = json.getAsJsonObject();
+                Gson gson = new GsonBuilder()
+                    .registerTypeAdapter(List.class, new GsonListDeserializer())
+                    .create();
+
+                // ...and it is in the format we expect from AppSync for a list of objects in a relationship
+                if (jsonObject.has("items") && jsonObject.get("items").isJsonArray()) {
+                    List<Object> response = new ArrayList<>();
+                    JsonArray items = jsonObject.get("items").getAsJsonArray();
+
+                    if (items.size() == 0) {
+                        return null;
+                    } else {
+                        for (JsonElement item : items) {
+                            response.add(gson.fromJson(item, clazz));
+                        }
+
+                        return response;
+                    }
+                } else {
+                    throw new JsonParseException("Got JSON from an API call which was supposed to go with a List " +
+                            "but is in the form of an object rather than an array. It also is not in the standard " +
+                            "format of having an items property with the actual array of data so we do not know how " +
+                            "to deserialize it.");
+                }
+            }
+
+            return new Gson().fromJson(json, typeOfT);
+        }
     }
 }
