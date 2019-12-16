2e0f5617106a083b0b54162847398bf318bd05f3
Raphael Kim <raphk@amazon.com>
2019-12-16 14:53:08 -0800

    Fix checkstyle error

--- aws-datastore/src/main/java/com/amplifyframework/datastore/storage/OperatorInterfaceAdapter.java
+++ aws-datastore/src/main/java/com/amplifyframework/datastore/storage/OperatorInterfaceAdapter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package com.amplifyframework.datastore.storage;
 
 import com.amplifyframework.core.model.query.predicate.BeginsWithQueryOperator;
@@ -10,6 +25,7 @@ import com.amplifyframework.core.model.query.predicate.LessOrEqualQueryOperator;
 import com.amplifyframework.core.model.query.predicate.LessThanQueryOperator;
 import com.amplifyframework.core.model.query.predicate.NotEqualQueryOperator;
 import com.amplifyframework.core.model.query.predicate.QueryOperator;
+
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
