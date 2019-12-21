47a9b0ece6523b4087068262c76a129a4b4d88ba
Jameson Williams <jhwill@amazon.com>
2019-12-20 21:19:05 -0800

    [API] Accept Lambdas in GraphQL Behaviors
    To reduce verbosity, the `ResultListener` and `StreamListener` are
    removed from the contracts of the GraphQL behaviors. Instead, the
    GraphQL behaviors are reworked to accept "decomposed" versions of those
    callbacks, using `Consumer` and `Action`, directly.
    
    For example, a query in Amplify 0.9 would have been written as:
    
    ```
    Amplify.API.query(Person.class,
        new ResultListener<GraphQLResponse<Iterable<Person>>>() {
            @Override
            public void onResponse(GraphQLResponse<Iterable<Person>> response) {
                Log.i(TAG, "Got results!");
            }
    
            @Override
            public void onError(ApiException failure) {
                Log.e(TAG, "Failed.", failure);
            }
        }
    );
    ```
    
    But is now expressed as:
    
    ```
    Amplify.API.query(Person.class,
        response -> Log.i(TAG, "Got a response: " + response),
        failure -> Log.e(TAG, "Failed to get a GraphQL response.", failure)
    );
    ```

--- aws-datastore/src/main/java/com/amplifyframework/datastore/network/AppSyncApi.java
+++ aws-datastore/src/main/java/com/amplifyframework/datastore/network/AppSyncApi.java
@@ -21,18 +21,18 @@ import androidx.annotation.VisibleForTesting;
 
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.ApiCategoryBehavior;
+import com.amplifyframework.api.ApiException;
 import com.amplifyframework.api.GraphQlBehavior;
 import com.amplifyframework.api.graphql.GraphQLRequest;
 import com.amplifyframework.api.graphql.GraphQLResponse;
 import com.amplifyframework.api.graphql.SubscriptionType;
+import com.amplifyframework.core.Action;
 import com.amplifyframework.core.Amplify;
-import com.amplifyframework.core.ResultListener;
-import com.amplifyframework.core.StreamListener;
+import com.amplifyframework.core.Consumer;
 import com.amplifyframework.core.async.Cancelable;
 import com.amplifyframework.core.model.Model;
 import com.amplifyframework.core.model.ModelSchema;
 import com.amplifyframework.datastore.DataStoreException;
-import com.amplifyframework.logging.Logger;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -44,11 +44,7 @@ import java.util.Objects;
  * which used the {@link ApiCategoryBehavior} to implement the various
  * GraphQL operations.
  */
-@SuppressWarnings("unused") // Hold my beer...
 public final class AppSyncApi implements AppSyncEndpoint {
-    private static final Logger LOG = Amplify.Logging.forNamespace("amplify:aws-datastore");
-    private static final int DEFAULT_QUERY_LIMIT = 1000;
-
     private final GraphQlBehavior api;
     private final GraphQLRequest.VariablesSerializer variablesSerializer;
     private final ResponseDeserializer responseDeserializer;
@@ -80,20 +76,36 @@ public final class AppSyncApi implements AppSyncEndpoint {
     public <T extends Model> Cancelable sync(
             @NonNull Class<T> modelClass,
             @Nullable Long lastSync,
-            @NonNull ResultListener<GraphQLResponse<Iterable<ModelWithMetadata<T>>>, DataStoreException> responseListener) {
-
+            @NonNull Consumer<GraphQLResponse<Iterable<ModelWithMetadata<T>>>> onResponse,
+            @NonNull Consumer<DataStoreException> onFailure) {
         final String queryDoc;
         try {
             queryDoc = AppSyncRequestFactory.buildSyncDoc(modelClass, lastSync, null);
         } catch (DataStoreException queryDocConstructionError) {
-            responseListener.onError(queryDocConstructionError);
+            onFailure.accept(queryDocConstructionError);
             return new NoOpCancelable();
         }
 
         final GraphQLRequest<String> request =
             new GraphQLRequest<>(queryDoc, Collections.emptyMap(), String.class, variablesSerializer);
-        final Cancelable cancelable =
-            api.query(request, SyncAdapter.instance(responseListener, modelClass, responseDeserializer));
+
+        final Consumer<GraphQLResponse<Iterable<String>>> responseConsumer = apiQueryResponse -> {
+            if (apiQueryResponse.hasErrors()) {
+                onFailure.accept(new DataStoreException(
+                    "Failure performing sync query to AppSync: " + apiQueryResponse.getErrors().toString(),
+                    AmplifyException.TODO_RECOVERY_SUGGESTION
+                ));
+            } else {
+                onResponse.accept(responseDeserializer.deserialize(apiQueryResponse.getData(), modelClass));
+            }
+        };
+        final Consumer<ApiException> failureConsumer =
+            failure -> onFailure.accept(new DataStoreException(
+                "Failure performing sync query to AppSync.",
+                failure, AmplifyException.TODO_RECOVERY_SUGGESTION
+            ));
+
+        final Cancelable cancelable = api.query(request, responseConsumer, failureConsumer);
         if (cancelable != null) {
             return cancelable;
         }
@@ -105,28 +117,23 @@ public final class AppSyncApi implements AppSyncEndpoint {
     @Override
     public <T extends Model> Cancelable create(
             @NonNull T model,
-            @NonNull ResultListener<GraphQLResponse<ModelWithMetadata<T>>, DataStoreException> responseListener) {
-        final String doc;
+            @NonNull Consumer<GraphQLResponse<ModelWithMetadata<T>>> onResponse,
+            @NonNull Consumer<DataStoreException> onFailure) {
         try {
-            doc = AppSyncRequestFactory.buildCreationDoc(model.getClass());
+            final String doc = AppSyncRequestFactory.buildCreationDoc(model.getClass());
 
             Class<T> modelClass = (Class<T>) model.getClass();
             ModelSchema schema = ModelSchema.fromModelClass(modelClass);
 
-            final Map<String, Object> variables = Collections.singletonMap(
-                    "input",
-                    schema.getMapOfFieldNameAndValues(model)
-            );
+            final Map<String, Object> variables =
+                Collections.singletonMap("input", schema.getMapOfFieldNameAndValues(model));
 
-            return mutation(doc, variables, modelClass, responseListener);
+            return mutation(doc, variables, modelClass, onResponse, onFailure);
         } catch (AmplifyException amplifyException) {
-            responseListener.onError(
-                    new DataStoreException(
-                            "Error encountered while creating model schema",
-                            amplifyException,
-                            "See attached exception for more details"
-                    )
-            );
+            onFailure.accept(new DataStoreException(
+                "Error encountered while creating model schema",
+                amplifyException, "See attached exception for more details"
+            ));
         }
 
         return new NoOpCancelable();
@@ -138,31 +145,25 @@ public final class AppSyncApi implements AppSyncEndpoint {
     public <T extends Model> Cancelable update(
             @NonNull T model,
             @NonNull Integer version,
-            @NonNull ResultListener<GraphQLResponse<ModelWithMetadata<T>>, DataStoreException> responseListener) {
-        final String doc;
+            @NonNull Consumer<GraphQLResponse<ModelWithMetadata<T>>> onResponse,
+            @NonNull Consumer<DataStoreException> onFailure) {
         try {
-            doc = AppSyncRequestFactory.buildUpdateDoc(model.getClass());
+            final String doc = AppSyncRequestFactory.buildUpdateDoc(model.getClass());
 
             Class<T> modelClass = (Class<T>) model.getClass();
             ModelSchema schema = ModelSchema.fromModelClass(modelClass);
 
-            Map<String, Object> updateInput = schema.getMapOfFieldNameAndValues(model);
+            final Map<String, Object> updateInput = schema.getMapOfFieldNameAndValues(model);
             updateInput.put("_version", version);
 
-            final Map<String, Object> variables = Collections.singletonMap(
-                    "input",
-                    updateInput
-            );
+            final Map<String, Object> variables = Collections.singletonMap("input", updateInput);
 
-            return mutation(doc, variables, (Class<T>) model.getClass(), responseListener);
+            return mutation(doc, variables, (Class<T>) model.getClass(), onResponse, onFailure);
         } catch (AmplifyException amplifyException) {
-            responseListener.onError(
-                    new DataStoreException(
-                            "Error encountered while creating model schema",
-                            amplifyException,
-                            "See attached exception for more details"
-                    )
-            );
+            onFailure.accept(new DataStoreException(
+                "Error encountered while creating model schema",
+                amplifyException, "See attached exception for more details"
+            ));
         }
 
         return new NoOpCancelable();
@@ -174,23 +175,20 @@ public final class AppSyncApi implements AppSyncEndpoint {
             @NonNull Class<T> clazz,
             @NonNull String objectId,
             @NonNull Integer version,
-            @NonNull ResultListener<GraphQLResponse<ModelWithMetadata<T>>, DataStoreException> responseListener) {
-        final String doc;
+            @NonNull Consumer<GraphQLResponse<ModelWithMetadata<T>>> onResponse,
+            @NonNull Consumer<DataStoreException> onFailure) {
         try {
-            doc = AppSyncRequestFactory.buildDeletionDoc(clazz);
+            final String doc = AppSyncRequestFactory.buildDeletionDoc(clazz);
 
-            Map<String, Object> deleteInput = new HashMap<>();
+            final Map<String, Object> deleteInput = new HashMap<>();
             deleteInput.put("id", objectId);
             deleteInput.put("_version", version);
 
-            final Map<String, Object> variables = Collections.singletonMap(
-                    "input",
-                    deleteInput
-            );
+            final Map<String, Object> variables = Collections.singletonMap("input", deleteInput);
 
-            return mutation(doc, variables, clazz, responseListener);
+            return mutation(doc, variables, clazz, onResponse, onFailure);
         } catch (DataStoreException dataStoreException) {
-            responseListener.onError(dataStoreException);
+            onFailure.accept(dataStoreException);
         }
 
         return new NoOpCancelable();
@@ -200,41 +198,68 @@ public final class AppSyncApi implements AppSyncEndpoint {
     @Override
     public <T extends Model> Cancelable onCreate(
             @NonNull Class<T> modelClass,
-            @NonNull StreamListener<GraphQLResponse<ModelWithMetadata<T>>, DataStoreException> subscriptionListener) {
-        return subscription(modelClass, subscriptionListener, SubscriptionType.ON_CREATE);
+            @NonNull Consumer<GraphQLResponse<ModelWithMetadata<T>>> onNextResponse,
+            @NonNull Consumer<DataStoreException> onSubscriptionFailure,
+            @NonNull Action onSubscriptionCompleted) {
+        return subscription(SubscriptionType.ON_CREATE, modelClass,
+            onNextResponse, onSubscriptionFailure, onSubscriptionCompleted);
     }
 
     @NonNull
     @Override
     public <T extends Model> Cancelable onUpdate(
             @NonNull Class<T> modelClass,
-            @NonNull StreamListener<GraphQLResponse<ModelWithMetadata<T>>, DataStoreException> subscriptionListener) {
-        return subscription(modelClass, subscriptionListener, SubscriptionType.ON_UPDATE);
+            @NonNull Consumer<GraphQLResponse<ModelWithMetadata<T>>> onNextResponse,
+            @NonNull Consumer<DataStoreException> onSubscriptionFailure,
+            @NonNull Action onSubscriptionCompleted) {
+        return subscription(SubscriptionType.ON_UPDATE, modelClass,
+            onNextResponse, onSubscriptionFailure, onSubscriptionCompleted);
     }
 
     @NonNull
     @Override
     public <T extends Model> Cancelable onDelete(
             @NonNull Class<T> modelClass,
-            @NonNull StreamListener<GraphQLResponse<ModelWithMetadata<T>>, DataStoreException> subscriptionListener) {
-        return subscription(modelClass, subscriptionListener, SubscriptionType.ON_DELETE);
+            @NonNull Consumer<GraphQLResponse<ModelWithMetadata<T>>> onNextResponse,
+            @NonNull Consumer<DataStoreException> onSubscriptionFailure,
+            @NonNull Action onSubscriptionCompleted) {
+        return subscription(SubscriptionType.ON_DELETE, modelClass,
+            onNextResponse, onSubscriptionFailure, onSubscriptionCompleted);
     }
 
     private <T extends Model> Cancelable subscription(
+            SubscriptionType subscriptionType,
             Class<T> clazz,
-            StreamListener<GraphQLResponse<ModelWithMetadata<T>>, DataStoreException> subscriptionListener,
-            SubscriptionType subscriptionType) {
+            Consumer<GraphQLResponse<ModelWithMetadata<T>>> onNextResponse,
+            Consumer<DataStoreException> onSubscriptionFailure,
+            Action onSubscriptionCompleted) {
         final String document;
         try {
             document = AppSyncRequestFactory.buildSubscriptionDoc(clazz, subscriptionType);
         } catch (DataStoreException docGenerationException) {
-            subscriptionListener.onError(docGenerationException);
+            onSubscriptionFailure.accept(docGenerationException);
             return new NoOpCancelable();
         }
         final GraphQLRequest<String> request =
             new GraphQLRequest<>(document, Collections.emptyMap(), String.class, variablesSerializer);
+
+        final Consumer<GraphQLResponse<String>> stringResponseConsumer = stringResponse -> {
+            if (stringResponse.hasErrors()) {
+                onSubscriptionFailure.accept(new DataStoreException(
+                    "Bad subscription data for " + clazz.getSimpleName() + ": " + stringResponse.getErrors(),
+                    AmplifyException.TODO_RECOVERY_SUGGESTION
+                ));
+            } else {
+                onNextResponse.accept(responseDeserializer.deserialize(stringResponse.getData(), clazz));
+            }
+        };
+        final Consumer<ApiException> failureConsumer = failure ->
+            onSubscriptionFailure.accept(new DataStoreException(
+                "Error during subscription.", failure, "Evaluate details."
+            ));
+
         final Cancelable cancelable =
-            api.subscribe(request, SubscriptionAdapter.instance(subscriptionListener, clazz, responseDeserializer));
+            api.subscribe(request, stringResponseConsumer, failureConsumer, onSubscriptionCompleted);
         if (cancelable != null) {
             return cancelable;
         }
@@ -245,11 +270,23 @@ public final class AppSyncApi implements AppSyncEndpoint {
             final String document,
             final Map<String, Object> variables,
             final Class<T> itemClass,
-            final ResultListener<GraphQLResponse<ModelWithMetadata<T>>, DataStoreException> responseListener) {
+            final Consumer<GraphQLResponse<ModelWithMetadata<T>>> onResponse,
+            final Consumer<DataStoreException> onFailure) {
         final GraphQLRequest<String> request =
             new GraphQLRequest<>(document, variables, String.class, variablesSerializer);
-        final Cancelable cancelable =
-            api.mutate(request, MutationAdapter.instance(responseListener, itemClass, responseDeserializer));
+
+        final Consumer<GraphQLResponse<String>> responseConsumer = response -> {
+            if (response.hasErrors()) {
+                onResponse.accept(new GraphQLResponse<>(null, response.getErrors()));
+            } else {
+                onResponse.accept(responseDeserializer.deserialize(response.getData(), itemClass));
+            }
+        };
+        final Consumer<ApiException> failureConsumer =
+            failure -> onFailure.accept(new DataStoreException(
+                "Failure during mutation.", failure, "Check details."
+            ));
+        final Cancelable cancelable = api.mutate(request, responseConsumer, failureConsumer);
         if (cancelable != null) {
             return cancelable;
         }
