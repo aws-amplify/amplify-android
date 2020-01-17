fe9678db01f537b88724b6a7d28a0ebe3ed1c51f
Jameson Williams <jhwill@amazon.com>
2020-01-16 17:41:39 -0800

    Overhaul module build scripts
    1. Move Android project boiler-plate into a central bootrapping function
       in the top-level build.gradle;
    2. Centralize dependency versions into the ext block of top-level
       build.gradle
    
    Inspiration taken from:
    
    - https://stackoverflow.com/a/45605982/695787
    - https://github.com/bugsnag/bugsnag-android/blob/master/build.gradle

--- aws-datastore/src/androidTest/java/com/amplifyframework/datastore/storage/sqlite/SQLiteStorageAdapterInstrumentedTest.java
+++ aws-datastore/src/androidTest/java/com/amplifyframework/datastore/storage/sqlite/SQLiteStorageAdapterInstrumentedTest.java
@@ -53,11 +53,11 @@ import java.util.concurrent.TimeUnit;
 
 import static com.amplifyframework.core.model.query.predicate.QueryPredicateOperation.not;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 /**
