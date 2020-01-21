afe9f2d80d1a0b671cdf0d780bbcbe00034190c5
Jameson Williams <jhwill@amazon.com>
2020-01-21 14:00:15 -0800

    Generate AmplifyConfiguration from a factory method (#239)
    * Generate AmplifyConfiguration from a factory method
    
    * Apply PR feedback from Raphael

--- core/src/main/java/com/amplifyframework/core/Amplify.java
+++ core/src/main/java/com/amplifyframework/core/Amplify.java
@@ -93,9 +93,7 @@ public final class Amplify {
      * @throws AmplifyException thrown when already configured or there is no plugin found for a configuration
      */
     public static void configure(@NonNull Context context) throws AmplifyException {
-        AmplifyConfiguration config = new AmplifyConfiguration();
-        config.populateFromConfigFile(context);
-        configure(config, context);
+        configure(AmplifyConfiguration.fromConfigFile(context), context);
     }
 
     /**
