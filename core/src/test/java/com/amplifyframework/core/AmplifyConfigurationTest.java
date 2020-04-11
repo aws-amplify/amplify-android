d19a0485753c29f6ed006972e9c6c3f5bc0f6d04
John Pignata <john@pignata.com>
2020-04-11 11:54:11 -0400

    [core] Handle missing configuration file
    If you do not generate a configuration file with Amplify init, your
    application will crash with an unhandled exception. This change throws
    an AmplifyException instead to allow the developer to catch their error
    during development.

---
+++ core/src/test/java/com/amplifyframework/core/AmplifyConfigurationTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amplifyframework.core;
+
+import android.content.Context;
+import androidx.test.core.app.ApplicationProvider;
+
+import com.amplifyframework.AmplifyException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Tests for {@link AmplifyConfiguration} behavior.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class AmplifyConfigurationTest {
+    private Context context = ApplicationProvider.getApplicationContext();
+
+    /**
+     * Attempting to call {@link AmplifyConfiguration#fromConfigFile(Context)}
+     * without a generated configuration file throws an AmplifyException.
+     *
+     * @throws Exception if the premise of the test is incorrect
+     */
+    @Test(expected = AmplifyException.class)
+    public void testMissingConfigurationFileThrowsAmplifyException() throws Exception {
+        AmplifyConfiguration.fromConfigFile(context);
+    }
+}
