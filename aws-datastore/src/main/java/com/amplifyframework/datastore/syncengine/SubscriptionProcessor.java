ad00bc790103cb8e16d9e66f97f3adaf2bf76f3a
Jameson Williams <jhwill@amazon.com>
2020-05-15 03:43:43 -0400

    [aws-datastore] Refactor SubscriptionProcessor
    The logic for the SubscriptionProcessor was split accross an anemic
    class, the actual SubscriptionProcessor, and then another class called
    RemoteModelMutations. Instead, thisl atter class is combined back into
    the SubscriptionProcessor.
    
    Unit test coverage is added.
    
    Previously, the processor would not wait for all of the subscriptions to
    have been fully established. This resulted in situations where data
    could be missed on a subscription. To resolve this, wait receipt of a
    subscription ID before yiedling control of a subscription observable.

--- aws-datastore/src/main/java/com/amplifyframework/datastore/syncengine/SubscriptionProcessor.java
+++ aws-datastore/src/main/java/com/amplifyframework/datastore/syncengine/SubscriptionProcessor.java
@@ -16,37 +16,63 @@
 package com.amplifyframework.datastore.syncengine;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
 
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.graphql.GraphQLResponse;
+import com.amplifyframework.api.graphql.SubscriptionType;
+import com.amplifyframework.core.Action;
 import com.amplifyframework.core.Amplify;
+import com.amplifyframework.core.Consumer;
+import com.amplifyframework.core.async.Cancelable;
+import com.amplifyframework.core.async.NoOpCancelable;
 import com.amplifyframework.core.model.Model;
+import com.amplifyframework.core.model.ModelProvider;
+import com.amplifyframework.datastore.DataStoreException;
+import com.amplifyframework.datastore.appsync.AppSync;
+import com.amplifyframework.datastore.appsync.ModelWithMetadata;
 import com.amplifyframework.logging.Logger;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
+import io.reactivex.Observable;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.ReplaySubject;
 
 /**
- * Observes mutations occurring on a remote system, via the {@link RemoteModelMutations}.
- * For each, marries the data back into the local DataStore, through the {@link Merger}.
+ * Observes mutations occurring on a remote {@link AppSync} system. The mutations arrive
+ * over a long-lived subscription, as {@link SubscriptionEvent}s.
+ * For every type of model provided by a {@link ModelProvider}, the SubscriptionProcessor
+ * marries mutated models back into the local DataStore, through the {@link Merger}.
  */
 final class SubscriptionProcessor {
+    private static final long SUBSCRIPTION_START_TIMEOUT_MS = TimeUnit.SECONDS.toMillis(10);
     private static final Logger LOG = Amplify.Logging.forNamespace("amplify:aws-datastore");
 
-    private final RemoteModelMutations remoteModelMutations;
+    private final AppSync appSync;
+    private final ModelProvider modelProvider;
     private final Merger merger;
     private final CompositeDisposable disposable;
     private final ReplaySubject<SubscriptionEvent<? extends Model>> buffer;
 
     /**
      * Constructs a new SubscriptionProcessor.
-     * @param remoteModelMutations An observable stream of mutations occurring on other end of network
+     * @param appSync An App Sync endpoint from which to receive subscription events
+     * @param modelProvider The processor will subscribe to changes for these types of models
      * @param merger A merger, to apply data back into local storage
      */
     SubscriptionProcessor(
-            @NonNull RemoteModelMutations remoteModelMutations,
+            @NonNull AppSync appSync,
+            @NonNull ModelProvider modelProvider,
             @NonNull Merger merger) {
-        this.remoteModelMutations = Objects.requireNonNull(remoteModelMutations);
+        this.appSync = Objects.requireNonNull(appSync);
+        this.modelProvider = Objects.requireNonNull(modelProvider);
         this.merger = Objects.requireNonNull(merger);
         this.disposable = new CompositeDisposable();
         this.buffer = ReplaySubject.create();
@@ -56,15 +82,47 @@ final class SubscriptionProcessor {
      * Start subscribing to model mutations.
      */
     void startSubscriptions() {
-        disposable.add(remoteModelMutations.observe()
+        Set<Observable<SubscriptionEvent<? extends Model>>> subscriptions = new HashSet<>();
+        for (Class<? extends Model> clazz : modelProvider.models()) {
+            for (SubscriptionType subscriptionType : SubscriptionType.values()) {
+                subscriptions.add(subscriptionObservable(appSync, subscriptionType, clazz));
+            }
+        }
+        disposable.add(Observable.merge(subscriptions)
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
             .subscribe(
-                mutationOnSubscription -> {
-                    buffer.onNext(mutationOnSubscription);
-                    LOG.info("Successfully enqueued mutation from subscription: " + mutationOnSubscription);
-                },
-                error -> LOG.warn("Error enqueuing mutation from subscription.", error),
-                () -> LOG.warn("Subscription to remote model mutations is completed.")
-            )
+                buffer::onNext,
+                buffer::onError,
+                buffer::onComplete
+            ));
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T extends Model> Observable<SubscriptionEvent<? extends Model>>
+            subscriptionObservable(AppSync appSync, SubscriptionType subscriptionType, Class<T> clazz) {
+        return Observable.<GraphQLResponse<ModelWithMetadata<T>>>create(emitter -> {
+            CountDownLatch latch = new CountDownLatch(1);
+            SubscriptionMethod method = subscriptionMethodFor(appSync, subscriptionType);
+            AtomicReference<Cancelable> cancelable = new AtomicReference<>(NoOpCancelable::new);
+            emitter.setCancellable(cancelable::get);
+            cancelable.set(method.subscribe(
+                clazz,
+                token -> latch.countDown(),
+                emitter::onNext,
+                emitter::onError,
+                emitter::onComplete
+            ));
+            latch.await(SUBSCRIPTION_START_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        })
+        .subscribeOn(Schedulers.io())
+        .observeOn(Schedulers.io())
+        .map(SubscriptionProcessor::unwrapResponse)
+        .map(modelWithMetadata -> SubscriptionEvent.<T>builder()
+            .type(fromSubscriptionType(subscriptionType))
+            .modelWithMetadata(modelWithMetadata)
+            .modelClass((Class<T>) modelWithMetadata.getModel().getClass())
+            .build()
         );
     }
 
@@ -88,5 +146,64 @@ final class SubscriptionProcessor {
      */
     void stopAllSubscriptionActivity() {
         disposable.clear();
+    }
+
+    @VisibleForTesting
+    static SubscriptionMethod subscriptionMethodFor(
+            AppSync appSync, SubscriptionType subscriptionType) throws DataStoreException {
+        switch (subscriptionType) {
+            case ON_UPDATE:
+                return appSync::onUpdate;
+            case ON_DELETE:
+                return appSync::onDelete;
+            case ON_CREATE:
+                return appSync::onCreate;
+            default:
+                throw new DataStoreException(
+                    "Failed to establish a model subscription.",
+                    "Was a new subscription type created?"
+                );
+        }
+    }
+
+    private static <T extends Model> ModelWithMetadata<T> unwrapResponse(
+            GraphQLResponse<? extends ModelWithMetadata<T>> response) throws DataStoreException {
+        final String errorMessage;
+        if (response.hasErrors()) {
+            errorMessage = String.format("Errors on subscription: %s", response.getErrors());
+        } else if (!response.hasData()) {
+            errorMessage = "Empty data received on subscription.";
+        } else {
+            errorMessage = null;
+        }
+        if (errorMessage != null) {
+            throw new DataStoreException(
+                errorMessage, AmplifyException.REPORT_BUG_TO_AWS_SUGGESTION
+            );
+        }
+        return response.getData();
+    }
+
+    private static SubscriptionEvent.Type fromSubscriptionType(SubscriptionType subscriptionType) {
+        switch (subscriptionType) {
+            case ON_CREATE:
+                return SubscriptionEvent.Type.CREATE;
+            case ON_DELETE:
+                return SubscriptionEvent.Type.DELETE;
+            case ON_UPDATE:
+                return SubscriptionEvent.Type.UPDATE;
+            default:
+                throw new IllegalArgumentException("Unknown subscription type: " + subscriptionType);
+        }
+    }
+
+    interface SubscriptionMethod {
+        <T extends Model> Cancelable subscribe(
+                @NonNull Class<T> clazz,
+                @NonNull Consumer<String> onStart,
+                @NonNull Consumer<GraphQLResponse<ModelWithMetadata<T>>> onResponse,
+                @NonNull Consumer<DataStoreException> onFailure,
+                @NonNull Action onComplete
+        );
     }
 }
