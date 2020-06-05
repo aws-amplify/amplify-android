aws-amplify/amplify-android

commit 775428de668dcc848ab1f884051f5d728f94c60c

fix(core): use sentinel for null query predicate (#540)
Around the code-base, a `null` value for a `QueryPredicate` has the
meaning of "don't apply any filter," or, "match everything."

That's sort of clear, maybe.

Instead of having a meaningful `null` value, use a sentinel, instead,
which declares its purpose as QueryPredicates.matchAll().

aws-api/src/main/java/com/amplifyframework/api/graphql/model/ModelQuery.java
@@ -16,13 +16,15 @@
 package com.amplifyframework.api.graphql.model;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.amplifyframework.api.aws.AppSyncGraphQLRequestFactory;
 import com.amplifyframework.api.graphql.GraphQLRequest;
 import com.amplifyframework.api.graphql.PaginatedResult;
 import com.amplifyframework.core.model.Model;
 import com.amplifyframework.core.model.query.predicate.QueryPredicate;
+import com.amplifyframework.core.model.query.predicate.QueryPredicates;
+
+import java.util.Objects;
 
 /**
  * Helper class that provides methods to create {@link GraphQLRequest} for queries
@@ -60,8 +62,10 @@ public final class ModelQuery {
      */
     public static <M extends Model> GraphQLRequest<Iterable<M>> list(
             @NonNull Class<M> modelType,
-            @Nullable QueryPredicate predicate
+            @NonNull QueryPredicate predicate
     ) {
+        Objects.requireNonNull(modelType);
+        Objects.requireNonNull(predicate);
         return AppSyncGraphQLRequestFactory.buildQuery(modelType, predicate);
     }
 
@@ -74,7 +78,7 @@ public final class ModelQuery {
      * @see #list(Class, QueryPredicate)
      */
     public static <M extends Model> GraphQLRequest<Iterable<M>> list(@NonNull Class<M> modelType) {
-        return list(modelType, (QueryPredicate) null);
+        return list(modelType, QueryPredicates.all());
     }
 
     /**

aws-api/src/main/java/com/amplifyframework/api/aws/AppSyncGraphQLRequestFactory.java
@@ -37,6 +37,7 @@ import com.amplifyframework.core.model.query.predicate.QueryOperator;
 import com.amplifyframework.core.model.query.predicate.QueryPredicate;
 import com.amplifyframework.core.model.query.predicate.QueryPredicateGroup;
 import com.amplifyframework.core.model.query.predicate.QueryPredicateOperation;
+import com.amplifyframework.core.model.query.predicate.QueryPredicates;
 import com.amplifyframework.util.Casing;
 import com.amplifyframework.util.FieldFinder;
 import com.amplifyframework.util.Immutable;
@@ -170,6 +171,8 @@ public final class AppSyncGraphQLRequestFactory {
             ModelSchema schema = ModelSchema.fromModelClass(modelClass);
             String graphQlTypeName = schema.getName();
 
+            boolean includePredicate = !QueryPredicates.all().equals(predicate);
+
             doc.append("query ")
                     .append("List")
                     .append(Casing.capitalizeFirst(graphQlTypeName))
@@ -183,7 +186,7 @@ public final class AppSyncGraphQLRequestFactory {
                     .append(getModelFields(modelClass, DEFAULT_LEVEL_DEPTH))
                     .append("} nextToken }}");
 
-            if (predicate != null) {
+            if (includePredicate) {
                 variables.put("filter", parsePredicate(predicate));
             }
             if (nextToken != null) {
@@ -231,6 +234,8 @@ public final class AppSyncGraphQLRequestFactory {
             String typeStr = type.toString();
             String graphQlTypeName = schema.getName();
 
+            boolean includePredicate = !QueryPredicates.all().equals(predicate);
+
             doc.append("mutation ")
                     .append(Casing.capitalize(typeStr))
                     .append(Casing.capitalizeFirst(graphQlTypeName))
@@ -239,7 +244,7 @@ public final class AppSyncGraphQLRequestFactory {
                     .append(Casing.capitalizeFirst(graphQlTypeName))
                     .append("Input!");
 
-            if (predicate != null) {
+            if (includePredicate) {
                 doc.append(", $condition: Model")
                         .append(graphQlTypeName)
                         .append("ConditionInput");
@@ -250,7 +255,7 @@ public final class AppSyncGraphQLRequestFactory {
                     .append(Casing.capitalizeFirst(graphQlTypeName))
                     .append("(input: $input");
 
-            if (predicate != null) {
+            if (includePredicate) {
                 doc.append(", condition: $condition");
             }
 
@@ -273,7 +278,7 @@ public final class AppSyncGraphQLRequestFactory {
                 }
             }
 
-            if (predicate != null) {
+            if (includePredicate) {
                 variables.put("condition", parsePredicate(predicate));
             }
 

aws-api/src/main/java/com/amplifyframework/api/graphql/model/ModelMutation.java
@@ -22,15 +22,13 @@ import com.amplifyframework.api.graphql.GraphQLRequest;
 import com.amplifyframework.api.graphql.MutationType;
 import com.amplifyframework.core.model.Model;
 import com.amplifyframework.core.model.query.predicate.QueryPredicate;
+import com.amplifyframework.core.model.query.predicate.QueryPredicates;
 
 /**
  * Helper class that provides methods to create {@link GraphQLRequest} from {@link Model}.
  */
 public final class ModelMutation {
-
-    /** This class should not be instantiated. */
-    private ModelMutation() {
-    }
+    private ModelMutation() {}
 
     /**
      * Creates a {@link GraphQLRequest} that represents a create mutation for a given {@code model} instance.
@@ -40,7 +38,7 @@ public final class ModelMutation {
      * @see MutationType#CREATE
      */
     public static <M extends Model> GraphQLRequest<M> create(@NonNull M model) {
-        return AppSyncGraphQLRequestFactory.buildMutation(model, null, MutationType.CREATE);
+        return AppSyncGraphQLRequestFactory.buildMutation(model, QueryPredicates.all(), MutationType.CREATE);
     }
 
     /**
@@ -67,7 +65,7 @@ public final class ModelMutation {
      * @see #update(Model, QueryPredicate)
      */
     public static <M extends Model> GraphQLRequest<M> update(@NonNull M model) {
-        return AppSyncGraphQLRequestFactory.buildMutation(model, null, MutationType.UPDATE);
+        return AppSyncGraphQLRequestFactory.buildMutation(model, QueryPredicates.all(), MutationType.UPDATE);
     }
 
     /**
@@ -94,7 +92,6 @@ public final class ModelMutation {
      * @see #delete(Model, QueryPredicate)
      */
     public static <M extends Model> GraphQLRequest<M> delete(@NonNull M model) {
-        return AppSyncGraphQLRequestFactory.buildMutation(model, null, MutationType.DELETE);
+        return AppSyncGraphQLRequestFactory.buildMutation(model, QueryPredicates.all(), MutationType.DELETE);
     }
-
 }

aws-api/src/test/java/com/amplifyframework/api/aws/AppSyncGraphQLRequestFactoryTest.java
@@ -20,6 +20,7 @@ import com.amplifyframework.api.ApiException;
 import com.amplifyframework.api.graphql.GraphQLRequest;
 import com.amplifyframework.api.graphql.MutationType;
 import com.amplifyframework.api.graphql.SubscriptionType;
+import com.amplifyframework.core.model.query.predicate.QueryPredicates;
 import com.amplifyframework.core.model.temporal.Temporal;
 import com.amplifyframework.testmodels.meeting.Meeting;
 import com.amplifyframework.testmodels.personcar.MaritalStatus;
@@ -156,7 +157,7 @@ public final class AppSyncGraphQLRequestFactoryTest {
 
         // Act: build a mutation to create a Meeting
         GraphQLRequest<Meeting> requestToCreateMeeting1 =
-                AppSyncGraphQLRequestFactory.buildMutation(meeting1, null, MutationType.CREATE);
+                AppSyncGraphQLRequestFactory.buildMutation(meeting1, QueryPredicates.all(), MutationType.CREATE);
 
         // Assert: expected is actual
         JSONAssert.assertEquals(Resources.readAsString("create-meeting1.txt"),

aws-datastore/src/androidTest/java/com/amplifyframework/datastore/appsync/SynchronousAppSync.java
@@ -22,6 +22,7 @@ import com.amplifyframework.api.graphql.GraphQLResponse;
 import com.amplifyframework.core.NoOpConsumer;
 import com.amplifyframework.core.model.Model;
 import com.amplifyframework.core.model.query.predicate.QueryPredicate;
+import com.amplifyframework.core.model.query.predicate.QueryPredicates;
 import com.amplifyframework.datastore.DataStoreException;
 import com.amplifyframework.testutils.Await;
 
@@ -33,7 +34,10 @@ import io.reactivex.Observable;
  * A synchronous wrapper around an AppSync client, useful in test, so you can just
  * wait for the results of network operations.
  */
-@SuppressWarnings("unused") // Oh, that it might be, someday, though!
+@SuppressWarnings({
+    "unused", // Some of the behaviors aren't, currently, true.
+    "WeakerAccess"
+})
 public final class SynchronousAppSync {
     private final AppSync appSync;
 
@@ -93,8 +97,8 @@ public final class SynchronousAppSync {
      */
     @NonNull
     public <T extends Model> GraphQLResponse<ModelWithMetadata<T>> update(
-        @NonNull T model, @NonNull Integer version) throws DataStoreException {
-        return update(model, version, null);
+            @NonNull T model, @NonNull Integer version) throws DataStoreException {
+        return update(model, version, QueryPredicates.all());
     }
 
     /**
@@ -108,7 +112,7 @@ public final class SynchronousAppSync {
      */
     @NonNull
     public <T extends Model> GraphQLResponse<ModelWithMetadata<T>> update(
-        @NonNull T model, @NonNull Integer version, @Nullable QueryPredicate predicate) throws DataStoreException {
+            @NonNull T model, @NonNull Integer version, @NonNull QueryPredicate predicate) throws DataStoreException {
         return Await.<GraphQLResponse<ModelWithMetadata<T>>, DataStoreException>result(((onResult, onError) ->
             appSync.update(model, version, predicate, onResult, onError)
         ));
@@ -126,7 +130,7 @@ public final class SynchronousAppSync {
     @NonNull
     <T extends Model> GraphQLResponse<ModelWithMetadata<T>> delete(
         @NonNull Class<T> clazz, @NonNull String objectId, @NonNull Integer version) throws DataStoreException {
-        return delete(clazz, objectId, version, null);
+        return delete(clazz, objectId, version, QueryPredicates.all());
     }
 
     /**
@@ -144,7 +148,7 @@ public final class SynchronousAppSync {
             @NonNull Class<T> clazz,
             @NonNull String objectId,
             @NonNull Integer version,
-            @Nullable QueryPredicate predicate) throws DataStoreException {
+            @NonNull QueryPredicate predicate) throws DataStoreException {
         return Await.<GraphQLResponse<ModelWithMetadata<T>>, DataStoreException>result((onResult, onError) ->
             appSync.delete(clazz, objectId, version, predicate, onResult, onError)
         );

aws-datastore/src/androidTest/java/com/amplifyframework/datastore/storage/SynchronousStorageAdapter.java
@@ -24,6 +24,7 @@ import com.amplifyframework.core.model.ModelSchema;
 import com.amplifyframework.core.model.query.QueryOptions;
 import com.amplifyframework.core.model.query.Where;
 import com.amplifyframework.core.model.query.predicate.QueryPredicate;
+import com.amplifyframework.core.model.query.predicate.QueryPredicates;
 import com.amplifyframework.datastore.DataStoreException;
 import com.amplifyframework.testutils.Await;
 
@@ -111,15 +112,14 @@ public final class SynchronousStorageAdapter {
      * @throws DataStoreException On any failure to save model into storage adapter
      */
     public <T extends Model> void save(@NonNull T model) throws DataStoreException {
-        //noinspection ConstantConditions
-        save(model, null);
+        save(model, QueryPredicates.all());
     }
 
     /**
      * Save a model.
      * @param model Model to save
      * @param predicate An existing instance of the model in the storage adapter must meet these criteria
-     *                  in order for the save to succeed. If null, no criteria are considered
+     *                  in order for the save to succeed.
      * @param <T> Type of model being saved
      * @throws DataStoreException On any failure to save the model
      */
@@ -145,8 +145,7 @@ public final class SynchronousStorageAdapter {
      * @return The exception that was raised while attempting to save the model
      */
     public <T extends Model> DataStoreException saveExpectingError(@NonNull T model) {
-        //noinspection ConstantConditions
-        return saveExpectingError(model, null);
+        return saveExpectingError(model, QueryPredicates.all());
     }
 
     /**
@@ -213,8 +212,7 @@ public final class SynchronousStorageAdapter {
      * @throws DataStoreException On any failure to delete model
      */
     public <T extends Model> void delete(@NonNull T model) throws DataStoreException {
-        //noinspection ConstantConditions
-        delete(model, null);
+        delete(model, QueryPredicates.all());
     }
 
     /**
@@ -249,8 +247,7 @@ public final class SynchronousStorageAdapter {
      */
     @SuppressWarnings("unused")
     public <T extends Model> DataStoreException deleteExpectingError(@NonNull T model) {
-        //noinspection ConstantConditions
-        return deleteExpectingError(model, null);
+        return deleteExpectingError(model, QueryPredicates.all());
     }
 
     /**
@@ -294,10 +291,8 @@ public final class SynchronousStorageAdapter {
      */
     public void clear() {
         //noinspection ResultOfMethodCallIgnored
-        Completable.fromSingle(single -> {
-            asyncDelegate.clear(() -> {
-                single.onSuccess(true);
-            }, single::onError);
-        }).blockingAwait(DEFAULT_OPERATION_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        Completable.create(emitter ->
+            asyncDelegate.clear(emitter::onComplete, emitter::onError)
+        ).blockingAwait(DEFAULT_OPERATION_TIMEOUT_MS, TimeUnit.MILLISECONDS);
     }
 }

aws-datastore/src/main/java/com/amplifyframework/datastore/AWSDataStorePlugin.java
@@ -36,6 +36,7 @@ import com.amplifyframework.core.model.ModelSchemaRegistry;
 import com.amplifyframework.core.model.query.QueryOptions;
 import com.amplifyframework.core.model.query.Where;
 import com.amplifyframework.core.model.query.predicate.QueryPredicate;
+import com.amplifyframework.core.model.query.predicate.QueryPredicates;
 import com.amplifyframework.datastore.appsync.AppSyncClient;
 import com.amplifyframework.datastore.model.ModelProviderLocator;
 import com.amplifyframework.datastore.storage.LocalStorageAdapter;
@@ -239,7 +240,7 @@ public final class AWSDataStorePlugin extends DataStorePlugin<Void> {
             @NonNull T item,
             @NonNull Consumer<DataStoreItemChange<T>> onItemSaved,
             @NonNull Consumer<DataStoreException> onFailureToSave) {
-        save(item, null, onItemSaved, onFailureToSave);
+        save(item, QueryPredicates.all(), onItemSaved, onFailureToSave);
     }
 
     /**
@@ -248,7 +249,7 @@ public final class AWSDataStorePlugin extends DataStorePlugin<Void> {
     @Override
     public <T extends Model> void save(
             @NonNull T item,
-            @Nullable QueryPredicate predicate,
+            @NonNull QueryPredicate predicate,
             @NonNull Consumer<DataStoreItemChange<T>> onItemSaved,
             @NonNull Consumer<DataStoreException> onFailureToSave) {
         beforeOperation(() -> sqliteStorageAdapter.save(
@@ -274,7 +275,7 @@ public final class AWSDataStorePlugin extends DataStorePlugin<Void> {
             @NonNull T item,
             @NonNull Consumer<DataStoreItemChange<T>> onItemDeleted,
             @NonNull Consumer<DataStoreException> onFailureToDelete) {
-        delete(item, null, onItemDeleted, onFailureToDelete);
+        delete(item, QueryPredicates.all(), onItemDeleted, onFailureToDelete);
     }
 
     /**
@@ -283,7 +284,7 @@ public final class AWSDataStorePlugin extends DataStorePlugin<Void> {
     @Override
     public <T extends Model> void delete(
             @NonNull T item,
-            @Nullable QueryPredicate predicate,
+            @NonNull QueryPredicate predicate,
             @NonNull Consumer<DataStoreItemChange<T>> onItemDeleted,
             @NonNull Consumer<DataStoreException> onFailureToDelete) {
         beforeOperation(() -> sqliteStorageAdapter.delete(

aws-datastore/src/main/java/com/amplifyframework/datastore/appsync/AppSync.java
@@ -107,7 +107,7 @@ public interface AppSync {
     <T extends Model> Cancelable update(
             @NonNull T model,
             @NonNull Integer version,
-            @Nullable QueryPredicate predicate,
+            @NonNull QueryPredicate predicate,
             @NonNull Consumer<GraphQLResponse<ModelWithMetadata<T>>> onResponse,
             @NonNull Consumer<DataStoreException> onFailure
     );
@@ -147,7 +147,7 @@ public interface AppSync {
             @NonNull Class<T> clazz,
             @NonNull String objectId,
             @NonNull Integer version,
-            @Nullable QueryPredicate predicate,
+            @NonNull QueryPredicate predicate,
             @NonNull Consumer<GraphQLResponse<ModelWithMetadata<T>>> onResponse,
             @NonNull Consumer<DataStoreException> onFailure
     );

commit e94c6dc8aad4cbb403569da805a963fd846c815a

Fix bug introduced in #540, caught by unit test
`null` is no longer a valid value for a QueryPredicate. QueryPredicates.all() should be used instead now.  Because `null` was still used, an IllegalStateException: Invalid predicate type error was thrown

aws-api/src/main/java/com/amplifyframework/api/graphql/model/ModelQuery.java
@@ -122,7 +122,7 @@ public final class ModelQuery {
             @NonNull ModelPagination pagination
     ) {
         return AppSyncGraphQLRequestFactory.buildPaginatedResultQuery(
-                modelType, null, pagination.getLimit());
+                modelType, QueryPredicates.all(), pagination.getLimit());
     }
 
 }