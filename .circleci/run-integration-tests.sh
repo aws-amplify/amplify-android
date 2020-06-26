964c7a5f489296eef0aa42431d708d19f0fae616
Richard McClellan <ricmccle@amazon.com>
2020-06-26 10:35:31 -0700

    logging

--- .circleci/run-integration-tests.sh
+++ .circleci/run-integration-tests.sh
@@ -8,13 +8,13 @@ tasks=($(./gradlew tasks --all | grep connectedDebugAndroidTest | cut -d " " -f 
 return_code=0
 tag="RUN_INTEGRATION_TESTS"
 for task in "${tasks[@]}"; do
-  echo "$tag: $task"
+  echo "$tag: Starting $task"
   .circleci/retry.sh 3 ./gradlew $task --no-daemon
   if [[ $? == 1 ]]; then
     return_code=1
-    echo "$task failed."
+    echo "$tag: $task failed."
   else
-    echo "$task succeeded."
+    echo "$tag: $task succeeded."
   fi
 done
 exit $return_code
