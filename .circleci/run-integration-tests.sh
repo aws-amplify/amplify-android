ed29dd406d4687998347fa1496ddcf6dbc241bcc
Richard McClellan <ricmccle@amazon.com>
2020-06-26 09:48:59 -0700

    Fix logging

--- .circleci/run-integration-tests.sh
+++ .circleci/run-integration-tests.sh
@@ -6,19 +6,16 @@ tasks=($(./gradlew tasks --all | grep connectedDebugAndroidTest | cut -d " " -f 
 
 # Run the integration tests for each module.  If it fails, retry.sh
 return_code=0
-summary=""
 tag="RUN_INTEGRATION_TESTS"
 for task in "${tasks[@]}"; do
   echo "$tag: $task"
   .circleci/retry.sh 3 ./gradlew $task --no-daemon
   if [[ $? == 1 ]]; then
     return_code=1
-    summary+="$task failed.\n"
+    echo "$task failed."
   else
-    summary+="$task succeeded.\n"
+    echo "$task succeeded."
   fi
 done
-echo "$tag SUMMARY:\n"
-printf $summary
 exit $return_code
 
