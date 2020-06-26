aa84929e9565513f82d60d0c437fa154dd766201
Richard McClellan <ricmccle@amazon.com>
2020-06-26 10:39:50 -0700

    logging and comments

--- .circleci/run-integration-tests.sh
+++ .circleci/run-integration-tests.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+tag="RUN_INTEGRATION_TESTS"
+return_code=0
+
 # List all available gradle tasks, grep for the integration test tasks, and then use cut to strip the task description
-# and just return the name of the task, one for each module (e.g. aws-api:connectedDebugAndroidTest
+# and just return the name of the task, one for each module (e.g. aws-api:connectedDebugAndroidTest)
 tasks=($(./gradlew tasks --all | grep connectedDebugAndroidTest | cut -d " " -f 1))
 
-# Run the integration tests for each module.  If it fails, retry.sh
-return_code=0
-tag="RUN_INTEGRATION_TESTS"
+# Run the integration tests for each module.  Make up to 3 attempts on each module before failing.
 for task in "${tasks[@]}"; do
   echo "$tag: Starting $task"
   .circleci/retry.sh 3 ./gradlew $task --no-daemon
