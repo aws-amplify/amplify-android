f5c54c4cdf160fb77083c1bf33db3af68715efa6
eatoeric <eatoeric@amazon.com>
2020-07-09 08:21:28 -0700

    added header and navigation to dev menu

--- core/src/main/java/com/amplifyframework/core/DeveloperMenuActivity.java
+++ core/src/main/java/com/amplifyframework/core/DeveloperMenuActivity.java
@@ -19,15 +19,29 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * This is the activity to display the developer menu.
  */
 public final class DeveloperMenuActivity extends Activity {
-    // The parent layout for the developer menu.
+    // Parent layout for the developer menu.
     private View devMenuLayout;
     // Detect and handle shake events.
     private ShakeDetector detector;
+    // View containing the buttons on the main
+    // developer menu view.
+    private View buttons;
+    // Environment information view.
+    private View envInfoView;
+    // Device information view.
+    private View deviceInfoView;
+    // Logs view.
+    private View logsView;
+    // File issue view.
+    private View fileIssueView;
+    // Back button.
+    private ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +51,21 @@ public final class DeveloperMenuActivity extends Activity {
         devMenuLayout.setFocusable(true);
         devMenuLayout.setVisibility(View.GONE);
         detector = new ShakeDetector(getApplicationContext(), this::changeVisibility);
+
+        buttons = findViewById(R.id.buttons);
+        envInfoView = findViewById(R.id.env_layout);
+        deviceInfoView = findViewById(R.id.device_layout);
+        logsView = findViewById(R.id.logs_layout);
+        fileIssueView = findViewById(R.id.file_issue_layout);
+
+        backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(view -> displayMainView());
+
+        // Set on-click listeners for each button on the main view of the developer menu.
+        findViewById(R.id.env_button).setOnClickListener(view -> leaveMainView(envInfoView));
+        findViewById(R.id.device_button).setOnClickListener(view -> leaveMainView(deviceInfoView));
+        findViewById(R.id.logs_button).setOnClickListener(view -> leaveMainView(logsView));
+        findViewById(R.id.file_issue_button).setOnClickListener(view -> leaveMainView(fileIssueView));
     }
 
     @Override
@@ -63,6 +92,17 @@ public final class DeveloperMenuActivity extends Activity {
         }
     }
 
+    @Override
+    public void onBackPressed() {
+        if (devMenuLayout.getVisibility() != View.VISIBLE) {
+            super.onBackPressed();
+        } else if (backButton.getVisibility() == View.VISIBLE) {
+            displayMainView();
+        } else {
+            devMenuLayout.setVisibility(View.GONE);
+        }
+    }
+
     /**
      * If the developer menu is visible, hide the developer menu. Otherwise,
      * make the developer menu visible.
@@ -73,5 +113,27 @@ public final class DeveloperMenuActivity extends Activity {
         } else {
             devMenuLayout.setVisibility(View.VISIBLE);
         }
+    }
+
+    /**
+     * Hides the main developer menu view and displays the given View.
+     * @param newView View to display.
+     */
+    private void leaveMainView(View newView) {
+        buttons.setVisibility(View.GONE);
+        newView.setVisibility(View.VISIBLE);
+        backButton.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Hide the current view and display the main developer menu view.
+     */
+    private void displayMainView() {
+        backButton.setVisibility(View.GONE);
+        envInfoView.setVisibility(View.GONE);
+        deviceInfoView.setVisibility(View.GONE);
+        logsView.setVisibility(View.GONE);
+        fileIssueView.setVisibility(View.GONE);
+        buttons.setVisibility(View.VISIBLE);
     }
 }
