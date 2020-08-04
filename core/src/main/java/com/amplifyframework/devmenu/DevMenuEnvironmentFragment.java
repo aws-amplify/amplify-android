16e44cacdcf059ccb2f7ff7c833adaf33294b9c4
eatoeric <eatoeric@amazon.com>
2020-08-04 10:12:10 -0700

    renamed resources specific to developer menu

--- core/src/main/java/com/amplifyframework/devmenu/DevMenuEnvironmentFragment.java
+++ core/src/main/java/com/amplifyframework/devmenu/DevMenuEnvironmentFragment.java
@@ -38,6 +38,6 @@ public final class DevMenuEnvironmentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_environment, container, false);
+        return inflater.inflate(R.layout.dev_menu_fragment_environment, container, false);
     }
 }
