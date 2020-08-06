293f2ce7d73dbca68185c57ef1ce0396e3d51794
eatoeric <eatoeric@amazon.com>
2020-08-06 14:13:07 -0700

    created environment info ui

--- core/src/main/java/com/amplifyframework/devmenu/DevMenuEnvironmentFragment.java
+++ core/src/main/java/com/amplifyframework/devmenu/DevMenuEnvironmentFragment.java
@@ -15,10 +15,16 @@
 
 package com.amplifyframework.devmenu;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 import com.amplifyframework.core.R;
@@ -38,6 +44,27 @@ public final class DevMenuEnvironmentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.dev_menu_fragment_environment, container, false);
+        View envInfoView = inflater.inflate(R.layout.dev_menu_fragment_environment, container, false);
+        displayEnvironmentInfo((TextView) envInfoView.findViewById(R.id.env_info_text));
+        return envInfoView;
+    }
+
+    /**
+     * Displays the environment information on the given TextView.
+     * @param envInfoTextView the TextView that will contain the environment information text
+     */
+    private void displayEnvironmentInfo(TextView envInfoTextView) {
+        SpannableStringBuilder stringBuilder = new SpannableStringBuilder();
+        SpannableString pluginInfoTitle = new SpannableString("Amplify Plugins Information");
+        pluginInfoTitle.setSpan(new StyleSpan(Typeface.BOLD), 0, pluginInfoTitle.length(),
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        stringBuilder.append(pluginInfoTitle);
+        stringBuilder.append("\nPlugin versions will be added here...\n\n");
+        SpannableString devEnvInfoTitle = new SpannableString("Developer Environment Information");
+        devEnvInfoTitle.setSpan(new StyleSpan(Typeface.BOLD), 0, devEnvInfoTitle.length(),
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        stringBuilder.append(devEnvInfoTitle);
+        stringBuilder.append("\nDeveloper environment information will be added here...");
+        envInfoTextView.setText(stringBuilder);
     }
 }
