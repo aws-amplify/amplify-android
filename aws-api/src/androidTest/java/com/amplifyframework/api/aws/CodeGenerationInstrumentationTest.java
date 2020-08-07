be81163e240c7e9ec06af3984a5dcb7eab55c710
Raphael Kim <52714340+raphkim@users.noreply.github.com>
2020-08-07 05:44:27 -0700

    Fix broken assert statement (#705)

--- aws-api/src/androidTest/java/com/amplifyframework/api/aws/CodeGenerationInstrumentationTest.java
+++ aws-api/src/androidTest/java/com/amplifyframework/api/aws/CodeGenerationInstrumentationTest.java
@@ -201,7 +201,7 @@ public final class CodeGenerationInstrumentationTest {
                 .build(),
             Person.LAST_NAME.eq("Dandelion")
         );
-        assertTrue(errors.get(0).getMessage().contains("ConditionalCheckFailedException"));
+        assertTrue(errors.get(0).toString().contains("ConditionalCheckFailedException"));
 
         api.delete(PERSON_API_NAME, Person.justId(person.getId()));
     }
