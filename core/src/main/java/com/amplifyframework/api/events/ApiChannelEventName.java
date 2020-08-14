14a95bc2c5e0d777f9095d1bcec822dcf9c59282
Rafael Juliano <r_juliano@hotmail.com>
2020-08-14 16:12:09 -0400

    feature(datastore) Trigger network-related hub events (#716)

---
+++ core/src/main/java/com/amplifyframework/api/events/ApiChannelEventName.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amplifyframework.api.events;
+
+import com.amplifyframework.api.ApiCategory;
+import com.amplifyframework.hub.HubCategory;
+import com.amplifyframework.hub.HubChannel;
+import com.amplifyframework.hub.HubEvent;
+
+/**
+ * An enumeration of the names of events relating the the {@link ApiCategory},
+ * that are published via {@link HubCategory#publish(HubChannel, HubEvent)} on the
+ * {@link HubChannel#API} channel.
+ */
+public enum ApiChannelEventName {
+    /**
+     * Indicates that the HTTP client's ability to reach the backend API has changed. This
+     * event will be triggered when/if the HTTP client used to communicate with the
+     * API endpoint receives an event that affects its current status. For example,
+     * if the backend API becomes unreachable due to network error OR if it's currently
+     * unreachable, and the client is suddenly able to communicate with the backend.
+     * @see com.amplifyframework.api.events.ApiEndpointStatusChangeEvent
+     * @see com.amplifyframework.api.events.ApiEndpointStatusChangeEvent.ApiEndpointStatus
+     */
+    API_ENDPOINT_STATUS_CHANGED
+}
