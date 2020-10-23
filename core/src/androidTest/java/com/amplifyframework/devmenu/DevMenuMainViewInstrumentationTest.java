5b56ac7d591e531e024007298d661cda6933a2fc
Rafael Juliano <rjjulian@amazon.com>
2020-10-23 19:20:04 -0400

    Checkstyle fix

--- core/src/androidTest/java/com/amplifyframework/devmenu/DevMenuMainViewInstrumentationTest.java
+++ core/src/androidTest/java/com/amplifyframework/devmenu/DevMenuMainViewInstrumentationTest.java
@@ -60,8 +60,7 @@ public final class DevMenuMainViewInstrumentationTest {
      * Tests that the app is navigated to the environment information
      * screen when the view environment information button is pressed.
      */
-    @Test
-    @Ignore("All of these tests timeout frequently with an error about the root of the view hierarchy never taking focus")
+    @Ignore
     public void testNavigationToEnvInfoScreen() {
         testNavigationOnButtonPress(R.id.env_button, R.id.environment_fragment);
     }
@@ -70,8 +69,7 @@ public final class DevMenuMainViewInstrumentationTest {
      * Tests that the app is navigated to the device information
      * screen when the view device information button is pressed.
      */
-    @Test
-    @Ignore("All of these tests timeout frequently with an error about the root of the view hierarchy never taking focus")
+    Ignore
     public void testNavigationToDeviceInfoScreen() {
         testNavigationOnButtonPress(R.id.device_button, R.id.device_fragment);
     }
@@ -80,8 +78,7 @@ public final class DevMenuMainViewInstrumentationTest {
      * Tests that the app is navigated to the logs screen when
      * the view logs button is pressed.
      */
-    @Test
-    @Ignore("All of these tests timeout frequently with an error about the root of the view hierarchy never taking focus")
+    Ignore
     public void testNavigationToLogsScreen() {
         testNavigationOnButtonPress(R.id.logs_button, R.id.logs_fragment);
     }
@@ -90,8 +87,7 @@ public final class DevMenuMainViewInstrumentationTest {
      * Tests that the app is navigated to the file issue screen
      * when the file issue button is pressed.
      */
-    @Test
-    @Ignore("All of these tests timeout frequently with an error about the root of the view hierarchy never taking focus")
+    Ignore
     public void testNavigationToFileIssueScreen() {
         testNavigationOnButtonPress(R.id.file_issue_button, R.id.file_issue_fragment);
     }
