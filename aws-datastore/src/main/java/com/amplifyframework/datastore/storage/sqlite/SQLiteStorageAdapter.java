5c193b430b33af83a36b3e166d1b6cfe0aed7c1d
Raphael Kim <52714340+raphkim@users.noreply.github.com>
2021-01-25 16:57:31 -0800

    fix incorrectly serialized model for delete (#1131)

--- aws-datastore/src/main/java/com/amplifyframework/datastore/storage/sqlite/SQLiteStorageAdapter.java
+++ aws-datastore/src/main/java/com/amplifyframework/datastore/storage/sqlite/SQLiteStorageAdapter.java
@@ -498,7 +498,7 @@ public final class SQLiteStorageAdapter implements LocalStorageAdapter {
                     ModelSchema schema = modelSchemaRegistry.getModelSchemaForModelInstance(cascadedModel);
                     itemChangeSubject.onNext(StorageItemChange.builder()
                         .item(cascadedModel)
-                        .patchItem(SerializedModel.create(cascadedModel, modelSchema))
+                        .patchItem(SerializedModel.create(cascadedModel, schema))
                         .modelSchema(schema)
                         .type(StorageItemChange.Type.DELETE)
                         .predicate(QueryPredicates.all())
