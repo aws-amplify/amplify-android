4ba5ca1666c98f2081a30cdb383cc3469eb07931
Rafael Juliano <rjjulian@amazon.com>
2021-01-27 20:39:40 -0500

    Fix call to reporting script

--- scripts/run_test_in_devicefarm.sh
+++ scripts/run_test_in_devicefarm.sh
@@ -76,7 +76,7 @@ done
 echo "Status = $status Result = $result"
 
 echo "PR=$CODEBUILD_SOURCE_VERSION RUN_ARN=$run_arn"
-./generate_df_testrun_report --run_arn run_arn --module_name=module_name
+./scripts/generate_df_testrun_report --run_arn run_arn --module_name=module_name
 # If the result is PASSED, then exit with a return code 0
 if [ "$result" = "PASSED" ]
 then
