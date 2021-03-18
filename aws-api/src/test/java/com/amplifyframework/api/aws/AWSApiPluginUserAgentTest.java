149eea89a2539c68bd0f2342a44f9a12818a4a18
Rafael Juliano <rjjulian@amazon.com>
2021-03-18 17:14:26 -0400

    Remove shadowlog references

--- aws-api/src/test/java/com/amplifyframework/api/aws/AWSApiPluginUserAgentTest.java
+++ aws-api/src/test/java/com/amplifyframework/api/aws/AWSApiPluginUserAgentTest.java
@@ -27,7 +27,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-import org.robolectric.shadows.ShadowLog;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -65,7 +64,6 @@ public final class AWSApiPluginUserAgentTest {
      */
     @Before
     public void setUp() throws Exception {
-        ShadowLog.stream = System.out;
         // Set up backend
         server = new MockWebServer();
         server.start();
