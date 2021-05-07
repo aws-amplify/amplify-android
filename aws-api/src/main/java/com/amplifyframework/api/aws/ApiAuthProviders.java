b0a0e6d13b67ff6213d7dc70cba48ae006f22d6b
Rafael Juliano <rjjulian@amazon.com>
2021-05-07 11:19:09 -0400

    chore(api):tweaks to the api init process

--- aws-api/src/main/java/com/amplifyframework/api/aws/ApiAuthProviders.java
+++ aws-api/src/main/java/com/amplifyframework/api/aws/ApiAuthProviders.java
@@ -20,9 +20,12 @@ import androidx.annotation.NonNull;
 import com.amplifyframework.api.aws.sigv4.ApiKeyAuthProvider;
 import com.amplifyframework.api.aws.sigv4.CognitoUserPoolsAuthProvider;
 import com.amplifyframework.api.aws.sigv4.OidcAuthProvider;
+import com.amplifyframework.core.Amplify;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -72,6 +75,28 @@ public final class ApiAuthProviders {
      */
     public CognitoUserPoolsAuthProvider getCognitoUserPoolsAuthProvider() {
         return this.cognitoUserPoolsAuthProvider;
+    }
+
+    /**
+     * Returns a list of the auth providers currently configured.
+     * @param apiConfiguration A reference to the API configuration.
+     * @return a list of {@link AuthorizationType}
+     */
+    public List<AuthorizationType> getAvailableAuthorizationTypes(ApiConfiguration apiConfiguration) {
+        List<AuthorizationType> result = new ArrayList<>();
+        if (cognitoUserPoolsAuthProvider != null || Amplify.Auth.getPlugins().size() > 0) {
+            result.add(AuthorizationType.AMAZON_COGNITO_USER_POOLS);
+        }
+        if (oidcAuthProvider != null) {
+            result.add(AuthorizationType.OPENID_CONNECT);
+        }
+        if (awsCredentialsProvider != null) {
+            result.add(AuthorizationType.AWS_IAM);
+        }
+        if (apiKeyAuthProvider != null || apiConfiguration.getApiKey() != null) {
+            result.add(AuthorizationType.API_KEY);
+        }
+        return result;
     }
 
     /**
