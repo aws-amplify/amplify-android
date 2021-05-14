0ae1319333bd1a9e7b6d0c722a255427f318573f
Richard McClellan <ricmccle@amazon.com>
2021-05-14 18:02:03 -0500

    chore: fix datastore integration test timeout by using sync expressions to limit the sync (#1307)

--- testmodels/src/main/java/com/amplifyframework/testmodels/commentsblog/AmplifyModelProvider.java
+++ testmodels/src/main/java/com/amplifyframework/testmodels/commentsblog/AmplifyModelProvider.java
@@ -1,8 +1,8 @@
 package com.amplifyframework.testmodels.commentsblog;
 
+import com.amplifyframework.util.Immutable;
 import com.amplifyframework.core.model.Model;
 import com.amplifyframework.core.model.ModelProvider;
-import com.amplifyframework.util.Immutable;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -10,10 +10,10 @@ import java.util.Set;
 /** 
  *  Contains the set of model classes that implement {@link Model}
  * interface.
- **/
+ */
 
 public final class AmplifyModelProvider implements ModelProvider {
-  private static final String AMPLIFY_MODEL_VERSION = "b9b6862f30ce3aabef2a878280946a8b";
+  private static final String AMPLIFY_MODEL_VERSION = "313b773087e72b546d24c87e137d7b58";
   private static AmplifyModelProvider amplifyGeneratedModelInstance;
   private AmplifyModelProvider() {
     
@@ -30,7 +30,7 @@ public final class AmplifyModelProvider implements ModelProvider {
    * Get a set of the model classes.
    * 
    * @return a set of the model classes.
-   **/
+   */
   @Override
    public Set<Class<? extends Model>> models() {
     final Set<Class<? extends Model>> modifiableSet = new HashSet<>(
@@ -45,7 +45,7 @@ public final class AmplifyModelProvider implements ModelProvider {
    * Get the version of the models.
    * 
    * @return the version string of the models.
-   **/
+   */
   @Override
    public String version() {
     return AMPLIFY_MODEL_VERSION;
