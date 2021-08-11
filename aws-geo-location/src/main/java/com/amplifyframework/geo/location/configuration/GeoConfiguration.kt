9fe2119e266417d03db4183c4115faa6cdabb3c4
Raphael Kim <chparkkim@gmail.com>
2021-08-11 11:30:01 -0700

    feat(geo): add plugin configuration

---
+++ aws-geo-location/src/main/java/com/amplifyframework/geo/location/configuration/GeoConfiguration.kt
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amplifyframework.geo.location.configuration
+
+import com.amplifyframework.geo.location.AWSLocationGeoPlugin
+import org.json.JSONObject
+
+/**
+ * Configuration options for [AWSLocationGeoPlugin].
+ */
+data class GeoConfiguration internal constructor(
+    val region: String,
+    val maps: MapsConfiguration?
+) {
+    companion object {
+        private const val DEFAULT_REGION = "us-east-1"
+
+        /**
+         * Returns a builder object for plugin configuration.
+         * @return fresh configuration builder instance.
+         */
+        fun builder(): Builder {
+            return Builder()
+        }
+
+        /**
+         * Returns a builder object populated from JSON.
+         * @return populated builder instance.
+         */
+        internal fun fromJson(pluginJson: JSONObject): Builder {
+            return Builder(pluginJson)
+        }
+    }
+
+    /**
+     * Builder class for constructing [GeoConfiguration].
+     */
+    data class Builder internal constructor(
+        private val configJson: JSONObject? = null
+    ) {
+        private var region: String = DEFAULT_REGION
+        private var maps: MapsConfiguration? = null
+
+        init {
+            configJson?.run {
+                val regionString = optString(Config.REGION.key)
+                if (!regionString.isNullOrEmpty()) {
+                    region = regionString
+                }
+
+                optJSONObject(Config.MAPS.key)?.let {
+                    maps = MapsConfiguration.fromJson(it).build()
+                }
+            }
+        }
+
+        fun region(region: String) = apply { this.region = region }
+        fun maps(maps: MapsConfiguration) = apply { this.maps = maps }
+        fun build() = GeoConfiguration(region, maps)
+    }
+
+    private enum class Config(val key: String) {
+        /**
+         * Contains configuration for Maps APIs.
+         */
+        MAPS("maps"),
+
+        /**
+         * Amazon Location Service endpoint region.
+         */
+        REGION("region")
+    }
+}
---
+++ aws-geo-location/src/main/java/com/amplifyframework/geo/location/configuration/MapsConfiguration.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amplifyframework.geo.location.configuration
+
+import com.amplifyframework.geo.models.MapStyle
+import org.json.JSONObject
+
+/**
+ * Configuration options for specifying available map resources.
+ */
+data class MapsConfiguration internal constructor(
+    val items: List<MapStyle>,
+    val default: MapStyle
+) {
+    companion object {
+        /**
+         * Returns a builder object for maps configuration.
+         * @return fresh configuration builder instance.
+         */
+        fun builder(): Builder {
+            return Builder()
+        }
+
+        /**
+         * Returns a builder object populated from JSON.
+         * @return populated builder instance.
+         */
+        internal fun fromJson(configJson: JSONObject): Builder {
+            return Builder(configJson)
+        }
+    }
+
+    /**
+     * Builder class for constructing [MapsConfiguration].
+     */
+    data class Builder internal constructor(
+        private val configJson: JSONObject? = null
+    ) {
+        private var items: List<MapStyle> = emptyList()
+        private var default: MapStyle? = null
+
+        init {
+            configJson?.run {
+                val mapItems = getJSONObject(Config.ITEMS.key)
+                mapItems.let {
+                    val maps = ArrayList<MapStyle>()
+                    for (mapName in it.keys()) {
+                        val style = it.getString(mapName)
+                        maps.add(MapStyle(mapName, style))
+                    }
+                    items = maps
+                }
+
+                val defaultMap = optString(Config.DEFAULT.key)
+                if (!defaultMap.isNullOrEmpty()) {
+                    default = items.firstOrNull { it.mapName == defaultMap }
+                }
+            }
+        }
+
+        fun items(items: List<MapStyle>) = apply { this.items = items }
+        fun default(default: MapStyle) = apply { this.default = default }
+        fun build() = MapsConfiguration(items, default ?: items.first())
+    }
+
+    private enum class Config(val key: String) {
+        /**
+         * Contains a list of maps and their corresponding style.
+         */
+        ITEMS("items"),
+
+        /**
+         * Contains name of the default map.
+         */
+        DEFAULT("default")
+    }
+}
