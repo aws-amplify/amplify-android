a839c6b87f74c5472908de800476c423905b701c
Raphael Kim <chparkkim@gmail.com>
2021-08-30 16:37:43 -0700

    feat(geo): add geo service

--- aws-geo-location/src/main/java/com/amplifyframework/geo/location/AWSLocationGeoPlugin.kt
+++ aws-geo-location/src/main/java/com/amplifyframework/geo/location/AWSLocationGeoPlugin.kt
@@ -17,12 +17,17 @@ package com.amplifyframework.geo.location
 
 import android.content.Context
 
+import com.amazonaws.auth.AWSCredentialsProvider
 import com.amazonaws.services.geo.AmazonLocationClient
 import com.amplifyframework.AmplifyException
+import com.amplifyframework.auth.AuthCategory
+import com.amplifyframework.core.Amplify
 import com.amplifyframework.core.Consumer
 import com.amplifyframework.geo.GeoCategoryPlugin
 import com.amplifyframework.geo.GeoException
 import com.amplifyframework.geo.location.configuration.GeoConfiguration
+import com.amplifyframework.geo.location.service.AmazonLocationService
+import com.amplifyframework.geo.location.service.GeoService
 import com.amplifyframework.geo.models.MapStyle
 
 import org.json.JSONObject
@@ -31,13 +36,16 @@ import org.json.JSONObject
  * A plugin for the Geo category to interact with Amazon Location Service.
  */
 class AWSLocationGeoPlugin(
-    private val userConfiguration: GeoConfiguration? = null // for programmatically overriding amplifyconfiguration.json
+    private val userConfiguration: GeoConfiguration? = null, // for programmatically overriding amplifyconfiguration.json
+    private val authProvider: AuthCategory = Amplify.Auth
 ): GeoCategoryPlugin<AmazonLocationClient?>() {
     companion object {
         private const val GEO_PLUGIN_KEY = "awsLocationGeoPlugin"
+        private const val AUTH_PLUGIN_KEY = "awsCognitoAuthPlugin"
     }
 
-    private lateinit var pluginConfiguration: GeoConfiguration
+    private lateinit var configuration: GeoConfiguration
+    private lateinit var geoService: GeoService<AmazonLocationClient>
 
     override fun getPluginKey(): String {
         return GEO_PLUGIN_KEY
@@ -46,15 +54,16 @@ class AWSLocationGeoPlugin(
     @Throws(AmplifyException::class)
     override fun configure(pluginConfiguration: JSONObject, context: Context) {
         try {
-            this.pluginConfiguration = userConfiguration ?: GeoConfiguration.fromJson(pluginConfiguration).build()
+            this.configuration = userConfiguration ?: GeoConfiguration.fromJson(pluginConfiguration).build()
+            this.geoService = AmazonLocationService(credentialsProvider(), configuration.region)
         } catch (error: Exception) {
             throw GeoException("Failed to configure AWSLocationGeoPlugin.", error,
                 "Make sure your amplifyconfiguration.json is valid.")
         }
     }
 
-    override fun getEscapeHatch(): AmazonLocationClient? {
-        return null
+    override fun getEscapeHatch(): AmazonLocationClient {
+        return geoService.provider
     }
 
     override fun getVersion(): String {
@@ -66,7 +75,7 @@ class AWSLocationGeoPlugin(
         onError: Consumer<GeoException>
     ) {
         try {
-            onResult.accept(pluginConfiguration.maps!!.items)
+            onResult.accept(configuration.maps!!.items)
         } catch (error: Exception) {
             onError.accept(Errors.mapsError(error))
         }
@@ -77,9 +86,14 @@ class AWSLocationGeoPlugin(
         onError: Consumer<GeoException>
     ) {
         try {
-            onResult.accept(pluginConfiguration.maps!!.default)
+            onResult.accept(configuration.maps!!.default)
         } catch (error: Exception) {
             onError.accept(Errors.mapsError(error))
         }
+    }
+
+    private fun credentialsProvider(): AWSCredentialsProvider {
+        val authPlugin = authProvider.getPlugin(AUTH_PLUGIN_KEY)
+        return authPlugin.escapeHatch as AWSCredentialsProvider
     }
 }
