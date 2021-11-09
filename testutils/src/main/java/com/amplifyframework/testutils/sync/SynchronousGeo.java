6005ddd74ffffa45d37379d32c1c427f3a4aaae6
Raphael Kim <52714340+raphkim@users.noreply.github.com>
2021-11-09 14:14:01 -0800

    feat(geo): Add search APIs (#1558)
    * Add search API to geo category interface (#1522)
    
    * Add search API to geo category interface
    
    * fix checkstyle error
    
    * add todo implementation in plugin
    
    * Add configuration for search indices (#1529)
    
    * Implement Geo search API (#1534)
    
    * implement search APIs
    
    * Add configuration for search indices (#1529)
    
    * make country codes enum instead of string
    
    * address pr suggestion
    
    * move search index from category options to plugin option
    
    * Test search API (#1552)
    
    * Update aws-geo-location/src/main/java/com/amplifyframework/geo/location/service/AmazonLocationService.kt
    
    Co-authored-by: Daniel Rochetti <daniel.rochetti@gmail.com>
    
    * address PR comments
    
    * fix checkstyle and compiler errors
    
    * bump version
    
    Co-authored-by: Daniel Rochetti <daniel.rochetti@gmail.com>

--- testutils/src/main/java/com/amplifyframework/testutils/sync/SynchronousGeo.java
+++ testutils/src/main/java/com/amplifyframework/testutils/sync/SynchronousGeo.java
@@ -21,9 +21,13 @@ import com.amplifyframework.core.Amplify;
 import com.amplifyframework.geo.GeoCategory;
 import com.amplifyframework.geo.GeoCategoryBehavior;
 import com.amplifyframework.geo.GeoException;
+import com.amplifyframework.geo.models.Coordinates;
 import com.amplifyframework.geo.models.MapStyle;
 import com.amplifyframework.geo.models.MapStyleDescriptor;
+import com.amplifyframework.geo.options.GeoSearchByCoordinatesOptions;
+import com.amplifyframework.geo.options.GeoSearchByTextOptions;
 import com.amplifyframework.geo.options.GetMapStyleDescriptorOptions;
+import com.amplifyframework.geo.result.GeoSearchResult;
 import com.amplifyframework.testutils.Await;
 
 import java.util.Collection;
@@ -46,8 +50,8 @@ public final class SynchronousGeo {
      * Creates a synchronous geo wrapper which delegates calls to the provided geo
      * category behavior.
      *
-     * @param asyncDelegate Performs the actual geo operations
-     * @return A synchronous geo wrapper
+     * @param asyncDelegate Performs the actual geo operations.
+     * @return A synchronous geo wrapper.
      */
     @NonNull
     public static SynchronousGeo delegatingTo(@NonNull GeoCategoryBehavior asyncDelegate) {
@@ -58,7 +62,7 @@ public final class SynchronousGeo {
     /**
      * Creates a synchronous geo wrapper which delegates to the {@link Amplify#Geo} facade.
      *
-     * @return A synchronous geo wrapper
+     * @return A synchronous geo wrapper.
      */
     @NonNull
     public static SynchronousGeo delegatingToAmplify() {
@@ -68,7 +72,7 @@ public final class SynchronousGeo {
     /**
      * Fetches the list of available map styles from configuration.
      *
-     * @return a collection of map styles
+     * @return a collection of map styles.
      * @throws GeoException if maps are not configured.
      */
     public Collection<MapStyle> getAvailableMaps() throws GeoException {
@@ -78,7 +82,7 @@ public final class SynchronousGeo {
     /**
      * Fetches the default map style among configured map resources.
      *
-     * @return the map style object
+     * @return the map style object.
      * @throws GeoException if default map is not configured.
      */
     public MapStyle getDefaultMap() throws GeoException {
@@ -86,10 +90,10 @@ public final class SynchronousGeo {
     }
 
     /**
-     * Fetches the list of available map styles from configuration.
+     * Fetches the map style descriptor document.
      *
      * @param options options to specify name of the map.
-     * @return a collection of map styles
+     * @return a map style descriptor.
      * @throws GeoException if maps are not configured.
      */
     public MapStyleDescriptor getMapStyleDescriptor(
@@ -97,5 +101,37 @@ public final class SynchronousGeo {
     ) throws GeoException {
         return Await.<MapStyleDescriptor, GeoException>result((onResult, onError) ->
                 asyncDelegate.getMapStyleDescriptor(options, onResult, onError));
+    }
+
+    /**
+     * Fetches geocode that matches given string query.
+     *
+     * @param query string query to search.
+     * @param options options to specify search parameters.
+     * @return search result object containing a list of places.
+     * @throws GeoException if search indexes are not configured.
+     */
+    public GeoSearchResult searchByText(
+            String query,
+            GeoSearchByTextOptions options
+    ) throws GeoException {
+        return Await.<GeoSearchResult, GeoException>result((onResult, onError) ->
+                asyncDelegate.searchByText(query, options, onResult, onError));
+    }
+
+    /**
+     * Fetches geocode of provided geo-spacial coordinates.
+     *
+     * @param coordinates coordinates to reverse geocode.
+     * @param options options to specify search parameters.
+     * @return search result object containing a list of places.
+     * @throws GeoException if search indexes are not configured.
+     */
+    public GeoSearchResult searchByCoordinates(
+            Coordinates coordinates,
+            GeoSearchByCoordinatesOptions options
+    ) throws GeoException {
+        return Await.<GeoSearchResult, GeoException>result((onResult, onError) ->
+                asyncDelegate.searchByCoordinates(coordinates, options, onResult, onError));
     }
 }
