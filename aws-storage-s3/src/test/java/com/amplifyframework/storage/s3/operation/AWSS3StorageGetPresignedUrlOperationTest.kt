639a618fa17ca9e6b48c5737a603d3b45b7b83e9
Saijad Dhuka <83975678+sdhuka@users.noreply.github.com>
2022-02-17 08:45:14 -0600

    feat: Add support for custom prefix resolver (#1659)
    * feat: Add support for custom prefix resolver
    
    * fix the plugin constructor
    
    * upgrade lifecycleversion to fix lint error
    
    * address PR comments
    
    * fix builder class
    
    * fix build

---
+++ aws-storage-s3/src/test/java/com/amplifyframework/storage/s3/operation/AWSS3StorageGetPresignedUrlOperationTest.kt
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.amplifyframework.storage.s3.operation
+
+import android.util.Log
+import com.amplifyframework.core.Consumer
+import com.amplifyframework.storage.StorageAccessLevel
+import com.amplifyframework.storage.StorageException
+import com.amplifyframework.storage.s3.CognitoAuthProvider
+import com.amplifyframework.storage.s3.configuration.AWSS3PluginPrefixResolver
+import com.amplifyframework.storage.s3.configuration.AWSS3StoragePluginConfiguration
+import com.amplifyframework.storage.s3.request.AWSS3StorageGetPresignedUrlRequest
+import com.amplifyframework.storage.s3.service.StorageService
+import com.google.common.util.concurrent.MoreExecutors
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+
+public class AWSS3StorageGetPresignedUrlOperationTest {
+
+    private lateinit var awsS3StorageGetPresignedUrlOperation: AWSS3StorageGetPresignedUrlOperation
+    private lateinit var storageService: StorageService
+    private lateinit var cognitoAuthProvider: CognitoAuthProvider
+
+    @Before
+    fun setup() {
+        storageService = Mockito.spy(StorageService::class.java)
+        cognitoAuthProvider = Mockito.mock(CognitoAuthProvider::class.java)
+    }
+
+    @Test
+    fun defaultPrefixResolverAWSS3PluginConfigTest() {
+        val key = "123"
+        val expectedKey = "public/123"
+        val request = AWSS3StorageGetPresignedUrlRequest(
+            key,
+            StorageAccessLevel.PUBLIC,
+            "",
+            1
+        )
+        Mockito.`when`(cognitoAuthProvider.identityId).thenReturn("abc")
+        awsS3StorageGetPresignedUrlOperation = AWSS3StorageGetPresignedUrlOperation(
+            storageService,
+            MoreExecutors.newDirectExecutorService(),
+            cognitoAuthProvider,
+            request,
+            AWSS3StoragePluginConfiguration {},
+            {},
+            {}
+        )
+        awsS3StorageGetPresignedUrlOperation.start()
+        Mockito.verify(storageService).getPresignedUrl(expectedKey, 1)
+    }
+
+    @Test
+    fun customEmptyPrefixResolverAWSS3PluginConfigTest() {
+        val key = "123"
+        val expectedKey = "123"
+        val request = AWSS3StorageGetPresignedUrlRequest(
+            key,
+            StorageAccessLevel.PUBLIC,
+            "",
+            1
+        )
+        Mockito.`when`(cognitoAuthProvider.identityId).thenReturn("abc")
+        awsS3StorageGetPresignedUrlOperation = AWSS3StorageGetPresignedUrlOperation(
+            storageService,
+            MoreExecutors.newDirectExecutorService(),
+            cognitoAuthProvider,
+            request,
+            AWSS3StoragePluginConfiguration {
+                awsS3PluginPrefixResolver = object : AWSS3PluginPrefixResolver {
+                    override fun resolvePrefix(
+                        accessLevel: StorageAccessLevel,
+                        targetIdentity: String?,
+                        onSuccess: Consumer<String>,
+                        onError: Consumer<StorageException>
+                    ) {
+                        onSuccess.accept("")
+                    }
+                }
+            },
+            {},
+            { Log.e("TAG", "$it") }
+        )
+        awsS3StorageGetPresignedUrlOperation.start()
+        Mockito.verify(storageService).getPresignedUrl(expectedKey, 1)
+    }
+
+    @Test
+    fun customPrefixResolverAWSS3PluginConfigTest() {
+        val key = "123"
+        val expectedKey = "publicCustom/123"
+        val request = AWSS3StorageGetPresignedUrlRequest(
+            key,
+            StorageAccessLevel.PUBLIC,
+            "",
+            1
+        )
+        Mockito.`when`(cognitoAuthProvider.identityId).thenReturn("abc")
+        awsS3StorageGetPresignedUrlOperation = AWSS3StorageGetPresignedUrlOperation(
+            storageService,
+            MoreExecutors.newDirectExecutorService(),
+            cognitoAuthProvider,
+            request,
+            AWSS3StoragePluginConfiguration {
+                awsS3PluginPrefixResolver = object : AWSS3PluginPrefixResolver {
+                    override fun resolvePrefix(
+                        accessLevel: StorageAccessLevel,
+                        targetIdentity: String?,
+                        onSuccess: Consumer<String>,
+                        onError: Consumer<StorageException>
+                    ) {
+                        onSuccess.accept("publicCustom/")
+                    }
+                }
+            },
+            {},
+            { Log.e("TAG", "$it") }
+        )
+        awsS3StorageGetPresignedUrlOperation.start()
+        Mockito.verify(storageService).getPresignedUrl(expectedKey, 1)
+    }
+}
---
+++ aws-storage-s3/src/test/java/com/amplifyframework/storage/s3/operation/AWSS3StorageListOperationTest.kt
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amplifyframework.storage.s3.operation
+
+import com.amplifyframework.core.Consumer
+import com.amplifyframework.storage.StorageAccessLevel
+import com.amplifyframework.storage.StorageException
+import com.amplifyframework.storage.s3.CognitoAuthProvider
+import com.amplifyframework.storage.s3.configuration.AWSS3PluginPrefixResolver
+import com.amplifyframework.storage.s3.configuration.AWSS3StoragePluginConfiguration
+import com.amplifyframework.storage.s3.request.AWSS3StorageListRequest
+import com.amplifyframework.storage.s3.service.StorageService
+import com.google.common.util.concurrent.MoreExecutors
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+
+public class AWSS3StorageListOperationTest {
+
+    private lateinit var awsS3StorageListOperation: AWSS3StorageListOperation
+    private lateinit var storageService: StorageService
+    private lateinit var cognitoAuthProvider: CognitoAuthProvider
+
+    @Before
+    fun setup() {
+        storageService = Mockito.spy(StorageService::class.java)
+        cognitoAuthProvider = Mockito.mock(CognitoAuthProvider::class.java)
+    }
+
+    @Test
+    fun defaultPrefixResolverAWSS3PluginConfigTest() {
+        val path = ""
+        val expectedKey = "public/"
+        val request = AWSS3StorageListRequest(
+            path,
+            StorageAccessLevel.PUBLIC,
+            "",
+        )
+        Mockito.`when`(cognitoAuthProvider.identityId).thenReturn("abc")
+        awsS3StorageListOperation = AWSS3StorageListOperation(
+            storageService,
+            MoreExecutors.newDirectExecutorService(),
+            cognitoAuthProvider,
+            request,
+            AWSS3StoragePluginConfiguration {},
+            {},
+            {}
+        )
+        awsS3StorageListOperation.start()
+        Mockito.verify(storageService).listFiles(expectedKey)
+    }
+
+    @Test
+    fun customEmptyResolverAWSS3PluginConfigTest() {
+        val path = ""
+        val expectedKey = ""
+        val request = AWSS3StorageListRequest(
+            path,
+            StorageAccessLevel.PUBLIC,
+            "",
+        )
+        Mockito.`when`(cognitoAuthProvider.identityId).thenReturn("abc")
+        awsS3StorageListOperation = AWSS3StorageListOperation(
+            storageService,
+            MoreExecutors.newDirectExecutorService(),
+            cognitoAuthProvider,
+            request,
+            AWSS3StoragePluginConfiguration {
+                awsS3PluginPrefixResolver = object : AWSS3PluginPrefixResolver {
+                    override fun resolvePrefix(
+                        accessLevel: StorageAccessLevel,
+                        targetIdentity: String?,
+                        onSuccess: Consumer<String>,
+                        onError: Consumer<StorageException>
+                    ) {
+                        onSuccess.accept("")
+                    }
+                }
+            },
+            {},
+            {}
+        )
+        awsS3StorageListOperation.start()
+        Mockito.verify(storageService).listFiles(expectedKey)
+    }
+
+    @Test
+    fun customResolverAWSS3PluginConfigTest() {
+        val path = ""
+        val expectedKey = "publicCustom/"
+        val request = AWSS3StorageListRequest(
+            path,
+            StorageAccessLevel.PUBLIC,
+            "",
+        )
+        Mockito.`when`(cognitoAuthProvider.identityId).thenReturn("abc")
+        awsS3StorageListOperation = AWSS3StorageListOperation(
+            storageService,
+            MoreExecutors.newDirectExecutorService(),
+            cognitoAuthProvider,
+            request,
+            AWSS3StoragePluginConfiguration {
+                awsS3PluginPrefixResolver = object : AWSS3PluginPrefixResolver {
+                    override fun resolvePrefix(
+                        accessLevel: StorageAccessLevel,
+                        targetIdentity: String?,
+                        onSuccess: Consumer<String>,
+                        onError: Consumer<StorageException>
+                    ) {
+                        onSuccess.accept("publicCustom/")
+                    }
+                }
+            },
+            {},
+            {}
+        )
+        awsS3StorageListOperation.start()
+        Mockito.verify(storageService).listFiles(expectedKey)
+    }
+}
