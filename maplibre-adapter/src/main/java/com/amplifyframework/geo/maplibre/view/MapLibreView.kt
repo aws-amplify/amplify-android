aac215edc915ecf0913fa35652caf0f32e9d8ebe
Erica Eaton <eatoeric@amazon.com>
2022-04-04 12:08:09 -0400

    add setClusterBehavior API

--- maplibre-adapter/src/main/java/com/amplifyframework/geo/maplibre/view/MapLibreView.kt
+++ maplibre-adapter/src/main/java/com/amplifyframework/geo/maplibre/view/MapLibreView.kt
@@ -18,6 +18,7 @@ package com.amplifyframework.geo.maplibre.view
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.util.AttributeSet
 import android.view.Gravity
 import androidx.annotation.UiThread
@@ -29,10 +30,18 @@ import com.amplifyframework.geo.maplibre.AmplifyMapLibreAdapter
 import com.amplifyframework.geo.maplibre.R
 import com.amplifyframework.geo.maplibre.view.support.AttributionInfoView
 import com.amplifyframework.geo.models.MapStyle
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.style.expressions.Expression
+import com.mapbox.mapboxsdk.style.layers.CircleLayer
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer
+import com.mapbox.mapboxsdk.style.sources.GeoJsonOptions
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
+import java.lang.Double.min
 
 
 typealias MapLibreOptions = com.mapbox.mapboxsdk.maps.MapboxMapOptions
@@ -59,6 +68,9 @@ class MapLibreView
     companion object {
         private val log = Amplify.Logging.forNamespace("amplify:maplibre-adapter")
 
+        private const val CLUSTER_CIRCLE_LAYER_ID = "cluster-circles"
+        private const val CLUSTER_NUMBER_LAYER_ID = "cluster-numbers"
+
         const val PLACE_ICON_NAME = "place"
         const val PLACE_ACTIVE_ICON_NAME = "place-active"
     }
@@ -75,6 +87,9 @@ class MapLibreView
 
     var defaultPlaceIcon = R.drawable.place
     var defaultPlaceActiveIcon = R.drawable.place_active
+
+    private var shouldCluster = true
+    private var mapStyle: MapStyle? = null
 
     init {
         setup(context, options)
@@ -111,7 +126,7 @@ class MapLibreView
     }
 
     /**
-     * Get the both the map and its style asynchronously.
+     * Get both the map and its style asynchronously.
      *
      * **Implementation notes:** This is a shortcut to the existing nested callback solution:
      *
@@ -134,7 +149,7 @@ class MapLibreView
     }
 
     /**
-     * Update the map using the passed style. If no style is style is set, the default
+     * Update the map using the passed style. If no style is set, the default
      * configured using the Amplify CLI is used.
      *
      * @param style the map style object
@@ -143,6 +158,7 @@ class MapLibreView
     fun setStyle(style: MapStyle? = null, callback: Style.OnStyleLoaded) {
         getMapAsync { map ->
             adapter.setStyle(map, style) {
+                mapStyle = style
                 // setup the symbol manager
                 it.apply {
                     addImage(
@@ -154,13 +170,93 @@ class MapLibreView
                         BitmapFactory.decodeResource(resources, defaultPlaceActiveIcon)
                     )
                 }
-                this.symbolManager = SymbolManager(this, map, it, null, null).apply {
-                    iconAllowOverlap = true
-                    iconIgnorePlacement = true
+
+                // Clear the current symbols from the map since a new SymbolManager will be created
+                if (this::symbolManager.isInitialized) {
+                    this.symbolManager.deleteAll()
+                }
+
+                removeClusterLayers(it)
+                if (shouldCluster) {
+                    enableClustering(map, it)
+                } else {
+                    this.symbolManager = SymbolManager(this, map, it, null, null).apply {
+                        iconAllowOverlap = true
+                        iconIgnorePlacement = true
+                    }
                 }
 
                 callback.onStyleLoaded(it)
             }
+        }
+    }
+
+    // TODO : add a Javadoc comment here
+    fun setClusterBehavior(shouldCluster: Boolean, callback: () -> Unit) {
+        this.shouldCluster = shouldCluster
+        setStyle(mapStyle) {
+            callback()
+        }
+    }
+
+    private fun removeClusterLayers(style: Style) {
+        style.removeLayer(CLUSTER_CIRCLE_LAYER_ID)
+        style.removeLayer(CLUSTER_NUMBER_LAYER_ID)
+    }
+
+    private fun enableClustering(map: MapboxMap, style: Style) {
+        val clusterMaxZoom = 13
+        val geoJsonClusterOptions = GeoJsonOptions().withCluster(true).withClusterMaxZoom(clusterMaxZoom).withClusterRadius(75)
+        this.symbolManager = SymbolManager(this, map, style, null, geoJsonClusterOptions).apply {
+            iconAllowOverlap = true
+            iconIgnorePlacement = true
+        }
+
+        val geoJsonSources = style.sources.filterIsInstance<GeoJsonSource>()
+        var geoJsonSourceId = geoJsonSources[0].id
+        if (geoJsonSources.size > 1) {
+            geoJsonSourceId = geoJsonSources[1].id
+        }
+
+        // Create a circle layer for cluster circles
+        val clusterCircleLayer = CircleLayer(CLUSTER_CIRCLE_LAYER_ID, geoJsonSourceId)
+        val circleRadiusExpression = Expression.interpolate(Expression.exponential(1.75), Expression.zoom(),
+            Expression.stop(map.minZoomLevel, 60), Expression.stop(clusterMaxZoom, 20))
+        val circleColorStops = arrayOf(Expression.stop(15, Expression.rgb(0, 255, 0)),
+            Expression.stop(30, Expression.rgb(255, 0, 0)))
+        val circleColorExpression = Expression.step(Expression.get("point_count"),
+            Expression.rgb(0, 0, 255), *circleColorStops)
+        clusterCircleLayer.setProperties(
+            PropertyFactory.circleColor(circleColorExpression),
+            PropertyFactory.circleRadius(circleRadiusExpression)
+        )
+        clusterCircleLayer.setFilter(Expression.has("point_count"))
+
+        // Create a symbol layer for cluster numbers (point count)
+        val clusterNumberLayer = SymbolLayer(CLUSTER_NUMBER_LAYER_ID, geoJsonSourceId)
+        clusterNumberLayer.setProperties(
+            PropertyFactory.textField(Expression.toString(Expression.get("point_count"))),
+            PropertyFactory.textFont(arrayOf("Arial Bold")),
+            PropertyFactory.textColor(Color.WHITE),
+            PropertyFactory.textIgnorePlacement(true),
+            PropertyFactory.textAllowOverlap(true)
+        )
+
+        style.apply {
+            addLayer(clusterCircleLayer)
+            addLayer(clusterNumberLayer)
+        }
+
+        // Set the behavior when a cluster is clicked
+        map.addOnMapClickListener { latLngPoint ->
+            val pointClicked = map.projection.toScreenLocation(latLngPoint)
+            val features = map.queryRenderedFeatures(pointClicked, "cluster-circles")
+            if (features.isNotEmpty()) {
+                // Center the cluster that was clicked within the map view and zoom in
+                val zoomLevel = min(map.maxZoomLevel, map.cameraPosition.zoom + 1)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLngPoint, zoomLevel))
+            }
+            true
         }
     }
 
