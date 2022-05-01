dc95ae572e4a8f9cbba8e1c61afed9dd6c4912c3
poojamat <poojamat@gmail.com>
2022-04-30 18:34:38 -0700

    Accidental change.

--- core/src/main/java/com/amplifyframework/core/model/SerializedModel.java
+++ core/src/main/java/com/amplifyframework/core/model/SerializedModel.java
@@ -244,7 +244,7 @@ public final class SerializedModel implements Model {
      * Gets the name of the model as known in the hybrid platform, e.g. "Post".
      * @return Name of the model in the hybrid platform.
      */
-    @NonNull
+    @Nullable
     @Override
     public String getModelName() {
         return modelSchema == null ? null : modelSchema.getName();
