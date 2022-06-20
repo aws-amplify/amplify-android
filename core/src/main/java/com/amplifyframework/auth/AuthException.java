7f14a626d3873aacb3972bd1a9c1194edec4acc6
Banji Jolaoso <banjij@amazon.com>
2022-06-20 08:14:29 -0700

    added implementation for updatepassword

--- core/src/main/java/com/amplifyframework/auth/AuthException.java
+++ core/src/main/java/com/amplifyframework/auth/AuthException.java
@@ -270,6 +270,27 @@ public class AuthException extends AmplifyException {
         }
     }
 
+    public static class UpdatePasswordException extends AuthException {
+        private static final long serialVersionUID = 1L;
+        private static final String MESSAGE = "Your password cannot be updated.";
+        private static final String RECOVERY_SUGGESTION = "Please sign in and reattempt the operation.";
+
+        /**
+         * Default message/recovery suggestion without a cause.
+         */
+        public UpdatePasswordException() {
+            super(MESSAGE, RECOVERY_SUGGESTION);
+        }
+
+        /**
+         * Default message/recovery suggestion with a cause.
+         * @param cause The original error.
+         */
+        public UpdatePasswordException(Throwable cause) {
+            super(MESSAGE, cause, RECOVERY_SUGGESTION);
+        }
+    }
+
     /**
      * Could not perform the action because user was not found in the system.
      */
