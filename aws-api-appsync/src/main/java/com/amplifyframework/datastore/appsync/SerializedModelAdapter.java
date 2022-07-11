c2a07b8b956bbd8973ab70a4be691fc6860fce5a
poojamat <poojamat@gmail.com>
2022-07-11 09:39:33 -0700

    Checkstyle fixes.

--- aws-api-appsync/src/main/java/com/amplifyframework/datastore/appsync/SerializedModelAdapter.java
+++ aws-api-appsync/src/main/java/com/amplifyframework/datastore/appsync/SerializedModelAdapter.java
@@ -28,13 +28,11 @@ import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
-import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
