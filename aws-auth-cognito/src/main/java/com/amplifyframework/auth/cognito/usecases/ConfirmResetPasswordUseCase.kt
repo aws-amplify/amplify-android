e3265ec777542ae114b11bf0e2ae48786c6c3f28
Sunil Timalsina <sunil.timalsina@gmail.com>
2022-07-25 11:41:13 -0700

    feat(auth): add confirm reset password use case

--- aws-auth-cognito/src/main/java/com/amplifyframework/auth/cognito/usecases/ConfirmResetPasswordUseCase.kt
+++ aws-auth-cognito/src/main/java/com/amplifyframework/auth/cognito/usecases/ConfirmResetPasswordUseCase.kt
@@ -1,28 +1,13 @@
-/*
- * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
 package com.amplifyframework.auth.cognito.usecases
 
 import aws.sdk.kotlin.services.cognitoidentityprovider.CognitoIdentityProviderClient
 import com.amplifyframework.auth.AuthException
 import com.amplifyframework.auth.AuthException.REPORT_BUG_TO_AWS_SUGGESTION
 import com.amplifyframework.auth.cognito.CognitoAuthExceptionConverter
-import com.amplifyframework.auth.options.AWSCognitoAuthConfirmResetPasswordOptions
 import com.amplifyframework.auth.options.AuthConfirmResetPasswordOptions
 import com.amplifyframework.core.Action
 import com.amplifyframework.core.Consumer
+import java.lang.Exception
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -61,3 +46,10 @@ internal class ConfirmResetPasswordUseCase(
         }
     }
 }
+
+/**
+ * Cognito extension of confirm reset password options to add the platform specific fields.
+ */
+data class AWSCognitoAuthConfirmResetPasswordOptions(
+    val metadata: Map<String, String>
+) : AuthConfirmResetPasswordOptions()
