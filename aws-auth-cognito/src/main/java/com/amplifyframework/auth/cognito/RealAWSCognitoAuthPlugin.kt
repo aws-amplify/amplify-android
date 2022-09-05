f1a535ab67de43ca677610de4b58015a9330a034
Sunil Timalsina <sunil.timalsina@gmail.com>
2022-09-04 22:22:01 -0700

    feat(auth): publish hub events for auth

--- aws-auth-cognito/src/main/java/com/amplifyframework/auth/cognito/RealAWSCognitoAuthPlugin.kt
+++ aws-auth-cognito/src/main/java/com/amplifyframework/auth/cognito/RealAWSCognitoAuthPlugin.kt
@@ -95,6 +95,7 @@ import com.amplifyframework.statemachine.codegen.states.SRPSignInState
 import com.amplifyframework.statemachine.codegen.states.SignInChallengeState
 import com.amplifyframework.statemachine.codegen.states.SignInState
 import com.amplifyframework.statemachine.codegen.states.SignOutState
+import java.util.concurrent.atomic.AtomicReference
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
@@ -111,6 +112,8 @@ internal class RealAWSCognitoAuthPlugin(
     private val logger: Logger
 
 ) : AuthCategoryBehavior {
+
+    private val lastPublishedHubEventName = AtomicReference<AuthChannelEventName> ()
 
     init {
         addAuthStateChangeListener()
@@ -1335,7 +1338,7 @@ internal class RealAWSCognitoAuthPlugin(
             { authState ->
                 logger.verbose("Auth State Change: $authState")
 
-                // TODO: listen and dispatch hub events
+                dispatchHubEvent(authState)
 
                 when (authState) {
                     is AuthState.WaitingForCachedCredentials -> credentialStoreStateMachine.send(
@@ -1374,6 +1377,20 @@ internal class RealAWSCognitoAuthPlugin(
             },
             null
         )
+    }
+
+    private fun dispatchHubEvent(authState: AuthState) {
+        when {
+            authState.authNState is AuthenticationState.SignedIn -> AuthChannelEventName.SIGNED_IN
+            authState.authNState is AuthenticationState.SignedOut -> AuthChannelEventName.SIGNED_OUT
+            authState.authZState?.deleteUserState is DeleteUserState.UserDeleted -> AuthChannelEventName.USER_DELETED
+            else -> null
+        }?.takeUnless {
+            it != lastPublishedHubEventName.get()
+        }?.let { eventName ->
+            lastPublishedHubEventName.set(eventName)
+            Amplify.Hub.publish(HubChannel.AUTH, HubEvent.create(eventName))
+        }
     }
 
     private fun configureAuthStates() {
