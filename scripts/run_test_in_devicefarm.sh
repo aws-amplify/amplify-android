2c92551926276f35fe00cd05e2c60218c83d3b79
Saijad Dhuka <sdhuka@amazon.com>
2022-10-09 14:59:15 -0500

    increase pool to 3

--- scripts/run_test_in_devicefarm.sh
+++ scripts/run_test_in_devicefarm.sh
@@ -105,7 +105,7 @@ stopDuplicates
 #   +-+-+ +-+-+-+ +-+-+-+-+-+ +-+-+ +-+-+-+-+
 #   |D|o| |n|o|t| |m|e|r|g|e| |t|o| |m|a|i|n|
 #   +-+-+ +-+-+-+ +-+-+-+-+-+ +-+-+ +-+-+-+-+
-max_devices=1
+max_devices=3
 
 # Schedule the test run in device farm
 echo "Scheduling test run"
