e920773ec97e4ce3d3f7d5e12454a198d8e0c7c8
gpanshu <91897496+gpanshu@users.noreply.github.com>
2022-10-12 14:16:14 -0500

    Fix(Auth): Fix for error propagation when sign in fails inside Sign In Custom Auth Action (#2022)
    * Fix for error propagation when sign in fails inside Sign In Custom Auth Action
    
    * Pr comments resolution
    
    * Fix for when the user is already authenticated

--- aws-auth-cognito/src/main/java/com/amplifyframework/auth/cognito/actions/SignInCustomActions.kt
+++ aws-auth-cognito/src/main/java/com/amplifyframework/auth/cognito/actions/SignInCustomActions.kt
@@ -27,6 +27,7 @@ import com.amplifyframework.statemachine.codegen.actions.CustomSignInActions
 import com.amplifyframework.statemachine.codegen.data.SignInMethod
 import com.amplifyframework.statemachine.codegen.events.AuthenticationEvent
 import com.amplifyframework.statemachine.codegen.events.CustomSignInEvent
+import com.amplifyframework.statemachine.codegen.events.SignInEvent
 
 object SignInCustomActions : CustomSignInActions {
     private const val KEY_SECRET_HASH = "SECRET_HASH"
@@ -74,7 +75,11 @@ object SignInCustomActions : CustomSignInActions {
                     )
                 }
             } catch (e: Exception) {
-                AuthenticationEvent(AuthenticationEvent.EventType.CancelSignIn(error = e))
+                val errorEvent = SignInEvent(SignInEvent.EventType.ThrowError(e))
+                logger.verbose("$id Sending event ${errorEvent.type}")
+                dispatcher.send(errorEvent)
+
+                AuthenticationEvent(AuthenticationEvent.EventType.CancelSignIn())
             }
             logger.verbose("$id Sending event ${evt.type}")
             dispatcher.send(evt)
