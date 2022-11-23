41da13967178a51ebeac25bc4f10fa322c4d1611
Divyesh Chitroda <div5yesh@gmail.com>
2022-11-22 23:39:59 -0800

    add large image support

--- aws-push-notifications-pinpoint-utils/src/main/java/com/amplifyframework/pushnotifications/pinpoint/utils/AWSPinpointPushNotificationUtils.kt
+++ aws-push-notifications-pinpoint-utils/src/main/java/com/amplifyframework/pushnotifications/pinpoint/utils/AWSPinpointPushNotificationUtils.kt
@@ -1,32 +1,48 @@
+/*
+ * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package com.amplifyframework.pushnotifications.pinpoint.utils
 
 import android.annotation.SuppressLint
 import android.app.ActivityManager
 import android.app.ActivityManager.RunningAppProcessInfo
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Build
 import androidx.annotation.ChecksSdkIntAtLeast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import com.amplifyframework.notifications.pushnotifications.PushNotificationsDetails
+import java.net.URL
 import kotlin.random.Random
 
 class AWSPinpointPushNotificationUtils(private val context: Context) {
 
     private val channel: NotificationChannel? by lazy {
-        createNotificationChannel()
+        // create before notification trigger for API 32 or lower
+        createDefaultNotificationChannel()
     }
 
     @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.O)
     private val isNotificationChannelSupported = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
 
     @SuppressLint("NewApi")
-    private fun createNotificationChannel(): NotificationChannel? {
+    private fun createDefaultNotificationChannel(): NotificationChannel? {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (isNotificationChannelSupported) {
@@ -45,6 +61,10 @@ class AWSPinpointPushNotificationUtils(private val context: Context) {
         return null
     }
 
+    private fun downloadImage(url: String): Bitmap? {
+        return BitmapFactory.decodeStream(URL(url).openConnection().getInputStream())
+    }
+
     fun isAppInForeground(): Boolean {
         // Gets a list of running processes.
         val am = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
@@ -60,6 +80,11 @@ class AWSPinpointPushNotificationUtils(private val context: Context) {
             }
         }
         return false
+    }
+
+    fun areNotificationsEnabled(): Boolean {
+        // check for app level opt out
+        return NotificationManagerCompat.from(context).areNotificationsEnabled()
     }
 
     @SuppressLint("NewApi")
@@ -84,12 +109,15 @@ class AWSPinpointPushNotificationUtils(private val context: Context) {
             NotificationCompat.Builder(context)
         }
 
+        val largeImageIcon = details.imageUrl?.let { downloadImage(it) }
+
         builder.apply {
             setContentTitle(title)
             setContentText(body)
             setSmallIcon(R.drawable.ic_launcher_foreground)
             setContentIntent(intent)
             setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            setLargeIcon(largeImageIcon)
         }
 
         with(NotificationManagerCompat.from(context)) {
