a4e77111892997684466079d0bce5eefe6dca472
Saijad Dhuka <sdhuka@amazon.com>
2022-12-07 14:22:28 -0600

    fix: usermeta was persisted as empty in the database

--- aws-storage-s3/src/main/java/com/amplifyframework/storage/s3/transfer/TransferDB.kt
+++ aws-storage-s3/src/main/java/com/amplifyframework/storage/s3/transfer/TransferDB.kt
@@ -749,19 +749,26 @@ internal class TransferDB private constructor(context: Context) {
         val values = ContentValues()
         metadata?.let {
             values.apply {
-                TransferTable.COLUMN_USER_METADATA to JsonUtils.mapToString(it.userMetadata)
-                TransferTable.COLUMN_HEADER_CONTENT_TYPE to it.metaData[ObjectMetadata.CONTENT_TYPE].toString()
-                TransferTable.COLUMN_HEADER_CONTENT_ENCODING to it.metaData[ObjectMetadata.CONTENT_ENCODING].toString()
-                TransferTable.COLUMN_HEADER_CACHE_CONTROL to it.metaData[ObjectMetadata.CACHE_CONTROL].toString()
-                TransferTable.COLUMN_CONTENT_MD5 to it.metaData[ObjectMetadata.CONTENT_MD5].toString()
-                TransferTable.COLUMN_HEADER_CONTENT_DISPOSITION to
+                put(TransferTable.COLUMN_USER_METADATA, JsonUtils.mapToString(it.userMetadata))
+                put(TransferTable.COLUMN_HEADER_CONTENT_TYPE, it.metaData[ObjectMetadata.CONTENT_TYPE].toString())
+                put(
+                    TransferTable.COLUMN_HEADER_CONTENT_ENCODING,
+                    it.metaData[ObjectMetadata.CONTENT_ENCODING].toString()
+                )
+                put(TransferTable.COLUMN_HEADER_CACHE_CONTROL, it.metaData[ObjectMetadata.CACHE_CONTROL].toString())
+                put(TransferTable.COLUMN_CONTENT_MD5, it.metaData[ObjectMetadata.CONTENT_MD5].toString())
+                put(
+                    TransferTable.COLUMN_HEADER_CONTENT_DISPOSITION,
                     it.metaData[ObjectMetadata.CONTENT_DISPOSITION].toString()
-                TransferTable.COLUMN_SSE_ALGORITHM to it.metaData[ObjectMetadata.SERVER_SIDE_ENCRYPTION].toString()
-                TransferTable.COLUMN_SSE_KMS_KEY to
+                )
+                put(TransferTable.COLUMN_SSE_ALGORITHM, it.metaData[ObjectMetadata.SERVER_SIDE_ENCRYPTION].toString())
+                put(
+                    TransferTable.COLUMN_SSE_KMS_KEY,
                     it.metaData[ObjectMetadata.SERVER_SIDE_ENCRYPTION_KMS_KEY_ID].toString()
-                TransferTable.COLUMN_EXPIRATION_TIME_RULE_ID to it.expirationTimeRuleId
-                TransferTable.COLUMN_HTTP_EXPIRES_DATE to it.httpExpiresDate?.time.toString()
-                TransferTable.COLUMN_HEADER_STORAGE_CLASS to it.metaData[ObjectMetadata.STORAGE_CLASS].toString()
+                )
+                put(TransferTable.COLUMN_EXPIRATION_TIME_RULE_ID, it.expirationTimeRuleId)
+                put(TransferTable.COLUMN_HTTP_EXPIRES_DATE, it.httpExpiresDate?.time)
+                put(TransferTable.COLUMN_HEADER_STORAGE_CLASS, it.metaData[ObjectMetadata.STORAGE_CLASS].toString())
             }
         }
         return values
