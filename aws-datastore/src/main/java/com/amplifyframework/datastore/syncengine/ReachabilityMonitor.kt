5477469f3c58e453c5e1aa50a42ef70954ff1398
Tyler Roach <tjroach@amazon.com>
2023-06-01 17:08:42 -0400

    Fix reachability debouncer causing delay on local inserts

--- aws-datastore/src/main/java/com/amplifyframework/datastore/syncengine/ReachabilityMonitor.kt
+++ aws-datastore/src/main/java/com/amplifyframework/datastore/syncengine/ReachabilityMonitor.kt
@@ -62,26 +62,29 @@ private class ReachabilityMonitorImpl constructor(val schedulerProvider: Schedul
 
     override fun configure(context: Context, connectivityProvider: ConnectivityProvider) {
         this.connectivityProvider = connectivityProvider
-        connectivityProvider.registerDefaultNetworkCallback(
-            context,
-            object : NetworkCallback() {
-                override fun onAvailable(network: Network) {
-                    subject.onNext(true)
-                }
+        val observable = Observable.create { emitter ->
+            connectivityProvider.registerDefaultNetworkCallback(
+                context,
+                object : NetworkCallback() {
+                    override fun onAvailable(network: Network) {
+                        emitter.onNext(true)
+                    }
 
-                override fun onLost(network: Network) {
-                    subject.onNext(false)
+                    override fun onLost(network: Network) {
+                        emitter.onNext(false)
+                    }
                 }
-            }
-        )
+            )
+            emitter.onNext(connectivityProvider.hasActiveNetwork)
+        }
+        observable.debounce(250, TimeUnit.MILLISECONDS)
+            .distinctUntilChanged()
+            .subscribe(subject)
     }
 
     override fun getObservable(): Observable<Boolean> {
-        connectivityProvider?.let { connectivityProvider ->
+        connectivityProvider?.let {
             return subject.subscribeOn(schedulerProvider.io())
-                .doOnSubscribe { subject.onNext(connectivityProvider.hasActiveNetwork) }
-                .debounce(250, TimeUnit.MILLISECONDS, schedulerProvider.computation())
-                .distinctUntilChanged()
         } ?: run {
             throw DataStoreException(
                 "ReachabilityMonitor has not been configured.",
