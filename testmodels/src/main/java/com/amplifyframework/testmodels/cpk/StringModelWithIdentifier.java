c54555257b0e41e1658d359848d03b6bdef4300b
Tyler Roach <tjroach@amazon.com>
2023-06-14 15:52:34 -0400

    Update ModelIdentifier to properly return getIdentifier() string for newly proposed ModelIdentifier codegen changes

---
+++ testmodels/src/main/java/com/amplifyframework/testmodels/cpk/StringModelWithIdentifier.java
@@ -0,0 +1,139 @@
+package com.amplifyframework.testmodels.cpk;
+
+import static com.amplifyframework.core.model.query.predicate.QueryField.field;
+
+import androidx.core.util.ObjectsCompat;
+
+import com.amplifyframework.core.model.Model;
+import com.amplifyframework.core.model.ModelIdentifier;
+import com.amplifyframework.core.model.annotations.Index;
+import com.amplifyframework.core.model.annotations.ModelConfig;
+import com.amplifyframework.core.model.annotations.ModelField;
+import com.amplifyframework.core.model.query.predicate.QueryField;
+import com.amplifyframework.core.model.temporal.Temporal;
+
+import java.util.Objects;
+
+/** This is an auto generated class representing the StringModelWithIdentifier type in your schema. */
+@SuppressWarnings("all")
+@ModelConfig(pluralName = "StringModelWithIdentifiers", type = Model.Type.USER, version = 1)
+@Index(name = "undefined", fields = {"customKey"})
+public final class StringModelWithIdentifier implements Model {
+  public static final QueryField CUSTOM_KEY = field("StringModelWithIdentifier", "customKey");
+  private final @ModelField(targetType="String", isRequired = true) String customKey;
+  private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime createdAt;
+  private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime updatedAt;
+  private StringModelWithIdentifierIdentifier stringModelWithIdentifierIdentifier;
+  public StringModelWithIdentifierIdentifier resolveIdentifier() {
+    if (stringModelWithIdentifierIdentifier == null) {
+      this.stringModelWithIdentifierIdentifier = new StringModelWithIdentifierIdentifier(customKey);
+    }
+    return stringModelWithIdentifierIdentifier;
+  }
+  
+  public String getCustomKey() {
+      return customKey;
+  }
+  
+  public Temporal.DateTime getCreatedAt() {
+      return createdAt;
+  }
+  
+  public Temporal.DateTime getUpdatedAt() {
+      return updatedAt;
+  }
+  
+  private StringModelWithIdentifier(String customKey) {
+    this.customKey = customKey;
+  }
+  
+  @Override
+   public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      } else if(obj == null || getClass() != obj.getClass()) {
+        return false;
+      } else {
+      StringModelWithIdentifier stringModelWithIdentifier = (StringModelWithIdentifier) obj;
+      return ObjectsCompat.equals(getCustomKey(), stringModelWithIdentifier.getCustomKey()) &&
+              ObjectsCompat.equals(getCreatedAt(), stringModelWithIdentifier.getCreatedAt()) &&
+              ObjectsCompat.equals(getUpdatedAt(), stringModelWithIdentifier.getUpdatedAt());
+      }
+  }
+  
+  @Override
+   public int hashCode() {
+    return new StringBuilder()
+      .append(getCustomKey())
+      .append(getCreatedAt())
+      .append(getUpdatedAt())
+      .toString()
+      .hashCode();
+  }
+  
+  @Override
+   public String toString() {
+    return new StringBuilder()
+      .append("StringModelWithIdentifier {")
+      .append("customKey=" + String.valueOf(getCustomKey()) + ", ")
+      .append("createdAt=" + String.valueOf(getCreatedAt()) + ", ")
+      .append("updatedAt=" + String.valueOf(getUpdatedAt()))
+      .append("}")
+      .toString();
+  }
+  
+  public static CustomKeyStep builder() {
+      return new Builder();
+  }
+  
+  public CopyOfBuilder copyOfBuilder() {
+    return new CopyOfBuilder(customKey);
+  }
+  public interface CustomKeyStep {
+    BuildStep customKey(String customKey);
+  }
+  
+
+  public interface BuildStep {
+    StringModelWithIdentifier build();
+  }
+  
+
+  public static class Builder implements CustomKeyStep, BuildStep {
+    private String customKey;
+    @Override
+     public StringModelWithIdentifier build() {
+        
+        return new StringModelWithIdentifier(
+          customKey);
+    }
+    
+    @Override
+     public BuildStep customKey(String customKey) {
+        Objects.requireNonNull(customKey);
+        this.customKey = customKey;
+        return this;
+    }
+  }
+  
+
+  public final class CopyOfBuilder extends Builder {
+    private CopyOfBuilder(String customKey) {
+      super.customKey(customKey);
+    }
+    
+    @Override
+     public CopyOfBuilder customKey(String customKey) {
+      return (CopyOfBuilder) super.customKey(customKey);
+    }
+  }
+  
+
+  public static class StringModelWithIdentifierIdentifier extends ModelIdentifier<StringModelWithIdentifier> {
+    private static final long serialVersionUID = 1L;
+    public StringModelWithIdentifierIdentifier(String customKey) {
+      super(customKey);
+    }
+  }
+  
+}
