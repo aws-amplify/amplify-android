0cb41505b933069ce75b1f111ddd1e562869a27d
tjroach <tjroach@amazon.com>
2023-09-21 10:02:36 -0400

    fix tests

--- core/src/main/java/com/amplifyframework/core/model/SchemaRegistryUtils.kt
+++ core/src/main/java/com/amplifyframework/core/model/SchemaRegistryUtils.kt
@@ -2,6 +2,7 @@ package com.amplifyframework.core.model
 
 import com.amplifyframework.core.model.annotations.ModelConfig
 import com.amplifyframework.datastore.DataStoreException.IrRecoverableException
+import java.lang.NullPointerException
 
 internal object SchemaRegistryUtils {
 
@@ -27,13 +28,23 @@ internal object SchemaRegistryUtils {
         modelName: String,
         modelSchema: ModelSchema
     ) {
-        if (modelSchema.modelClass.getAnnotation(ModelConfig::class.java)?.hasLazySupport == true) {
-            throw IrRecoverableException(
-                "Unsupported model type. Lazy model types are not yet supported on DataStore.",
-                "Regenerate models with generatemodelsforlazyloadandcustomselectionset=false."
-            )
-        } else {
-            modelSchemaMap[modelName] = modelSchema
+
+        try {
+            if (modelSchema.modelClass.getAnnotation(ModelConfig::class.java)?.hasLazySupport == true) {
+                throw IrRecoverableException(
+                    "Unsupported model type. Lazy model types are not yet supported on DataStore.",
+                    "Regenerate models with generatemodelsforlazyloadandcustomselectionset=false."
+                )
+            }
+        } catch (npe: NullPointerException) {
+            /*
+            modelSchema.modelClass could throw if modelClass was not set from builder.
+            This is likely not a valid scenario, as modelClass should be required, but
+            we have a number of test cases that don't provide on. Since the builder is public and
+            modelClass isn't a mandatory builder param, we add this block for additional safety.
+            */
         }
+
+        modelSchemaMap[modelName] = modelSchema
     }
 }
