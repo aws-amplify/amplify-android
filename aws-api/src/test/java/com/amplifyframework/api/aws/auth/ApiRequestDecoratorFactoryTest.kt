8211da833c1bfdb58c7234c99b5b116ea2447355
tjroach <tjroach@amazon.com>
2023-12-29 10:42:04 -0500

    Convert file from Java to Kotlin

--- aws-api/src/test/java/com/amplifyframework/api/aws/auth/ApiRequestDecoratorFactoryTest.kt
+++ aws-api/src/test/java/com/amplifyframework/api/aws/auth/ApiRequestDecoratorFactoryTest.kt
@@ -12,69 +12,65 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+package com.amplifyframework.api.aws.auth
 
-package com.amplifyframework.api.aws.auth;
-
-import com.amplifyframework.api.ApiException;
-import com.amplifyframework.api.aws.ApiAuthProviders;
-import com.amplifyframework.api.aws.AuthorizationType;
-import com.amplifyframework.api.aws.EndpointType;
-import com.amplifyframework.api.aws.sigv4.FunctionAuthProvider;
-
-import org.junit.Test;
-
-import okhttp3.Request;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
+import com.amplifyframework.api.ApiException
+import com.amplifyframework.api.ApiException.ApiAuthException
+import com.amplifyframework.api.aws.ApiAuthProviders
+import com.amplifyframework.api.aws.AuthorizationType
+import com.amplifyframework.api.aws.EndpointType
+import com.amplifyframework.api.aws.sigv4.FunctionAuthProvider
+import okhttp3.Request
+import okhttp3.Request.Builder
+import org.junit.Assert
+import org.junit.Test
 
 /**
- * Tests the behavior of {@link ApiRequestDecoratorFactory} for different
+ * Tests the behavior of [ApiRequestDecoratorFactory] for different
  * scenarios involving custom Authorization Provider overrides.
  */
-public final class ApiRequestDecoratorFactoryTest {
-    private static final String X_API_KEY = "x-api-key";
-    private static final String AUTHORIZATION = "authorization";
-
+class ApiRequestDecoratorFactoryTest {
     /**
      * Test cases for when no custom provider is given
-     * for {@link com.amplifyframework.api.aws.sigv4.ApiKeyAuthProvider}.
+     * for [com.amplifyframework.api.aws.sigv4.ApiKeyAuthProvider].
      * This is the recommended path, and a customer should
      * never have to provide a custom provider.
      * @throws ApiException From API configuration
      */
     @Test
-    public void testApiKeyOverrideNotProvided() throws ApiException {
-        final String apiKey = "API_KEY_1";
-        ApiAuthProviders providers = ApiAuthProviders.noProviderOverrides();
-        ApiRequestDecoratorFactory factory = new ApiRequestDecoratorFactory(
+    @Throws(ApiException::class)
+    fun testApiKeyOverrideNotProvided() {
+        val apiKey = "API_KEY_1"
+        val providers = ApiAuthProviders.noProviderOverrides()
+        val factory = ApiRequestDecoratorFactory(
             providers,
             AuthorizationType.API_KEY,
             "",
             EndpointType.GRAPHQL,
-            apiKey);
-        Request request = new Request.Builder().url("https://localhost/").build();
-        Request decoratedRequest = factory.forAuthType(AuthorizationType.API_KEY).decorate(request);
-        assertEquals(apiKey, decoratedRequest.header(X_API_KEY));
+            apiKey
+        )
+        val request: Request = Builder().url("https://localhost/").build()
+        val decoratedRequest = factory.forAuthType(AuthorizationType.API_KEY).decorate(request)
+        Assert.assertEquals(apiKey, decoratedRequest.header(X_API_KEY))
     }
 
     /**
      * Test cases for when no API key is provided by the config file..
      */
     @Test
-    public void testApiKeyNotProvidedInConfiguration() {
-        ApiAuthProviders providers = ApiAuthProviders.noProviderOverrides();
-        ApiRequestDecoratorFactory factory = new ApiRequestDecoratorFactory(
+    fun testApiKeyNotProvidedInConfiguration() {
+        val providers = ApiAuthProviders.noProviderOverrides()
+        val factory = ApiRequestDecoratorFactory(
             providers,
             AuthorizationType.API_KEY,
             "",
             EndpointType.GRAPHQL,
-            null);
-        Request request = new Request.Builder().url("https://localhost/").build();
-        assertThrows(
-            ApiException.ApiAuthException.class,
-            () -> factory.forAuthType(AuthorizationType.API_KEY).decorate(request)
-        );
+            null
+        )
+        val request: Request = Builder().url("https://localhost/").build()
+        Assert.assertThrows(
+            ApiAuthException::class.java
+        ) { factory.forAuthType(AuthorizationType.API_KEY).decorate(request) }
     }
 
     /**
@@ -84,135 +80,145 @@ public final class ApiRequestDecoratorFactoryTest {
      * @throws ApiException.ApiAuthException From factory.forAuthType
      */
     @Test
-    public void testApiKeyProvidedInterceptSucceeds() throws ApiException.ApiAuthException {
-        ApiAuthProviders providers = ApiAuthProviders.builder()
-                .apiKeyAuthProvider(() -> "CUSTOM_API_KEY")
-                .build();
-        ApiRequestDecoratorFactory factory = new ApiRequestDecoratorFactory(
+    @Throws(ApiAuthException::class)
+    fun testApiKeyProvidedInterceptSucceeds() {
+        val providers = ApiAuthProviders.builder()
+            .apiKeyAuthProvider { "CUSTOM_API_KEY" }
+            .build()
+        val factory = ApiRequestDecoratorFactory(
             providers,
             AuthorizationType.API_KEY,
             "",
             EndpointType.GRAPHQL,
-            null);
-        Request request = new Request.Builder().url("https://localhost/").build();
-        Request decoratedRequest = factory.forAuthType(AuthorizationType.API_KEY).decorate(request);
-        assertEquals("CUSTOM_API_KEY", decoratedRequest.header(X_API_KEY));
+            null
+        )
+        val request: Request = Builder().url("https://localhost/").build()
+        val decoratedRequest = factory.forAuthType(AuthorizationType.API_KEY).decorate(request)
+        Assert.assertEquals("CUSTOM_API_KEY", decoratedRequest.header(X_API_KEY))
     }
 
     /**
      * Test cases for when a custom implementation of
-     * {@link com.amplifyframework.api.aws.sigv4.ApiKeyAuthProvider}
+     * [com.amplifyframework.api.aws.sigv4.ApiKeyAuthProvider]
      * is provided to the plugin to override the default method
      * of obtaining API key directly from configuration.
      * @throws ApiException From API configuration
      */
     @Test
-    public void testApiKeyOverrideProvided() throws ApiException {
-        ApiAuthProviders providers = ApiAuthProviders.builder()
-                .apiKeyAuthProvider(() -> "CUSTOM_API_KEY")
-                .build();
-
-        ApiRequestDecoratorFactory factory = new ApiRequestDecoratorFactory(
+    @Throws(ApiException::class)
+    fun testApiKeyOverrideProvided() {
+        val providers = ApiAuthProviders.builder()
+            .apiKeyAuthProvider { "CUSTOM_API_KEY" }
+            .build()
+        val factory = ApiRequestDecoratorFactory(
             providers,
             AuthorizationType.API_KEY,
             "",
             EndpointType.GRAPHQL,
-            "CONFIG_API_KEY");
-        Request request = new Request.Builder().url("https://localhost/").build();
-        Request decoratedRequest = factory.forAuthType(AuthorizationType.API_KEY).decorate(request);
-        assertEquals("CUSTOM_API_KEY", decoratedRequest.header(X_API_KEY));
+            "CONFIG_API_KEY"
+        )
+        val request: Request = Builder().url("https://localhost/").build()
+        val decoratedRequest = factory.forAuthType(AuthorizationType.API_KEY).decorate(request)
+        Assert.assertEquals("CUSTOM_API_KEY", decoratedRequest.header(X_API_KEY))
     }
 
     /**
      * If a custom OIDC provider is not provided AND there is
-     * an API that uses {@link AuthorizationType#OPENID_CONNECT}
+     * an API that uses [AuthorizationType.OPENID_CONNECT]
      * as its auth mechanism, then the process will fail at runtime
      * while sending a request with that API.
      */
     @Test
-    public void testOidcOverrideNotProvided() {
-        ApiAuthProviders providers = ApiAuthProviders.noProviderOverrides();
-        ApiRequestDecoratorFactory factory = new ApiRequestDecoratorFactory(
+    fun testOidcOverrideNotProvided() {
+        val providers = ApiAuthProviders.noProviderOverrides()
+        val factory = ApiRequestDecoratorFactory(
             providers,
             AuthorizationType.OPENID_CONNECT,
             "",
             EndpointType.GRAPHQL,
-            "CONFIG_API_KEY");
-        Request request = new Request.Builder().url("https://localhost/").build();
-        assertThrows(
-            ApiException.ApiAuthException.class,
-            () -> factory.forAuthType(AuthorizationType.OPENID_CONNECT).decorate(request)
-        );
+            "CONFIG_API_KEY"
+        )
+        val request: Request = Builder().url("https://localhost/").build()
+        Assert.assertThrows(
+            ApiAuthException::class.java
+        ) { factory.forAuthType(AuthorizationType.OPENID_CONNECT).decorate(request) }
     }
 
     /**
      * Test to confirm that passing any custom implementation of
-     * {@link com.amplifyframework.api.aws.sigv4.OidcAuthProvider}
+     * [com.amplifyframework.api.aws.sigv4.OidcAuthProvider]
      * prevents crashes while intercepting requests.
      * @throws ApiException From API configuration
      */
     @Test
-    public void testOidcOverrideProvided() throws ApiException {
-        final String oidcJwtToken = "OIDC_JWT_TOKEN";
-
-        ApiAuthProviders providers = ApiAuthProviders.builder()
-                .oidcAuthProvider(() -> oidcJwtToken)
-                .build();
-        ApiRequestDecoratorFactory factory = new ApiRequestDecoratorFactory(
+    @Throws(ApiException::class)
+    fun testOidcOverrideProvided() {
+        val oidcJwtToken = "OIDC_JWT_TOKEN"
+        val providers = ApiAuthProviders.builder()
+            .oidcAuthProvider { oidcJwtToken }
+            .build()
+        val factory = ApiRequestDecoratorFactory(
             providers,
             AuthorizationType.OPENID_CONNECT,
             "",
             EndpointType.GRAPHQL,
-            "CONFIG_API_KEY");
-        Request request = new Request.Builder().url("https://localhost/").build();
-        Request decoratedRequest = factory.forAuthType(AuthorizationType.OPENID_CONNECT).decorate(request);
-        assertEquals(oidcJwtToken, decoratedRequest.header(AUTHORIZATION));
+            "CONFIG_API_KEY"
+        )
+        val request: Request = Builder().url("https://localhost/").build()
+        val decoratedRequest =
+            factory.forAuthType(AuthorizationType.OPENID_CONNECT).decorate(request)
+        Assert.assertEquals(oidcJwtToken, decoratedRequest.header(AUTHORIZATION))
     }
 
     /**
      * If a custom auth provider is not provided AND there is
-     * an API that uses {@link AuthorizationType#AWS_LAMBDA}
+     * an API that uses [AuthorizationType.AWS_LAMBDA]
      * as its auth mechanism, then the process will fail at runtime
      * while sending a request with that API.
      */
     @Test
-    public void testCustomOverrideNotProvided() {
-        ApiAuthProviders providers = ApiAuthProviders.noProviderOverrides();
-        ApiRequestDecoratorFactory factory = new ApiRequestDecoratorFactory(
+    fun testCustomOverrideNotProvided() {
+        val providers = ApiAuthProviders.noProviderOverrides()
+        val factory = ApiRequestDecoratorFactory(
             providers,
             AuthorizationType.AWS_LAMBDA,
             "",
             EndpointType.GRAPHQL,
-            "CONFIG_API_KEY");
-        Request request = new Request.Builder().url("https://localhost/").build();
-        assertThrows(
-            ApiException.ApiAuthException.class,
-            () -> factory.forAuthType(AuthorizationType.AWS_LAMBDA).decorate(request)
-        );
+            "CONFIG_API_KEY"
+        )
+        val request: Request = Builder().url("https://localhost/").build()
+        Assert.assertThrows(
+            ApiAuthException::class.java
+        ) { factory.forAuthType(AuthorizationType.AWS_LAMBDA).decorate(request) }
     }
 
     /**
      * Test to confirm that passing any custom implementation of
-     * {@link FunctionAuthProvider}
+     * [FunctionAuthProvider]
      * prevents crashes while intercepting requests.
      * @throws ApiException From API configuration
      */
     @Test
-    public void testCustomOverrideProvided() throws ApiException {
-        final String customToken = "CUSTOM_TOKEN";
-
-        ApiAuthProviders providers = ApiAuthProviders.builder()
-                .functionAuthProvider(() -> customToken)
-                .build();
-        ApiRequestDecoratorFactory factory = new ApiRequestDecoratorFactory(
+    @Throws(ApiException::class)
+    fun testCustomOverrideProvided() {
+        val customToken = "CUSTOM_TOKEN"
+        val providers = ApiAuthProviders.builder()
+            .functionAuthProvider { customToken }
+            .build()
+        val factory = ApiRequestDecoratorFactory(
             providers,
             AuthorizationType.AWS_LAMBDA,
             "",
             EndpointType.GRAPHQL,
-            "CONFIG_API_KEY");
-        Request request = new Request.Builder().url("https://localhost/").build();
-        Request decoratedRequest = factory.forAuthType(AuthorizationType.AWS_LAMBDA).decorate(request);
-        assertEquals(customToken, decoratedRequest.header(AUTHORIZATION));
+            "CONFIG_API_KEY"
+        )
+        val request: Request = Builder().url("https://localhost/").build()
+        val decoratedRequest = factory.forAuthType(AuthorizationType.AWS_LAMBDA).decorate(request)
+        Assert.assertEquals(customToken, decoratedRequest.header(AUTHORIZATION))
     }
 
+    companion object {
+        private const val X_API_KEY = "x-api-key"
+        private const val AUTHORIZATION = "authorization"
+    }
 }
