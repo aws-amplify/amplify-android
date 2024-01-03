2ecafd315badf06651d1aac9024bc3efa2ced860
tjroach <tjroach@amazon.com>
2024-01-03 17:03:56 -0500

    bump timeout

--- aws-datastore/src/test/java/com/amplifyframework/datastore/syncengine/VersionRepositoryTest.kt
+++ aws-datastore/src/test/java/com/amplifyframework/datastore/syncengine/VersionRepositoryTest.kt
@@ -36,6 +36,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import kotlin.time.Duration.Companion.seconds
 
 /**
  * Tests the [VersionRepository].
@@ -201,7 +202,7 @@ class VersionRepositoryTest {
 
     @Test
     // This test ensures the 950 chunking works
-    fun fetchModelVersionReturnsMoreThan950() = runTest {
+    fun fetchModelVersionReturnsMoreThan950() = runTest(timeout = 10.seconds) {
         val modelsWithMetadata = mutableListOf<ModelWithMetadata<BlogOwner>>()
         for (i in 0 until 1000) {
             val owner = BlogOwner.builder().name("Owner$i").build()
