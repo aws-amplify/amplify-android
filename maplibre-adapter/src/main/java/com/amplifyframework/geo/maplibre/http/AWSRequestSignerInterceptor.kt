fd3afa06f0baa6f92d6ad83a2d02a4ab62f0c1b5
tjroach <tjroach@amazon.com>
2024-02-01 11:01:29 -0500

    revert attempted change at url request conversions and correct previous implementation

--- maplibre-adapter/src/main/java/com/amplifyframework/geo/maplibre/http/AWSRequestSignerInterceptor.kt
+++ maplibre-adapter/src/main/java/com/amplifyframework/geo/maplibre/http/AWSRequestSignerInterceptor.kt
@@ -23,12 +23,15 @@ import aws.smithy.kotlin.runtime.http.Headers as AwsHeaders
 import aws.smithy.kotlin.runtime.http.HttpBody
 import aws.smithy.kotlin.runtime.http.HttpMethod
 import aws.smithy.kotlin.runtime.http.request.HttpRequest
+import aws.smithy.kotlin.runtime.net.Host
+import aws.smithy.kotlin.runtime.net.Scheme
+import aws.smithy.kotlin.runtime.net.toUrlString
 import aws.smithy.kotlin.runtime.net.url.Url
 import com.amplifyframework.geo.location.AWSLocationGeoPlugin
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import kotlinx.coroutines.runBlocking
-import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.RequestBody
@@ -72,13 +75,24 @@ internal class AWSRequestSignerInterceptor(
     }
 
     private fun Request.Builder.copyFrom(request: HttpRequest): Request.Builder {
-        this.url(request.url.toString().toHttpUrl())
+        val urlBuilder = HttpUrl.Builder()
+            .host(request.url.host.toUrlString())
+            .scheme(request.url.scheme.protocolName)
+            .encodedPath(request.url.requestRelativePath)
+            .encodedFragment(request.url.fragment?.encoded)
+
+        request.url.parameters.forEach { name, parameters ->
+            parameters.forEach {
+                it.encoded
+                urlBuilder.setEncodedQueryParameter(name.encoded, it.encoded)
+            }
+        }
         request.headers.forEach { name, values ->
             values.forEach {
                 this.header(name, it)
             }
         }
-        return this
+        return this.url(urlBuilder.build())
     }
 
     @OptIn(InternalApi::class)
@@ -99,7 +113,23 @@ internal class AWSRequestSignerInterceptor(
             credentials = plugin.credentialsProvider.resolve(emptyAttributes())
         }
 
-        val httpUrl = Url.parse(url.toString())
+        val httpUrl = Url {
+            scheme = Scheme(url.scheme, url.port)
+            host = Host.parse(url.host)
+            port = url.port
+            path {
+                encoded = url.encodedPath
+            }
+            parameters {
+                url.queryParameterNames.map { name ->
+                    url.queryParameter(name)?.let { value ->
+                        decodedParameters.add(name, value)
+                    }
+                }
+            }
+            encodedFragment = url.encodedFragment
+        }
+
         val bodyBytes: ByteArray = getBytes(request.body)
         val body2 = HttpBody.fromBytes(bodyBytes)
         val method = HttpMethod.parse(request.method)
