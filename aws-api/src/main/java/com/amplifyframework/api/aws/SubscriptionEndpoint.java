d50c303690e72207651c022b8fef71c1eca7c56d
tjroach <tjroach@amazon.com>
2024-02-16 10:43:47 -0500

    remove log

--- aws-api/src/main/java/com/amplifyframework/api/aws/SubscriptionEndpoint.java
+++ aws-api/src/main/java/com/amplifyframework/api/aws/SubscriptionEndpoint.java
@@ -166,7 +166,6 @@ final class SubscriptionEndpoint {
                 .toString();
 
             socket.send(jsonMessage);
-            LOG.info("Send to ws: " + jsonMessage);
 
         } catch (JSONException | ApiException exception) {
             // If the subscriptionId was still pending, then we can call the onSubscriptionError
