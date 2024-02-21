77b69a50febd9828dd0188acf734d140b0b9e3a9
tjroach <tjroach@amazon.com>
2024-02-21 10:43:55 -0500

    Update storage item with path

--- core/src/main/java/com/amplifyframework/storage/StorageItem.kt
+++ core/src/main/java/com/amplifyframework/storage/StorageItem.kt
@@ -12,24 +12,56 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+package com.amplifyframework.storage
 
-package com.amplifyframework.storage;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.util.ObjectsCompat;
-
-import java.util.Date;
+import java.util.Date
 
 /**
  * Used to store the data on each item in a storage.
  */
-public final class StorageItem {
-    private final String key;
-    private final long size;
-    private final Date lastModified;
-    private final String eTag;
-    private final Object pluginResults;
+data class StorageItem
+/**
+ * Object to represent an item listing in Storage.
+ * @param path The unique path of the object in storage.
+ * @param key The unique identifier of the object in storage.
+ * @param size Size in bytes of the object
+ * @param lastModified The date the Object was Last Modified
+ * @param eTag The entity tag is an MD5 hash of the object.
+ * ETag reflects only changes to the contents of an object, not its metadata.
+ * @param pluginResults Additional results specific to the plugin.
+ */ (
+    /**
+     * Get unique path of the object in storage.
+     * @return Unique path of the object in storage.
+     */
+    val path: String,
+    /**
+     * Get unique identifier of the object in storage.
+     * @return Unique identifier of the object in storage.
+     */
+    @Deprecated("Migrate to using path instead")
+    val key: String,
+    /**
+     * Get size in bytes of the object.
+     * @return Size in bytes of the object
+     */
+    val size: Long,
+    /**
+     * Get the date the Object was Last Modified.
+     * @return The date the Object was Last Modified
+     */
+    val lastModified: Date,
+    /**
+     * Get an MD5 hash of the object.
+     * @return An MD5 hash of the object
+     */
+    val eTag: String,
+    /**
+     * Get additional results specific to the plugin.
+     * @return Additional results specific to the plugin.
+     */
+    val pluginResults: Any?
+) {
 
     /**
      * Object to represent an item listing in Storage.
@@ -37,111 +69,14 @@ public final class StorageItem {
      * @param size Size in bytes of the object
      * @param lastModified The date the Object was Last Modified
      * @param eTag The entity tag is an MD5 hash of the object.
-     *             ETag reflects only changes to the contents of an object, not its metadata.
+     * ETag reflects only changes to the contents of an object, not its metadata.
      * @param pluginResults Additional results specific to the plugin.
      */
-    public StorageItem(
-            @NonNull String key,
-            long size,
-            @NonNull Date lastModified,
-            @NonNull String eTag,
-            @Nullable Object pluginResults
-    ) {
-        this.key = key;
-        this.size = size;
-        this.lastModified = lastModified;
-        this.eTag = eTag;
-        this.pluginResults = pluginResults;
-    }
-
-    /**
-     * Get unique identifier of the object in storage.
-     * @return Unique identifier of the object in storage.
-     */
-    @NonNull
-    public String getKey() {
-        return key;
-    }
-
-    /**
-     * Get size in bytes of the object.
-     * @return Size in bytes of the object
-     */
-    public long getSize() {
-        return size;
-    }
-
-    /**
-     * Get the date the Object was Last Modified.
-     * @return The date the Object was Last Modified
-     */
-    @NonNull
-    public Date getLastModified() {
-        return lastModified;
-    }
-
-    /**
-     * Get an MD5 hash of the object.
-     * @return An MD5 hash of the object
-     */
-    @NonNull
-    public String getETag() {
-        return eTag;
-    }
-
-    /**
-     * Get additional results specific to the plugin.
-     * @return Additional results specific to the plugin.
-     */
-    @Nullable
-    public Object getPluginResults() {
-        return pluginResults;
-    }
-
-    @Override
-    @NonNull
-    public String toString() {
-        return "StorageItem{" +
-                "key='" + key + '\'' +
-                ", size=" + size +
-                ", lastModified=" + lastModified.toString() +
-                ", eTag='" + eTag + '\'' +
-                ", pluginResults=" + pluginResults +
-                '}';
-    }
-
-    @Override
-    public boolean equals(@Nullable Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!(obj instanceof StorageItem)) {
-            return false;
-        }
-
-        StorageItem that = (StorageItem) obj;
-        if (!ObjectsCompat.equals(key, that.key)) {
-            return false;
-        }
-        if (size != that.size) {
-            return false;
-        }
-        if (!ObjectsCompat.equals(lastModified, that.lastModified)) {
-            return false;
-        }
-        if (!ObjectsCompat.equals(eTag, that.eTag)) {
-            return false;
-        }
-        return ObjectsCompat.equals(pluginResults, that.pluginResults);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = key.hashCode();
-        result = 31 * result + (int) size;
-        result = 31 * result + lastModified.hashCode();
-        result = 31 * result + eTag.hashCode();
-        result = 31 * result + (pluginResults != null ? pluginResults.hashCode() : 0);
-        return result;
-    }
-}
+    @Deprecated("Stop using this constructor in favor of constructor with path")
+     constructor(
+         key: String,
+         size: Long,
+         lastModified: Date,
+         eTag: String, pluginResults: Any?
+     ) : this(key, key, size, lastModified, eTag, pluginResults)
+ }
