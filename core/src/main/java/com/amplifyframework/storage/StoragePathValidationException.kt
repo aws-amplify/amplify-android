3b122ecc8359f17e087fe4b184099f5449b9598c
tjroach <tjroach@amazon.com>
2024-03-18 13:50:00 -0400

    Add StoragePathValidationException

---
+++ core/src/main/java/com/amplifyframework/storage/StoragePathValidationException.kt
@@ -0,0 +1,11 @@
+package com.amplifyframework.storage
+
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.annotations.InternalAmplifyApi
+
+class StoragePathValidationException @InternalAmplifyApi constructor(
+    message: String,
+    recoverySuggestion: String
+): AmplifyException(message, recoverySuggestion) {
+    @InternalAmplifyApi companion object
+}
