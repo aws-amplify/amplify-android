10f6c97b8fc7e37ad4a35a141875f408f9fbc784
tjroach <tjroach@amazon.com>
2024-03-18 16:26:38 -0400

    binary dump

--- aws-storage-s3/src/main/java/com/amplifyframework/storage/s3/extensions/StorageExceptionExtensions.kt
+++ aws-storage-s3/src/main/java/com/amplifyframework/storage/s3/extensions/StorageExceptionExtensions.kt
@@ -3,13 +3,13 @@ package com.amplifyframework.storage.s3.extensions
 import com.amplifyframework.storage.StorageException
 import com.amplifyframework.storage.StoragePathValidationException
 
-fun StoragePathValidationException.Companion.invalidStoragePathException() = StorageException(
+internal fun StoragePathValidationException.Companion.invalidStoragePathException() = StorageException(
     "Invalid StoragePath provided",
     StoragePathValidationException("Invalid StoragePath provided", "Path must be NonEmpty and start with /"),
     "Path must be NonEmpty and start with /"
 )
 
-fun StoragePathValidationException.Companion.unsupportedStoragePathException() = StorageException(
+internal fun StoragePathValidationException.Companion.unsupportedStoragePathException() = StorageException(
     "Unsupported StoragePath provided",
     StoragePathValidationException(
         "Unsupported StoragePath provided",
