74a01ef3c5433ef87af693fc5b027b254d2b8a01
Dan Tran <dantrong@amazon.com>
2024-04-07 11:53:08 -0400

    Add ConnectivityNetworkCallback class in ReachabilityMonitor

--- aws-datastore/src/main/java/com/amplifyframework/datastore/syncengine/ReachabilityMonitor.kt
+++ aws-datastore/src/main/java/com/amplifyframework/datastore/syncengine/ReachabilityMonitor.kt
@@ -15,15 +15,22 @@
 
 package com.amplifyframework.datastore.syncengine
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.ConnectivityManager.NetworkCallback
+import android.net.LinkProperties
 import android.net.Network
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.annotation.VisibleForTesting
 import com.amplifyframework.datastore.DataStoreException
+import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableEmitter
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 import java.util.concurrent.TimeUnit
+
 
 /**
  * The ReachabilityMonitor is responsible for watching the network status as provided by the OS.
@@ -35,7 +42,7 @@ import java.util.concurrent.TimeUnit
  * The network changes are debounced with a 250 ms delay to allow some time for one network to connect after another
  * network has disconnected (for example, wifi is lost, then cellular connects) to reduce thrashing.
  */
-public interface ReachabilityMonitor {
+interface ReachabilityMonitor {
     fun configure(context: Context)
     @VisibleForTesting
     fun configure(context: Context, connectivityProvider: ConnectivityProvider)
@@ -65,15 +72,7 @@ private class ReachabilityMonitorImpl constructor(val schedulerProvider: Schedul
         val observable = Observable.create { emitter ->
             connectivityProvider.registerDefaultNetworkCallback(
                 context,
-                object : NetworkCallback() {
-                    override fun onAvailable(network: Network) {
-                        emitter.onNext(true)
-                    }
-
-                    override fun onLost(network: Network) {
-                        emitter.onNext(false)
-                    }
-                }
+                ConnectivityNetworkCallback(emitter)
             )
             emitter.onNext(connectivityProvider.hasActiveNetwork)
         }
@@ -91,6 +90,61 @@ private class ReachabilityMonitorImpl constructor(val schedulerProvider: Schedul
         }
         return subject.subscribeOn(schedulerProvider.io())
     }
+
+    private inner class ConnectivityNetworkCallback(private val emitter: ObservableEmitter<Boolean>) : NetworkCallback() {
+        private var currentNetwork: Network? = null
+        private var currentCapabilities: NetworkCapabilities? = null
+        private val DELAY_MS: Long = 250
+
+        override fun onAvailable(network: Network) {
+            currentNetwork = network
+            asyncUpdateAndSend()
+        }
+
+        override fun onLosing(network: Network, maxMsToLive: Int) {
+            currentNetwork = network
+            updateAndSend()
+        }
+
+        override fun onLost(network: Network) {
+            currentNetwork = null
+            currentCapabilities = null
+            updateAndSend()
+        }
+
+        override fun onUnavailable() {
+            currentNetwork = null
+            currentCapabilities = null
+            updateAndSend()
+        }
+
+        override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
+            currentNetwork = network
+            currentCapabilities = networkCapabilities
+            updateAndSend()
+        }
+
+        override fun onLinkPropertiesChanged(network: Network, linkProperties: LinkProperties) {
+            if (currentNetwork != null) {
+                currentNetwork = network
+            }
+            asyncUpdateAndSend()
+        }
+
+        private fun updateAndSend() {
+            emitter.onNext(DefaultConnectivityProvider.isInternetReachable(currentCapabilities))
+        }
+
+        @SuppressLint("CheckResult")
+        private fun asyncUpdateAndSend() {
+            Completable.timer(DELAY_MS, TimeUnit.MILLISECONDS, schedulerProvider.computation()).subscribe {
+                currentCapabilities = DefaultConnectivityProvider.getNetworkCapabilities(connectivityProvider?.connectivityNetworkManager)
+                if (currentCapabilities != null) {
+                    updateAndSend()
+                }
+            }
+        }
+    }
 }
 
 /**
@@ -98,32 +152,74 @@ private class ReachabilityMonitorImpl constructor(val schedulerProvider: Schedul
  * is a concrete class created within context.getSystemService() it can't be overridden with a test
  * implementation, so this interface works around that issue.
  */
-public interface ConnectivityProvider {
+interface ConnectivityProvider {
     val hasActiveNetwork: Boolean
+    val connectivityNetworkManager: ConnectivityManager?
     fun registerDefaultNetworkCallback(context: Context, callback: NetworkCallback)
 }
 
 private class DefaultConnectivityProvider : ConnectivityProvider {
-
     private var connectivityManager: ConnectivityManager? = null
 
+    override val connectivityNetworkManager: ConnectivityManager?
+        get() = connectivityManager
+
     override val hasActiveNetwork: Boolean
-        get() = connectivityManager?.let { it.activeNetwork != null }
-            ?: run {
-                throw DataStoreException(
-                    "ReachabilityMonitor has not been configured.",
-                    "Call ReachabilityMonitor.configure() before calling ReachabilityMonitor.getObservable()"
-                )
-            }
+        get() = connectivityManager?.let {
+            val networkCapabilities: NetworkCapabilities? = getNetworkCapabilities(it)
+            isInternetReachable(networkCapabilities)
+        } ?: run {
+            throw DataStoreException(
+                "ReachabilityMonitor has not been configured.",
+                "Call ReachabilityMonitor.configure() before calling ReachabilityMonitor.getObservable()"
+            )
+        }
 
     override fun registerDefaultNetworkCallback(context: Context, callback: NetworkCallback) {
         connectivityManager = context.getSystemService(ConnectivityManager::class.java)
-        connectivityManager?.let { it.registerDefaultNetworkCallback(callback) }
+        connectivityManager?.registerDefaultNetworkCallback(callback)
             ?: run {
                 throw DataStoreException(
                     "ConnectivityManager not available",
                     "No recovery suggestion is available"
                 )
             }
+    }
+
+    companion object {
+        fun getNetworkCapabilities(connectivityManager: ConnectivityManager?): NetworkCapabilities? {
+            try {
+                return connectivityManager?.let {
+                    it.getNetworkCapabilities(it.activeNetwork)
+                }
+            } catch (ignored: SecurityException) {
+                // Android 11 may throw a 'package does not belong' security exception here.
+                // Google fixed Android 14, 13 and 12 with the issue where Chaland Jean patched those versions.
+                // Android 11 is too old, so that's why we have to catch this exception here to be safe.
+                //  https://android.googlesource.com/platform/frameworks/base/+/249be21013e389837f5b2beb7d36890b25ecfaaf%5E%21/
+                // We need to catch this to prevent app crash.
+            }
+            return null
+        }
+
+        fun isInternetReachable(capabilities: NetworkCapabilities?): Boolean {
+            var isInternetReachable = false
+            if (capabilities != null) {
+                // Check to see if the network is temporarily unavailable or if airplane mode is toggled on
+                val isInternetSuspended = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    !capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_SUSPENDED)
+                } else {
+                    // TODO: for SDK < 28, add extra check to evaluate airplane mode
+                    false
+                }
+                isInternetReachable = (capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                    && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+                    && !isInternetSuspended)
+                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)) {
+                    isInternetReachable = isInternetReachable && capabilities.linkDownstreamBandwidthKbps != 0
+                }
+            }
+            return isInternetReachable
+        }
     }
 }
