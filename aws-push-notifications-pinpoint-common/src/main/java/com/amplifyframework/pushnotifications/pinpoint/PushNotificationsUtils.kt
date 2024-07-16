6076039ce75e3b6e595164c9665ef5b621f9b78a
tKaneko129 <taisuke_kaneko@yaz.co.jp>
2024-07-16 15:45:43 +0900

    catch Exception when download icon image

--- aws-push-notifications-pinpoint-common/src/main/java/com/amplifyframework/pushnotifications/pinpoint/PushNotificationsUtils.kt
+++ aws-push-notifications-pinpoint-common/src/main/java/com/amplifyframework/pushnotifications/pinpoint/PushNotificationsUtils.kt
@@ -77,7 +77,9 @@ class PushNotificationsUtils(
     }
 
     private suspend fun downloadImage(url: String): Bitmap? = withContext(Dispatchers.IO) {
-        BitmapFactory.decodeStream(URL(url).openConnection().getInputStream())
+        runCatching {
+            BitmapFactory.decodeStream(URL(url).openConnection().getInputStream())
+        }.getOrNull()
     }
 
     fun isAppInForeground(): Boolean {
