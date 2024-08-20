70b357d4ae0b85e72cf159a2a0091d086e447cee
Tuan Pham <phantum@amazon.com>
2024-08-20 10:55:07 -0500

    update unit test per code review suggestion

--- aws-storage-s3/src/test/java/com/amplifyframework/storage/s3/AWSS3StoragePluginTest.kt
+++ aws-storage-s3/src/test/java/com/amplifyframework/storage/s3/AWSS3StoragePluginTest.kt
@@ -16,6 +16,7 @@
 package com.amplifyframework.storage.s3
 
 import com.amplifyframework.storage.BucketInfo
+import com.amplifyframework.storage.InvalidStorageBucketException
 import com.amplifyframework.storage.StorageBucket
 import com.amplifyframework.storage.StorageException
 import com.amplifyframework.storage.s3.service.AWSS3StorageService
@@ -23,6 +24,7 @@ import com.amplifyframework.storage.s3.service.StorageService
 import com.amplifyframework.testutils.configuration.amplifyOutputsData
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.throwable.shouldHaveCauseOfType
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
@@ -143,8 +145,9 @@ class AWSS3StoragePluginTest {
 
         plugin.configure(data, mockk())
         val bucket = StorageBucket.fromOutputs("myBucket")
-        shouldThrow<StorageException> {
+        val exception = shouldThrow<StorageException> {
             plugin.getStorageService(bucket)
         }
+        exception.shouldHaveCauseOfType<InvalidStorageBucketException>()
     }
 }
