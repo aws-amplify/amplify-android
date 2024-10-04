3026c6aafa2917d6d4ad59f6dbb3fd12fb53dbdb
Vincent Tran <hello@vincetran.me>
2024-10-04 15:22:51 -0700

    Add Email MFA integration test and add unit tests

---
+++ aws-auth-cognito/src/androidTest/java/com/amplifyframework/auth/cognito/AWSCognitoAuthPluginEmailMFATests.kt
@@ -0,0 +1,192 @@
+/*
+ * Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amplifyframework.auth.cognito
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.amplifyframework.api.aws.AWSApiPlugin
+import com.amplifyframework.api.graphql.SimpleGraphQLRequest
+import com.amplifyframework.auth.AuthUserAttribute
+import com.amplifyframework.auth.AuthUserAttributeKey
+import com.amplifyframework.auth.MFAType
+import com.amplifyframework.auth.cognito.exceptions.service.CodeMismatchException
+import com.amplifyframework.auth.cognito.test.R
+import com.amplifyframework.auth.cognito.testutils.AbortableCountDownLatch
+import com.amplifyframework.auth.options.AuthSignUpOptions
+import com.amplifyframework.auth.result.AuthSignUpResult
+import com.amplifyframework.auth.result.step.AuthSignInStep
+import com.amplifyframework.core.configuration.AmplifyOutputs
+import com.amplifyframework.core.configuration.AmplifyOutputsData
+import com.amplifyframework.datastore.generated.model.MfaInfo
+import com.amplifyframework.testutils.Assets
+import com.amplifyframework.testutils.sync.SynchronousAuth
+import java.util.Random
+import java.util.UUID
+import java.util.concurrent.TimeUnit
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import org.junit.After
+import org.junit.BeforeClass
+import org.junit.Test
+
+class AWSCognitoAuthPluginEmailMFATests {
+
+    private val password = "${UUID.randomUUID()}BleepBloop1234!"
+    private val userName = "test${Random().nextInt()}"
+    private val email = "$userName@testdomain.com"
+
+    companion object {
+        private var authPlugin = AWSCognitoAuthPlugin()
+        private var apiPlugin = AWSApiPlugin()
+        lateinit var synchronousAuth: SynchronousAuth
+        var mfaCode = ""
+        var abortableLatch: AbortableCountDownLatch? = null
+
+        @JvmStatic
+        @BeforeClass
+        fun initializePlugin() {
+            val context = ApplicationProvider.getApplicationContext<Context>()
+            val config = AmplifyOutputsData
+                .deserialize(context, AmplifyOutputs.fromResource(R.raw.amplify_outputs_email_or_totp_mfa))
+
+            authPlugin.configure(config, context)
+            apiPlugin.configure(config, context)
+            synchronousAuth = SynchronousAuth.delegatingTo(authPlugin)
+
+            apiPlugin.subscribe(
+                SimpleGraphQLRequest<MfaInfo>(
+                    Assets.readAsString("create-mfa-subscription.graphql"),
+                    MfaInfo::class.java,
+                    null
+                ),
+                { println("====== Subscription Established ======") },
+                {
+                    println("====== Received some MFA Info ======")
+                    mfaCode = it.data.code
+                    abortableLatch?.abort()
+                },
+                { println("====== Subscription Failed $it ======") },
+                { }
+            )
+        }
+    }
+
+    @After
+    fun tearDown() {
+        mfaCode = ""
+        synchronousAuth.deleteUser()
+    }
+
+    @Test
+    fun fresh_email_mfa_setup() {
+        // Step 1: Sign up a new user
+        signUpNewUser()
+
+        // Step 2: Attempt to sign in with the newly created user
+        var signInResult = synchronousAuth.signIn(userName, password)
+
+        // Validation 1: Validate that the next step is MFA Setup Selection
+        assertEquals(AuthSignInStep.CONTINUE_SIGN_IN_WITH_MFA_SETUP_SELECTION, signInResult.nextStep.signInStep)
+
+        // Validation 2: Validate that the available MFA choices are Email and TOTP
+        assertEquals(setOf(MFAType.EMAIL, MFAType.TOTP), signInResult.nextStep.allowedMFATypes)
+
+        // Step 3: Select "Email" as the MFA to set up
+        signInResult = synchronousAuth.confirmSignIn("EMAIL_OTP")
+
+        // Validation 2: Validate that the next step is to input the user's email address
+        assertEquals(AuthSignInStep.CONTINUE_SIGN_IN_WITH_EMAIL_MFA_SETUP, signInResult.nextStep.signInStep)
+
+        // Step 4: Input the email address to send the code to then wait for the MFA code
+        abortableLatch = AbortableCountDownLatch(1)
+        signInResult = synchronousAuth.confirmSignIn(email)
+
+        // Validation 3: Validate that the next step is to confirm the emailed MFA code
+        assertEquals(AuthSignInStep.CONFIRM_SIGN_IN_WITH_EMAIL_MFA_CODE, signInResult.nextStep.signInStep)
+
+        // Wait until the MFA code has been received
+        abortableLatch?.await(20, TimeUnit.SECONDS)
+
+        // Step 5: Input the emailed MFA code for confirmation
+        signInResult = synchronousAuth.confirmSignIn(mfaCode)
+
+        // Validation 4: Validate that MFA setup is done
+        assertEquals(AuthSignInStep.DONE, signInResult.nextStep.signInStep)
+    }
+
+    @Test
+    fun sign_in_to_existing_email_mfa() {
+        // Step 1: Sign up a new user with an existing email address
+        signUpNewUser(email)
+
+        // Step 2: Attempt to sign in with the newly created user
+        abortableLatch = AbortableCountDownLatch(1)
+        var signInResult = synchronousAuth.signIn(userName, password)
+
+        // Validation 1: Validate that the next step is to confirm the emailed MFA code
+        assertEquals(AuthSignInStep.CONFIRM_SIGN_IN_WITH_EMAIL_MFA_CODE, signInResult.nextStep.signInStep)
+
+        // Wait until the MFA code has been received
+        abortableLatch?.await(20, TimeUnit.SECONDS)
+
+        // Step 4: Input the emailed MFA code for confirmation
+        signInResult = synchronousAuth.confirmSignIn(mfaCode)
+
+        // Validation 2: Validate that MFA setup is done
+        assertEquals(AuthSignInStep.DONE, signInResult.nextStep.signInStep)
+    }
+
+    @Test
+    fun use_an_incorrect_MFA_code_then_sign_in_using_the_correct_one() {
+        // Step 1: Sign up a new user with an existing email address
+        signUpNewUser(email)
+
+        // Step 2: Attempt to sign in with the newly created user
+        abortableLatch = AbortableCountDownLatch(1)
+        var signInResult = synchronousAuth.signIn(userName, password)
+
+        // Validation 1: Validate that the next step is to confirm the emailed MFA code
+        assertEquals(AuthSignInStep.CONFIRM_SIGN_IN_WITH_EMAIL_MFA_CODE, signInResult.nextStep.signInStep)
+
+        // Wait until the MFA code has been received
+        abortableLatch?.await(20, TimeUnit.SECONDS)
+
+        // Step 4: Input the an incorrect MFA code
+        // Validation 2: Validate that an incorrect MFA code throws a CodeMismatchException
+        assertFailsWith<CodeMismatchException> {
+            signInResult = synchronousAuth.confirmSignIn(mfaCode.reversed())
+        }
+
+        // Step 5: Input the correct MFA code for validation
+        signInResult = synchronousAuth.confirmSignIn(mfaCode)
+
+        // Validation 3: Validate that MFA setup is done
+        assertEquals(AuthSignInStep.DONE, signInResult.nextStep.signInStep)
+    }
+
+    private fun signUpNewUser(email: String? = null): AuthSignUpResult {
+        val attributes = if (email == null) {
+            emptyList()
+        } else {
+            listOf(AuthUserAttribute(AuthUserAttributeKey.email(), email))
+        }
+        val options = AuthSignUpOptions.builder()
+            .userAttributes(
+                attributes
+            ).build()
+        return synchronousAuth.signUp(userName, password, options)
+    }
+}
---
+++ aws-auth-cognito/src/test/java/com/amplifyframework/auth/cognito/helpers/SignInChallengeHelperTest.kt
@@ -0,0 +1,388 @@
+/*
+ * Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.amplifyframework.auth.cognito.helpers
+
+import com.amplifyframework.auth.AuthCodeDeliveryDetails
+import com.amplifyframework.auth.AuthException
+import com.amplifyframework.auth.MFAType
+import com.amplifyframework.auth.TOTPSetupDetails
+import com.amplifyframework.auth.exceptions.UnknownException
+import com.amplifyframework.auth.result.AuthSignInResult
+import com.amplifyframework.auth.result.step.AuthNextSignInStep
+import com.amplifyframework.auth.result.step.AuthSignInStep
+import com.amplifyframework.statemachine.codegen.data.AuthChallenge
+import com.amplifyframework.statemachine.codegen.data.SignInTOTPSetupData
+import featureTest.utilities.APICaptorFactory.Companion.onError
+import featureTest.utilities.APICaptorFactory.Companion.onSuccess
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+
+class SignInChallengeHelperTest {
+    private val username = "username"
+    private val email = "test@testdomain.com"
+
+    // MFA Setup
+    @Test
+    fun `User needs to select either Email OTP or TOTP to setup`() {
+        var signInResult: AuthSignInResult? = null
+        var errorResult: AuthException? = null
+        SignInChallengeHelper.getNextStep(
+            challenge = AuthChallenge(
+                challengeName = "MFA_SETUP",
+                username = username,
+                session = "session",
+                parameters = mapOf("MFAS_CAN_SETUP" to "\"EMAIL_OTP\",\"SOFTWARE_TOKEN_MFA\"")
+            ),
+            onSuccess = {
+                signInResult = it
+            },
+            onError = {
+                errorResult = it
+            },
+            signInTOTPSetupData = null,
+            allowedMFAType = null
+        )
+        assertEquals(
+            AuthSignInResult(
+                false,
+                AuthNextSignInStep(
+                    AuthSignInStep.CONTINUE_SIGN_IN_WITH_MFA_SETUP_SELECTION,
+                    emptyMap(),
+                    null,
+                    null,
+                    setOf(MFAType.EMAIL, MFAType.TOTP)
+                )
+            ),
+            signInResult
+        )
+        assertNull(errorResult)
+    }
+
+    @Test
+    fun `User needs to setup TOTP`() {
+        val totpSetupData = SignInTOTPSetupData(
+            secretCode = "secretCode",
+            session = "session",
+            username = username
+        )
+
+        var signInResult: AuthSignInResult? = null
+        var errorResult: AuthException? = null
+        SignInChallengeHelper.getNextStep(
+            challenge = AuthChallenge(
+                challengeName = "MFA_SETUP",
+                username = username,
+                session = "session",
+                parameters = mapOf("MFAS_CAN_SETUP" to "\"SOFTWARE_TOKEN_MFA\"")
+            ),
+            onSuccess = {
+                signInResult = it
+            },
+            onError = {
+                errorResult = it
+            },
+            signInTOTPSetupData = totpSetupData,
+            allowedMFAType = null
+        )
+        assertEquals(
+            AuthSignInResult(
+                false,
+                AuthNextSignInStep(
+                    AuthSignInStep.CONTINUE_SIGN_IN_WITH_TOTP_SETUP,
+                    mapOf("MFAS_CAN_SETUP" to "\"SOFTWARE_TOKEN_MFA\""),
+                    null,
+                    TOTPSetupDetails(
+                        sharedSecret = totpSetupData.secretCode,
+                        username = totpSetupData.username
+                    ),
+                    null
+                )
+            ),
+            signInResult
+        )
+        assertNull(errorResult)
+    }
+
+    @Test
+    fun `User needs to setup email OTP`() {
+        var signInResult: AuthSignInResult? = null
+        var errorResult: AuthException? = null
+        SignInChallengeHelper.getNextStep(
+            challenge = AuthChallenge(
+                challengeName = "MFA_SETUP",
+                username = username,
+                session = "session",
+                parameters = mapOf("MFAS_CAN_SETUP" to "\"EMAIL_OTP\"")
+            ),
+            onSuccess = {
+                signInResult = it
+            },
+            onError = {
+                errorResult = it
+            },
+            signInTOTPSetupData = null,
+            allowedMFAType = null
+        )
+        assertEquals(
+            AuthSignInResult(
+                false,
+                AuthNextSignInStep(
+                    AuthSignInStep.CONTINUE_SIGN_IN_WITH_EMAIL_MFA_SETUP,
+                    emptyMap(),
+                    null,
+                    null,
+                    null
+                )
+            ),
+            signInResult
+        )
+        assertNull(errorResult)
+    }
+
+    // MFA Selection
+    @Test
+    fun `User needs to select which MFA type to use`() {
+        var signInResult: AuthSignInResult? = null
+        var errorResult: AuthException? = null
+        SignInChallengeHelper.getNextStep(
+            challenge = AuthChallenge(
+                challengeName = "SELECT_MFA_TYPE",
+                username = username,
+                session = "session",
+                parameters = mapOf("MFAS_CAN_CHOOSE" to "\"EMAIL_MFA\",\"SOFTWARE_TOKEN_MFA\",\"SMS_MFA\"")
+            ),
+            onSuccess = {
+                signInResult = it
+            },
+            onError = {
+                errorResult = it
+            },
+            signInTOTPSetupData = null,
+            allowedMFAType = null
+        )
+        assertEquals(
+            AuthSignInResult(
+                false,
+                AuthNextSignInStep(
+                    AuthSignInStep.CONTINUE_SIGN_IN_WITH_MFA_SELECTION,
+                    emptyMap(),
+                    null,
+                    null,
+                    setOf(MFAType.EMAIL, MFAType.TOTP, MFAType.SMS)
+                )
+            ),
+            signInResult
+        )
+        assertNull(errorResult)
+    }
+
+    // Email OTP
+    @Test
+    fun `User is asked to confirm an emailed MFA code`() {
+        val deliveryDetails = AuthCodeDeliveryDetails(email, AuthCodeDeliveryDetails.DeliveryMedium.EMAIL)
+
+        var signInResult: AuthSignInResult? = null
+        var errorResult: AuthException? = null
+        SignInChallengeHelper.getNextStep(
+            challenge = AuthChallenge(
+                challengeName = "EMAIL_OTP",
+                username = username,
+                session = "session",
+                parameters = mapOf(
+                    "CODE_DELIVERY_DELIVERY_MEDIUM" to "email",
+                    "CODE_DELIVERY_DESTINATION" to email
+                )
+            ),
+            onSuccess = {
+                signInResult = it
+            },
+            onError = {
+                errorResult = it
+            },
+            signInTOTPSetupData = null,
+            allowedMFAType = null
+        )
+        assertEquals(
+            AuthSignInResult(
+                false,
+                AuthNextSignInStep(
+                    AuthSignInStep.CONFIRM_SIGN_IN_WITH_EMAIL_MFA_CODE,
+                    emptyMap(),
+                    deliveryDetails,
+                    null,
+                    null
+                )
+            ),
+            signInResult
+        )
+        assertNull(errorResult)
+    }
+
+    // TOTP
+    @Test
+    fun `User is asked to input the TOTP code`() {
+        var signInResult: AuthSignInResult? = null
+        var errorResult: AuthException? = null
+        SignInChallengeHelper.getNextStep(
+            challenge = AuthChallenge(
+                challengeName = "SOFTWARE_TOKEN_MFA",
+                username = username,
+                session = "session",
+                parameters = null
+            ),
+            onSuccess = {
+                signInResult = it
+            },
+            onError = {
+                errorResult = it
+            },
+            signInTOTPSetupData = null,
+            allowedMFAType = null
+        )
+        assertEquals(
+            AuthSignInResult(
+                false,
+                AuthNextSignInStep(
+                    AuthSignInStep.CONFIRM_SIGN_IN_WITH_TOTP_CODE,
+                    emptyMap(),
+                    null,
+                    null,
+                    null
+                )
+            ),
+            signInResult
+        )
+        assertNull(errorResult)
+    }
+
+    // SMS
+    @Test
+    fun `User is asked to confirm an SMS MFA code`() {
+        val phoneNumber = "+15555555555"
+        val deliveryDetails = AuthCodeDeliveryDetails(phoneNumber, AuthCodeDeliveryDetails.DeliveryMedium.SMS)
+
+        var signInResult: AuthSignInResult? = null
+        var errorResult: AuthException? = null
+        SignInChallengeHelper.getNextStep(
+            challenge = AuthChallenge(
+                challengeName = "SMS_MFA",
+                username = username,
+                session = "session",
+                parameters = mapOf(
+                    "CODE_DELIVERY_DELIVERY_MEDIUM" to "sms",
+                    "CODE_DELIVERY_DESTINATION" to phoneNumber
+                )
+            ),
+            onSuccess = {
+                signInResult = it
+            },
+            onError = {
+                errorResult = it
+            },
+            signInTOTPSetupData = null,
+            allowedMFAType = null
+        )
+        assertEquals(
+            AuthSignInResult(
+                false,
+                AuthNextSignInStep(
+                    AuthSignInStep.CONFIRM_SIGN_IN_WITH_SMS_MFA_CODE,
+                    emptyMap(),
+                    deliveryDetails,
+                    null,
+                    null
+                )
+            ),
+            signInResult
+        )
+        assertNull(errorResult)
+    }
+
+    // Exceptions
+    @Test
+    fun `Exception when setting up unknown challenge type`() {
+        var signInResult: AuthSignInResult? = null
+        var errorResult: AuthException? = null
+        SignInChallengeHelper.getNextStep(
+            challenge = AuthChallenge(
+                challengeName = "MFA_SETUP",
+                username = username,
+                session = "session",
+                parameters = mapOf("MFAS_CAN_SETUP" to "\"UNKNOWN_CHALLENGE\"")
+            ),
+            onSuccess = {
+                signInResult = it
+            },
+            onError = {
+                errorResult = it
+            },
+            signInTOTPSetupData = null,
+            allowedMFAType = null
+        )
+        assertIs<UnknownException>(errorResult)
+        assertNull(signInResult)
+    }
+
+    @Test
+    fun `Exception when choosing an unknown challenge type`() {
+        var signInResult: AuthSignInResult? = null
+        var errorResult: AuthException? = null
+        SignInChallengeHelper.getNextStep(
+            challenge = AuthChallenge(
+                challengeName = "MFA_SETUP",
+                username = username,
+                session = "session",
+                parameters = mapOf("MFAS_CAN_CHOOSE" to "\"EMAIL_MFA\",\"UNKNOWN\",\"SMS_MFA\"")
+            ),
+            onSuccess = {
+                signInResult = it
+            },
+            onError = {
+                errorResult = it
+            },
+            signInTOTPSetupData = null,
+            allowedMFAType = null
+        )
+        assertIs<UnknownException>(errorResult)
+        assertNull(signInResult)
+    }
+
+    @Test
+    fun `Exception when receiving an unsupported challenge name`() {
+        var signInResult: AuthSignInResult? = null
+        var errorResult: AuthException? = null
+        SignInChallengeHelper.getNextStep(
+            challenge = AuthChallenge(
+                challengeName = "UNKNOWN",
+                username = username,
+                session = "session",
+                parameters = mapOf("MFAS_CAN_CHOOSE" to "\"EMAIL_MFA\",\"UNKNOWN\",\"SMS_MFA\"")
+            ),
+            onSuccess = {
+                signInResult = it
+            },
+            onError = {
+                errorResult = it
+            },
+            signInTOTPSetupData = null,
+            allowedMFAType = null
+        )
+        assertIs<UnknownException>(errorResult)
+        assertNull(signInResult)
+    }
+}
