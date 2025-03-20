639f10799cf211c9ef0118666c2284179e8fe55c
Vincent Tran <hello@vincetran.me>
2025-03-20 13:24:38 -0700

    Updated parsing script to handle multiple jobs in a run

--- scripts/python/generate_df_testrun_report.py
+++ scripts/python/generate_df_testrun_report.py
@@ -23,23 +23,6 @@ def parse_arguments():
     parser.add_argument("-o", "--output_path", help="Destination path for the build reports.", required=True)
     return parser.parse_args()
 
-def download_logs(df_client, arn, output_dir):
-    response = df_client.list_artifacts(
-        arn=arn,
-        type="FILE"
-    )
-
-    artifacts = response["artifacts"]
-    customer_artifact = next((artifact for artifact in artifacts if artifact["type"] == "CUSTOMER_ARTIFACT"), None)
-
-    LOGGER.debug(f"Found customer artifact: {customer_artifact}")
-
-    unzip_result = dload.save_unzip(customer_artifact["url"], extract_path=output_dir, delete_after=True)
-    if unzip_result is None or unzip_result == "":
-        LOGGER.error("Unzip of test run artifacts failed")
-    else:
-        LOGGER.info(f"Save/Unzip passed: {unzip_result}")
-
 def main(arguments):
     LOGGER.info(f"Starting to generate report...")
     args = parse_arguments()
@@ -52,125 +35,147 @@ def main(arguments):
         region_name='us-west-2'
     )
 
-    download_logs(df_client, args.run_arn, logs_dir)
+#     download_logs(df_client, args.run_arn, logs_dir)
+    response = df_client.list_artifacts(
+        arn=args.run_arn,
+        type="FILE"
+    )
 
-    parser = Parser(args.module_name)
+    # A single test run may have multiple jobs where each job tests on a different device
+    # A regular PR typically tests on one device while a release PR typically tests on 3 devices
+    # The instrumentation logs for each job is uploaded as a separate CUSTOMER_ARTIFACT in the
+    # run's artifacts.
+    artifacts = response["artifacts"]
+    customer_artifacts = (artifact for artifact in artifacts if artifact["type"] == "CUSTOMER_ARTIFACT")
+    while (customer_artifact := next(customer_artifacts, None)) is not None:
+        LOGGER.info(f"Parsing result for artifact ARN: {customer_artifact['arn']}")
 
-    metrics = []
-    try:
-        # Open the provided file and then start to parse it
-        with open(log_file, "r") as file:
-            for line in file:
-                parser.parse_line(line.strip())
+        # TODO: Change delete_after to True
+        unzip_result = dload.save_unzip(customer_artifact["url"], extract_path=logs_dir, delete_after=False)
+        if unzip_result is None or unzip_result == "":
+            LOGGER.error("Unzip of test run artifacts failed")
+            break
 
-        module_passing_tests = module_failing_tests = 0
-        LOGGER.info(f"\n--------------------------\nTest Suite Statistics\n--------------------------")
-        test_run_passing_tests = test_run_failing_tests = 0
+        parser = Parser(args.module_name)
+        metrics = []
+        try:
+            # Open the provided file and then start to parse it
+            with open(log_file, "r") as file:
+                for line in file:
+                    try:
+                        parser.parse_line(line.strip())
+                    except Exception as e:
+                        exception_value, exception_location = get_exception(sys.exc_info())
+                        LOGGER.error(f"Encountered an exception trying to parse the results: {exception_value} at [{exception_location}] for line:\n{line.strip()}")
 
-        # The Device Farm run ARN is in the format of:
-        # arn:aws:devicefarm:us-west-2:ACCOUNT_ID:job:PROJECT_ARN_ID/RUN_ARN_ID
-        # So split the run ARN by ':', take the last element, split it by '/' and then use each
-        # component to format a URL to add to the test report for easy access to the logs and output files
-        arn_components = args.run_arn.split(":")[-1].split("/")
-        run_url = f"https://us-west-2.console.aws.amazon.com/devicefarm/home#/mobile/projects/{arn_components[0]}/runs/{arn_components[1]}/jobs/00000"
-        debug_messaging = f"You can find the detailed logs and output files at {run_url}"
-        for test_suite_name, test_suite in parser.test_run.test_suites.items():
-            test_cases = []
-            for test_name, test in test_suite.tests.items():
-                if test.status_code == 0 or test.status_code == -3 or test.status_code == -4:
-                    test_status = "PASSED"
-                elif test.status_code == -1:
-                    test_status = "ERROR"
-                else:
-                    test_status = "FAILED"
+            module_passing_tests = module_failing_tests = 0
+            LOGGER.info(f"\n--------------------------\nTest Suite Statistics\n--------------------------")
+            test_run_passing_tests = test_run_failing_tests = 0
 
-                tc = TestCase(test_name,
-                    classname=test_suite_name,
-                    stdout=f"{debug_messaging}\n{test.stack_trace}",
-                    status=test_status
-                )
+            # The Device Farm run ARN is in the format of:
+            # arn:aws:devicefarm:us-west-2:ACCOUNT_ID:job:PROJECT_ARN_ID/RUN_ARN_ID
+            # So split the run ARN by ':', take the last element, split it by '/' and then use each
+            # component to format a URL to add to the test report for easy access to the logs and output files
+            arn_components = args.run_arn.split(":")[-1].split("/")
+            run_url = f"https://us-west-2.console.aws.amazon.com/devicefarm/home#/mobile/projects/{arn_components[0]}/runs/{arn_components[1]}/jobs/00000"
+            debug_messaging = f"You can find the detailed logs and output files at {run_url}"
+            for test_suite_name, test_suite in parser.test_run.test_suites.items():
+                test_cases = []
+                for test_name, test in test_suite.tests.items():
+                    if test.status_code == 0 or test.status_code == -3 or test.status_code == -4:
+                        test_status = "PASSED"
+                    elif test.status_code == -1:
+                        test_status = "ERROR"
+                    else:
+                        test_status = "FAILED"
 
-                if test_status == "FAILED":
-                    tc.add_failure_info(test.stack_trace)
-                elif test_status == "ERROR":
-                    tc.add_error_info(test.stack_trace)
+                    tc = TestCase(test_name,
+                        classname=test_suite_name,
+                        stdout=f"{debug_messaging}\n{test.stack_trace}",
+                        status=test_status
+                    )
 
-                test_cases.append(tc)
+                    if test_status == "FAILED":
+                        tc.add_failure_info(test.stack_trace)
+                    elif test_status == "ERROR":
+                        tc.add_error_info(test.stack_trace)
 
-            ts = TestSuite(test_suite_name, test_cases=test_cases)
-            ts_output = TestSuite.to_xml_string([ts])
-            LOGGER.info(f"Saving test suite {test_suite_name} report.")
+                    test_cases.append(tc)
 
-            if not os.path.exists(args.output_path):
-                os.makedirs(args.output_path)
-            f = open(args.output_path + test_suite_name + ".xml", "w")
-            f.write(ts_output)
-            f.close()
+                ts = TestSuite(test_suite_name, test_cases=test_cases)
+                ts_output = TestSuite.to_xml_string([ts])
+                LOGGER.info(f"Saving test suite {test_suite_name} report.")
 
-            success_percentage = test_suite.passing_tests/(test_suite.passing_tests + test_suite.failing_tests)
-            LOGGER.info(f"Name: {test_suite_name}")
-            LOGGER.info(f"Passing Tests: {test_suite.passing_tests}")
-            LOGGER.info(f"Failing Tests: {test_suite.failing_tests}")
+                if not os.path.exists(args.output_path):
+                    os.makedirs(args.output_path)
+                f = open(args.output_path + test_suite_name + ".xml", "w")
+                f.write(ts_output)
+                f.close()
+
+                success_percentage = test_suite.passing_tests/(test_suite.passing_tests + test_suite.failing_tests)
+                LOGGER.info(f"Name: {test_suite_name}")
+                LOGGER.info(f"Passing Tests: {test_suite.passing_tests}")
+                LOGGER.info(f"Failing Tests: {test_suite.failing_tests}")
+                LOGGER.info(f"Success Percentage: {success_percentage}")
+                LOGGER.info(f"------------------------------------------------")
+                test_run_passing_tests += test_suite.passing_tests
+                module_passing_tests += test_suite.passing_tests
+                test_run_failing_tests += test_suite.failing_tests
+                module_failing_tests += test_suite.failing_tests
+                if (success_percentage < 1.0):
+                    parser.get_stack_traces(test_suite, metrics)
+
+                test_suite_dimension = [ get_dimension("Module", args.module_name), get_dimension("Test Suite", test_suite_name) ]
+
+                # Test Suite Success Percentage
+                metrics.append(get_metric("Test Success Percentage", test_suite_dimension, success_percentage, "Count"))
+
+                # Test Suite Success Count
+                metrics.append(get_metric("Tests Succeeded", test_suite_dimension, test_run_passing_tests, "Count"))
+
+                # Test Suite Failure Count
+                metrics.append(get_metric("Tests Failed", test_suite_dimension, test_run_failing_tests, "Count"))
+
+            LOGGER.info(f"\n--------------------------\nTest Run Statistics\n--------------------------")
+            LOGGER.info(f"Run Name: {args.module_name}")
+            LOGGER.info(f"Test Successes: {test_run_passing_tests}")
+            LOGGER.info(f"Test Failures: {test_run_failing_tests}")
+            success_percentage = test_run_passing_tests/(test_run_passing_tests + test_run_failing_tests)
             LOGGER.info(f"Success Percentage: {success_percentage}")
-            LOGGER.info(f"------------------------------------------------")
-            test_run_passing_tests += test_suite.passing_tests
-            module_passing_tests += test_suite.passing_tests
-            test_run_failing_tests += test_suite.failing_tests
-            module_failing_tests += test_suite.failing_tests
-            if (success_percentage < 1.0):
-                parser.get_stack_traces(test_suite, metrics)
+            LOGGER.info(f"Test Run Execution Time: {parser.execution_time}")
 
-            test_suite_dimension = [ get_dimension("Module", args.module_name), get_dimension("Test Suite", test_suite_name) ]
+            module_dimension = [ get_dimension("Module", args.module_name) ]
+            success_percentage = module_passing_tests/(module_passing_tests + module_failing_tests)
+            # Test Run Success Percentage
+            metrics.append(get_metric("Test Success Percentage", module_dimension, success_percentage, "Count"))
+            # Test Run Success Count
+            metrics.append(get_metric("Tests Succeeded", module_dimension, module_passing_tests, "Count"))
+            # Test Run Failure Count
+            metrics.append(get_metric("Tests Failed", module_dimension, module_failing_tests, "Count"))
+            # Test Run Execution Time
+            metrics.append(get_metric("Execution Time", module_dimension, float(parser.execution_time), "Seconds"))
+        except Exception as e:
+            exception_value, exception_location = get_exception(sys.exc_info())
 
-            # Test Suite Success Percentage
-            metrics.append(get_metric("Test Success Percentage", test_suite_dimension, success_percentage, "Count"))
+            LOGGER.error(f"Encountered an exception trying to parse the results: {exception_value} at [{exception_location}]")
+            exception_dimensions = [ get_dimension("Exception", exception_value), get_dimension("Line Number", exception_location) ]
+            metrics.append(get_metric("Test Run Reporting Error", exception_dimensions, 1, "Count"))
+            print(f"Adding metric [{get_metric('Test Run Reporting Error', exception_dimensions, 1, 'Count')}]")
+        try:
+            cw_client = boto3.client(
+                'cloudwatch',
+                region_name='us-west-2'
+            )
 
-            # Test Suite Success Count
-            metrics.append(get_metric("Tests Succeeded", test_suite_dimension, test_run_passing_tests, "Count"))
-
-            # Test Suite Failure Count
-            metrics.append(get_metric("Tests Failed", test_suite_dimension, test_run_failing_tests, "Count"))
-
-        LOGGER.info(f"\n--------------------------\nTest Run Statistics\n--------------------------")
-        LOGGER.info(f"Run Name: {args.module_name}")
-        LOGGER.info(f"Test Successes: {test_run_passing_tests}")
-        LOGGER.info(f"Test Failures: {test_run_failing_tests}")
-        success_percentage = test_run_passing_tests/(test_run_passing_tests + test_run_failing_tests)
-        LOGGER.info(f"Success Percentage: {success_percentage}")
-        LOGGER.info(f"Test Run Execution Time: {parser.execution_time}")
-
-        module_dimension = [ get_dimension("Module", args.module_name) ]
-        success_percentage = module_passing_tests/(module_passing_tests + module_failing_tests)
-        # Test Run Success Percentage
-        metrics.append(get_metric("Test Success Percentage", module_dimension, success_percentage, "Count"))
-        # Test Run Success Count
-        metrics.append(get_metric("Tests Succeeded", module_dimension, module_passing_tests, "Count"))
-        # Test Run Failure Count
-        metrics.append(get_metric("Tests Failed", module_dimension, module_failing_tests, "Count"))
-        # Test Run Execution Time
-        metrics.append(get_metric("Execution Time", module_dimension, float(parser.execution_time), "Seconds"))
-    except Exception as e:
-        exception_value, exception_location = get_exception(sys.exc_info())
-
-        LOGGER.error(f"Encountered an exception trying to parse the results: {exception_value} at [{exception_location}]")
-        exception_dimensions = [ get_dimension("Exception", exception_value), get_dimension("Line Number", exception_location) ]
-        metrics.append(get_metric("Test Run Reporting Error", exception_dimensions, 1, "Count"))
-        print(f"Adding metric [{get_metric('Test Run Reporting Error', exception_dimensions, 1, 'Count')}]")
-    try:
-        cw_client = boto3.client(
-            'cloudwatch',
-            region_name='us-west-2'
-        )
-
-        response = cw_client.put_metric_data(
-            Namespace='AmplifyAndroidV2-IntegTests',
-            MetricData=metrics
-        )
-        LOGGER.info(response)
-    except Exception as e:
-        exception_value, exception_location = get_exception(sys.exc_info())
-        LOGGER.error(f"Encountered an exception trying to parse the results: {exception_value} at [{exception_location}]")
-        LOGGER.error(f"The metrics that were attempted to be published: {metrics}")
+            response = cw_client.put_metric_data(
+                Namespace='AmplifyTests-7',
+                MetricData=metrics
+            )
+            LOGGER.info(response)
+        except Exception as e:
+            exception_value, exception_location = get_exception(sys.exc_info())
+            LOGGER.error(f"Encountered an exception trying to parse the results: {exception_value} at [{exception_location}]")
+            LOGGER.error(f"The metrics that were attempted to be published: {metrics}")
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
