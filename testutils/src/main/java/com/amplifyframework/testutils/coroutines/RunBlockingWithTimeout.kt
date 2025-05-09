84bd0ff59a1c443152e5a1fcfaeceef844b54734
tjroach <tjroach@amazon.com>
2025-05-09 09:29:06 -0400

    lint

--- testutils/src/main/java/com/amplifyframework/testutils/coroutines/RunBlockingWithTimeout.kt
+++ testutils/src/main/java/com/amplifyframework/testutils/coroutines/RunBlockingWithTimeout.kt
@@ -1,17 +1,14 @@
 package com.amplifyframework.testutils.coroutines
 
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeout
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
 
 /**
  * Runs a blocking coroutine with a timeout.
  */
-fun <T> runBlockingWithTimeout(
-    timeout: Duration = 5.seconds,
-    block: suspend () -> T
-): T = runBlocking {
+fun <T> runBlockingWithTimeout(timeout: Duration = 5.seconds, block: suspend () -> T): T = runBlocking {
     withTimeout(timeout) {
         block()
     }
