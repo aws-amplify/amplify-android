9dc4d7a794a6c1bf3c71905d5df77fdebeddc957
tjroach <tjroach@amazon.com>
2025-09-04 15:19:55 -0400

    lint

--- aws-api-appsync/src/main/java/com/amplifyframework/core/model/query/predicate/GsonPredicateAdapters.java
+++ aws-api-appsync/src/main/java/com/amplifyframework/core/model/query/predicate/GsonPredicateAdapters.java
@@ -17,6 +17,7 @@ package com.amplifyframework.core.model.query.predicate;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
@@ -24,7 +25,6 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
-import com.google.gson.JsonArray;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
