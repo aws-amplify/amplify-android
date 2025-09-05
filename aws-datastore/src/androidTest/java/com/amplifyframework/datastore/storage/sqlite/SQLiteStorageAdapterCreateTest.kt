0374b7abe3c5158aefac29a691730d51ad262128
tjroach <tjroach@amazon.com>
2025-09-05 16:11:49 -0400

    kotlin conversions

--- aws-datastore/src/androidTest/java/com/amplifyframework/datastore/storage/sqlite/SQLiteStorageAdapterCreateTest.kt
+++ aws-datastore/src/androidTest/java/com/amplifyframework/datastore/storage/sqlite/SQLiteStorageAdapterCreateTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -12,80 +12,68 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+package com.amplifyframework.datastore.storage.sqlite
 
-package com.amplifyframework.datastore.storage.sqlite;
-
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import androidx.test.core.app.ApplicationProvider;
-
-import com.amplifyframework.datastore.StrictMode;
-import com.amplifyframework.datastore.storage.SynchronousStorageAdapter;
-import com.amplifyframework.datastore.syncengine.MigrationFlagsTable;
-import com.amplifyframework.testmodels.commentsblog.AmplifyModelProvider;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import static org.junit.Assert.assertTrue;
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import androidx.test.core.app.ApplicationProvider
+import com.amplifyframework.datastore.storage.SynchronousStorageAdapter
+import com.amplifyframework.datastore.syncengine.MigrationFlagsTable
+import com.amplifyframework.testmodels.commentsblog.AmplifyModelProvider
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
 
 /**
- * Test the creation functionality of {@link SQLiteStorageAdapter} operations.
+ * Test the creation functionality of [SQLiteStorageAdapter] operations.
  */
-public final class SQLiteStorageAdapterCreateTest {
-    private SynchronousStorageAdapter adapter;
-
-    /**
-     * Enables Android Strict Mode, to help catch common errors while using
-     * SQLite, such as forgetting to close the database.
-     */
-    @BeforeClass
-    public static void enableStrictMode() {
-        StrictMode.enable();
-    }
+class SQLiteStorageAdapterCreateTest {
+    private lateinit var adapter: SynchronousStorageAdapter
 
     /**
      * Remove any old database files, and then re-provision a new storage adapter,
      * that is able to store the Comment-Blog family of models.
      */
     @Before
-    public void setup() {
-        TestStorageAdapter.cleanup();
-        this.adapter = TestStorageAdapter.create(AmplifyModelProvider.getInstance());
+    fun setup() {
+        TestStorageAdapter.cleanup()
+        this.adapter = TestStorageAdapter.create(AmplifyModelProvider.getInstance())
     }
 
     /**
      * Close the open database, and cleanup any database files that it left.
      */
     @After
-    public void teardown() {
-        TestStorageAdapter.cleanup(adapter);
+    fun teardown() {
+        TestStorageAdapter.cleanup(adapter)
     }
 
     /**
      * Test that initial creation creates migration flags table with initial entries.
      */
     @Test
-    public void verifyMigrationFlagsTableExistsAndContainsRecordsOnCreate() {
+    fun verifyMigrationFlagsTableExistsAndContainsRecordsOnCreate() {
         // Verify migration flags table exists and has initial entries
-        String dbPath = ApplicationProvider.getApplicationContext()
-                .getDatabasePath(SQLiteStorageAdapter.DEFAULT_DATABASE_NAME).getAbsolutePath();
-        SQLiteDatabase database = SQLiteDatabase.openDatabase(
-                dbPath,
-                null,
-                SQLiteDatabase.OPEN_READONLY
-        );
-        try (Cursor cursor = database.rawQuery(
+        val dbPath = ApplicationProvider.getApplicationContext<Context>()
+            .getDatabasePath(SQLiteStorageAdapter.DEFAULT_DATABASE_NAME).absolutePath
+        val database = SQLiteDatabase.openDatabase(
+            dbPath,
+            null,
+            SQLiteDatabase.OPEN_READONLY
+        )
+        try {
+            database.rawQuery(
                 "SELECT 1 FROM " + MigrationFlagsTable.TABLE_NAME +
-                " WHERE " + MigrationFlagsTable.COLUMN_FLAG_NAME + " = ?", 
-                new String[]{
+                    " WHERE " + MigrationFlagsTable.COLUMN_FLAG_NAME + " = ?",
+                arrayOf(
                     MigrationFlagsTable.CLEARED_V2_30_0_AND_BELOW_GROUP_SYNC_EXPRESSIONS
-                })) {
-            assertTrue("Migration flag row was not created", cursor.getCount() > 0);
+                )
+            ).use { cursor ->
+                Assert.assertTrue("Migration flag row was not created", cursor.count > 0)
+            }
         } finally {
-            database.close();
+            database.close()
         }
     }
 }
--- aws-datastore/src/main/java/com/amplifyframework/datastore/storage/sqlite/migrations/ClearInvalidGroupSyncExpressions.kt
+++ aws-datastore/src/main/java/com/amplifyframework/datastore/storage/sqlite/migrations/ClearInvalidGroupSyncExpressions.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -12,73 +12,64 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+package com.amplifyframework.datastore.storage.sqlite.migrations
 
-package com.amplifyframework.datastore.storage.sqlite.migrations;
-
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-
-import com.amplifyframework.core.Amplify;
-import com.amplifyframework.core.category.CategoryType;
-import com.amplifyframework.datastore.syncengine.MigrationFlagsTable;
-import com.amplifyframework.logging.Logger;
+import android.database.sqlite.SQLiteDatabase
+import com.amplifyframework.core.Amplify
+import com.amplifyframework.core.category.CategoryType
+import com.amplifyframework.datastore.syncengine.MigrationFlagsTable
+import com.amplifyframework.datastore.syncengine.MigrationFlagsTable.flags
 
 /**
  * Clear syncExpression for rows containing "_type":"GROUP".
  * Amplify 2.30.0 and older were improperly serializing QueryPredicateGroup expressions.
  */
-final class ClearInvalidGroupSyncExpressions implements ModelMigration {
-    private static final Logger LOG = Amplify.Logging.logger(CategoryType.DATASTORE, "amplify:aws-datastore");
-    private static final String MIGRATION_FLAG = MigrationFlagsTable.CLEARED_V2_30_0_AND_BELOW_GROUP_SYNC_EXPRESSIONS;
-    private final SQLiteDatabase database;
+internal class ClearInvalidGroupSyncExpressions(private val database: SQLiteDatabase) : ModelMigration {
 
-    /**
-     * Constructor for the migration class.
-     * @param database Connection to the SQLite database.
-     */
-    ClearInvalidGroupSyncExpressions(SQLiteDatabase database) {
-        this.database = database;
-    }
-
-    @Override
-    public void apply() {
-        database.execSQL(MigrationFlagsTable.CREATE_SQL);
-        if (isAlreadyCompleted()) {
-            return;
+    override fun apply() {
+        database.execSQL(MigrationFlagsTable.CREATE_SQL)
+        if (isAlreadyCompleted) {
+            return
         }
-        clearGroupSyncExpressions();
-        markMigrationCompleted();
-        LOG.debug("Cleared potentially invalid QueryPredicateGroup sync expressions");
-    }
-
-    private boolean isAlreadyCompleted() {
-        try (Cursor cursor = database.rawQuery(
-                "SELECT 1 FROM " + MigrationFlagsTable.TABLE_NAME + 
-                " WHERE " + MigrationFlagsTable.COLUMN_FLAG_NAME + " = ?", 
-                new String[]{MIGRATION_FLAG})) {
-            return cursor.getCount() > 0;
-        } catch (Exception exception) {
-            return false;
+        try {
+            database.beginTransaction()
+            clearGroupSyncExpressions()
+            markMigrationCompleted()
+            database.setTransactionSuccessful()
+            LOG.debug("Cleared potentially invalid QueryPredicateGroup sync expressions")
+        } finally {
+            database.endTransaction()
         }
     }
 
-    private void clearGroupSyncExpressions() {
+    private val isAlreadyCompleted: Boolean
+        get() {
+            try {
+                database.rawQuery(
+                    "SELECT 1 FROM ${MigrationFlagsTable.TABLE_NAME} WHERE ${MigrationFlagsTable.COLUMN_FLAG_NAME} = ?",
+                    arrayOf(MIGRATION_FLAG)
+                ).use { cursor ->
+                    return cursor.count > 0
+                }
+            } catch (_: Exception) {
+                return false
+            }
+        }
+
+    private fun clearGroupSyncExpressions() {
         database.execSQL(
-                "UPDATE LastSyncMetadata " +
-                        "SET syncExpression = NULL " +
-                        "WHERE syncExpression LIKE '%\"_type\":\"GROUP\"%'"
-        );
+            "UPDATE LastSyncMetadata SET syncExpression = NULL WHERE syncExpression LIKE '%\"_type\":\"GROUP\"%'"
+        )
     }
 
-    private void markMigrationCompleted() {
-        if (MigrationFlagsTable.getFlags()
-                .containsKey(MigrationFlagsTable.CLEARED_V2_30_0_AND_BELOW_GROUP_SYNC_EXPRESSIONS)
-        ) {
-            database.execSQL(
-                    MigrationFlagsTable.getFlags().get(
-                            MigrationFlagsTable.CLEARED_V2_30_0_AND_BELOW_GROUP_SYNC_EXPRESSIONS
-                    )
-            );
+    private fun markMigrationCompleted() {
+        if (flags.containsKey(MigrationFlagsTable.CLEARED_V2_30_0_AND_BELOW_GROUP_SYNC_EXPRESSIONS)) {
+            database.execSQL(flags[MigrationFlagsTable.CLEARED_V2_30_0_AND_BELOW_GROUP_SYNC_EXPRESSIONS])
         }
+    }
+
+    companion object {
+        private val LOG = Amplify.Logging.logger(CategoryType.DATASTORE, "amplify:aws-datastore")
+        private const val MIGRATION_FLAG = MigrationFlagsTable.CLEARED_V2_30_0_AND_BELOW_GROUP_SYNC_EXPRESSIONS
     }
 }
